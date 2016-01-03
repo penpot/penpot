80ec20d9cd8ac531220aff57fc943cf03fc23868
Andrey Antukh <niwi@niwi.nz>
2016-01-03 18:44:13 +0200

    Add helper for find dom objects by class name.

--- frontend/uxbox/ui/dom.cljs
+++ frontend/uxbox/ui/dom.cljs
@@ -1,4 +1,9 @@
-(ns uxbox.ui.dom)
+(ns uxbox.ui.dom
+  (:require [goog.dom :as dom]))
+
+(defn get-element-by-class
+  [^string classname]
+  (dom/getElementByClass classname))
 
 (defn stop-propagation
   [e]
