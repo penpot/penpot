1725f518183bd5643b8c8f4eb0d16cbecf6d5644
Andrey Antukh <niwi@niwi.nz>
2016-01-17 23:27:44 +0200

    Add math utils namespace.

---
+++ src/uxbox/math.cljs
@@ -0,0 +1,33 @@
+(ns uxbox.math
+  "A collection of math utils."
+  (:require [goog.math :as math]))
+
+(defn sin
+  "Returns the sine of a number"
+  [^number v]
+  (js/Math.sin v))
+
+(defn cos
+  "Returns the cosine of a number."
+  [^number v]
+  (js/Math.cos v))
+
+(defn tan
+  "Returns the tangent of a number."
+  [^number v]
+  (js/Math.tan v))
+
+(defn neg
+  "Negate the number"
+  [^number v]
+  (- v))
+
+(defn radiants
+  "Converts degrees to radians."
+  [^number degrees]
+  (math/toRadians degrees))
+
+(defn degrees
+  "Converts radians to degrees."
+  [^number radiants]
+  (math/toDegrees radiants))
