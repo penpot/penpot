8284f0141c3ab18c025db6acb7e85878f51adff8
Andrey Antukh <niwi@niwi.nz>
2016-01-22 19:34:46 +0200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [org.clojure/clojurescript "1.7.228" :scope "provided"]
-                 [figwheel-sidecar "0.5.0-2" :scope "test"]
+                 [figwheel-sidecar "0.5.0-3" :scope "test"]
 
                  ;; runtime
                  [rum "0.6.0" :exclusions [sablono]]
@@ -14,14 +14,14 @@
                  [cljsjs/react "0.14.3-0"]
                  [cljsjs/react-dom "0.14.3-1"]
                  [cljsjs/react-dom-server "0.14.3-0"]
-                 [cljsjs/moment "2.10.6-0"]
+                 [cljsjs/moment "2.10.6-1"]
                  [funcool/promesa "0.7.0"]
-                 [funcool/beicon "0.5.1"]
-                 [funcool/cats "1.2.1"]
+                 [funcool/beicon "0.6.0"]
                  [funcool/cuerdas "0.7.1"]
                  [funcool/hodgepodge "0.1.4"]
                  [bouncer "1.0.0"]
-                 [bidi "1.21.0"]]
+                 [bidi "1.25.0"]]
+  :plugins [[lein-ancient "0.6.7" :exclusions [org.clojure/tools.reader]]]
 
   :clean-targets ^{:protect false} ["resources/public/js" "target"]
   )
