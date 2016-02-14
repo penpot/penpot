fefd863957dc426115ba53b6df8f0cb522e50332
Andrey Antukh <niwi@niwi.nz>
2016-02-14 19:30:51 +0200

    Add missing cats dep.

--- project.clj
+++ project.clj
@@ -14,6 +14,7 @@
                  [cljsjs/react "0.14.3-0"]
                  [cljsjs/react-dom "0.14.3-1"]
                  [cljsjs/moment "2.10.6-1"]
+                 [funcool/cats "1.2.1"]
                  [funcool/promesa "0.8.1"]
                  [funcool/beicon "0.6.1"]
                  [funcool/cuerdas "0.7.1"]
