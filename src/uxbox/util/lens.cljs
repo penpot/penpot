6f23272c631ad6f22df26adfc9fb0d39fc4f7421
Andrey Antukh <niwi@niwi.nz>
2016-02-21 20:32:09 +0200

    Remove unused lenses related code from utils ns.

--- src/uxbox/util/lens.cljs
+++ src/uxbox/util/lens.cljs
@@ -2,17 +2,6 @@
   (:refer-clojure :exclude [derive merge])
   (:require [cats.labs.lens :as l]))
 
-(defn dep-in
-  [where link]
-  {:pre [(vector? where) (vector? link)]}
-  (l/lens
-   (fn [s]
-     (let [value (get-in s link)
-           path (conj where value)]
-       (get-in s path)))
-   (fn [s f]
-     (throw (ex-info "Not implemented" {})))))
-
 (defn getter
   [f]
   (l/lens f #(throw (ex-info "Not implemented" {}))))
@@ -22,13 +11,3 @@
   (l/lens
    (fn [s] (cljs.core/merge s data))
    #(throw (ex-info "Not implemented" {}))))
-
-(defn derive
-  [a path]
-  (l/focus-atom (l/in path) a))
-
-(defn focus
-  ([state]
-   (l/focus-atom l/id state))
-  ([lens state]
-   (l/focus-atom lens state)))
