ea8b0f21f94b0cd2253655c75c5d646a9727ab6d
Andrey Antukh <niwi@niwi.nz>
2016-03-01 20:18:42 +0200

    Add license header to cljs files.

--- src/uxbox/ui/workspace/sidebar/layers.cljs
+++ src/uxbox/ui/workspace/sidebar/layers.cljs
@@ -1,3 +1,10 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; Copyright (c) 2015-2016 Andrey Antukh <niwi@niwi.nz>
+;; Copyright (c) 2015-2016 Juan de la Cruz <delacruzgarciajuan@gmail.com>
+
 (ns uxbox.ui.workspace.sidebar.layers
   (:require-macros [uxbox.util.syntax :refer (defer)])
   (:require [sablono.core :as html :refer-macros [html]]
