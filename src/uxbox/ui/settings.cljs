b33ca03af7aa3db657b09f976d43fbbe27e0d4a1
elhombretecla <delacruzgarciajuan@gmail.com>
2016-03-03 21:25:19 +0100

    create styles for user settings profile

--- src/uxbox/ui/settings.cljs
+++ src/uxbox/ui/settings.cljs
@@ -19,14 +19,28 @@
   (html
    [:main.dashboard-main
     (header)
-    [:section.dashboard-content
-     [:ul
-      [:li {:on-click #(r/go :settings/profile)} "Profile"]
-      [:li {:on-click #(r/go :settings/password)} "Password"]
-      [:li {:on-click #(r/go :settings/notifications)} "Notifications"]]
+    [:section.dashboard-content.user-settings
+     [:div.user-settings-nav
+      [:ul.user-settings-nav-inside
+       [:li.current {:on-click #(r/go :settings/profile)} "Profile"]
+       [:li {:on-click #(r/go :settings/password)} "Password"]
+       [:li {:on-click #(r/go :settings/notifications)} "Notifications"]]]
 
-     [:section.dashboard-grid.library
-      [:span "TODO 1"]]]]))
+     [:section.user-settings-content
+      [:span.user-settings-label "Your avatar"]
+      [:form.avatar-form
+       [:img {:src "images/favicon.png" :border "0"}]
+       [:input {:type "file"}]]
+      [:span.user-settings-label "Name"]
+      [:input.input-text {:type "text" :placeholder "Your name"}]
+      [:span.user-settings-label "Username"]
+      [:input.input-text {:type "text" :placeholder "Your username"}]
+      [:span.user-settings-label "Email"]
+      [:input.input-text {:type "email" :placeholder "Your email"}]
+      [:span.user-settings-label "Choose a color theme"]
+      [:span "TODO RADIO BUTTONS"]
+      [:input.btn-primary {:type "submit" :value "Update settings"}]
+     ]]]))
 
 (def ^:static profile-page
   (mx/component
