e9a51c14b8e37aed870dfc9314af75f08c9b4c92
Andrey Antukh <niwi@niwi.nz>
2016-03-15 20:52:29 +0200

    Minor renames on rstore impl.

--- src/uxbox/rstore.cljs
+++ src/uxbox/rstore.cljs
@@ -66,7 +66,7 @@
   ([event & events]
    (run! #(rx/push! bus %) (into [event] events))))
 
-(defn swap-state
+(defn swap
   "A helper for just apply some function to state
   without a need to declare additional event."
   [f]
@@ -75,7 +75,7 @@
     (-apply-update [_ state]
       (f state))))
 
-(defn reset-state
+(defn reset
   "A event that resets the internal state with
   the provided value."
   [state]
@@ -83,6 +83,8 @@
     UpdateEvent
     (-apply-update [_ _]
       state)))
+
+(enable-console-print!)
 
 (defn init
   "Initializes the stream event loop and
@@ -108,5 +110,5 @@
       (rx/subscribe $ (fn [[event model]] (-apply-effect event model))))
 
     ;; Initialize the stream machinary with initial state.
-    (emit! (swap-state (fn [s] (merge s state))))
+    (emit! (swap #(merge % state)))
     state-s))
