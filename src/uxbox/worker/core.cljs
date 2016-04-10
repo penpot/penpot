051163c3ae1e50d6534487d017d452164b26264c
Andrey Antukh <niwi@niwi.nz>
2016-04-10 18:44:12 +0300

    Add webworkers architecture (rcp).

---
+++ src/uxbox/worker/core.cljs
@@ -0,0 +1,31 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; Copyright (c) 2016 Andrey Antukh <niwi@niwi.nz>
+
+(ns uxbox.worker.core
+  (:require [uxbox.util.transit :as t]))
+
+(enable-console-print!)
+
+;; --- Handler
+
+(defmulti handler :cmd)
+
+(defmethod handler :default
+  [message]
+  (println "Unexpected message:" message))
+
+;; --- Helpers
+
+(defn worker?
+  "Check if the code is executed in webworker context."
+  []
+  (undefined? (.-document js/self)))
+
+(defn reply!
+  [sender message]
+  (let [message (assoc message :reply-to sender)]
+    (println "replying " message)
+    (.postMessage js/self (t/encode message))))
