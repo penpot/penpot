73d2a6cfd5d2fc74133ebe2de49a223676241fe1
Andrey Antukh <niwi@niwi.nz>
2016-04-14 21:13:44 +0300

    Add missing copyright/license part on colorpicker ns.

--- src/uxbox/ui/colorpicker.cljs
+++ src/uxbox/ui/colorpicker.cljs
@@ -1,3 +1,9 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; Copyright (c) 2016 Andrey Antukh <niwi@niwi.nz>
+
 (ns uxbox.ui.colorpicker
   (:require [sablono.core :as html :refer-macros [html]]
             [rum.core :as rum]
