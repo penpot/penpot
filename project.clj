82f1da06f80b31969d822c9413cd2adb46c86e2b
Andrey Antukh <niwi@niwi.nz>
2016-05-22 20:46:32 +0300

    Update dependencies.

--- project.clj
+++ project.clj
@@ -11,22 +11,22 @@
 
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [org.clojure/clojurescript "1.8.51" :scope "provided"]
-                 [figwheel-sidecar "0.5.3-1" :scope "test"]
+                 [figwheel-sidecar "0.5.3-2" :scope "test"]
 
                  ;; runtime
                  [com.cognitect/transit-cljs "0.8.237"]
                  [rum "0.8.3"]
-                 [cljsjs/react "15.0.2-0"]
-                 [cljsjs/react-dom "15.0.2-0"]
+                 [cljsjs/react "15.1.0-0"]
+                 [cljsjs/react-dom "15.1.0-0"]
                  [cljsjs/moment "2.10.6-4"]
                  [funcool/struct "0.1.0"]
                  [funcool/lentes "1.0.1"]
                  [funcool/httpurr "0.6.0"]
-                 [funcool/promesa "1.1.1"]
+                 [funcool/promesa "1.2.0"]
                  [funcool/beicon "1.3.0"]
                  [funcool/cuerdas "0.7.2"]
                  [bidi "2.0.9"]]
-  :plugins [[lein-ancient "0.6.7"]]
+  :plugins [[lein-ancient "0.6.10"]]
   :clean-targets ^{:protect false} ["resources/public/js" "target"]
   )
 
