3b675af74a0e0f467fe1202e08b7c634862bf57a
Andrey Antukh <niwi@niwi.nz>
2016-05-28 13:44:52 +0300

    Fix validation form errors rendering.

--- src/uxbox/ui/forms.cljs
+++ src/uxbox/ui/forms.cljs
@@ -5,11 +5,10 @@
 
 (defn input-error
   [errors field]
-  (when-let [errors (get errors field)]
+  (when-let [error (get errors field)]
     (html
      [:ul.form-errors
-      (for [error errors]
-        [:li {:key error} (tr error)])])))
+      [:li {:key error} (tr error)]])))
 
 (defn error-class
   [errors field]
