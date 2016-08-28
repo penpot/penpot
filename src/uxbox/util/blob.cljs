a6ec2cb38b87ac3c408500e4e5d085876baab3d3
Andrey Antukh <niwi@niwi.nz>
2016-08-28 14:52:56 +0300

    Add namespace with helpers for work with html5 blobs.

---
+++ src/uxbox/util/blob.cljs
@@ -0,0 +1,29 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; Copyright (c) 2016 Andrey Antukh <niwi@niwi.nz>
+
+(ns uxbox.util.blob
+  "Helpers for work with HTML5 Blob objects.")
+
+(defn ^boolean blob?
+  [v]
+  (instance? js/Blob v))
+
+(defn create
+  "Create a blob from content."
+  ([content]
+   (create content "application/octet-stream"))
+  ([content mimetype]
+   (js/Blob. #js [content] #js {:type mimetype})))
+
+(defn revoke-uri
+  [url]
+  (js/URL.revokeObjectURL url))
+
+(defn create-uri
+  "Create a url from blob."
+  [b]
+  {:pre [(blob? b)]}
+  (js/URL.createObjectURL b))
