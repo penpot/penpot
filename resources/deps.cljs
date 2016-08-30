ce8987f8b5d83b91f7d5dd518bad793a4ff98751
Andrey Antukh <niwi@niwi.nz>
2016-08-30 18:28:25 +0300

    Add jszip into resources/deps.cljs.

--- resources/deps.cljs
+++ resources/deps.cljs
@@ -1,5 +1,9 @@
 {:foreign-libs
  [{:file "js/snapsvg/snap.svg.js"
    :file-min "js/snapsvg/snap.svg.min.js"
-   :provides ["vendor.snapsvg"]}]
- :externs ["js/snapsvg/externs.js"]}
+   :provides ["vendor.snapsvg"]}
+  {:file "js/jszip/jszip.js"
+   :file-min "js/jszip/jszip.min.js"
+   :provides ["vendor.jszip"]}]
+ :externs ["js/snapsvg/externs.js"
+           "js/jszip/externs.js"]}
