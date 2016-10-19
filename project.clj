217e6bd70bb563eb989d802e1481106e0abeaec7
Andrey Antukh <niwi@niwi.nz>
2016-10-19 23:58:35 +0200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -10,14 +10,14 @@
   :profiles {:dev {:source-paths ["dev"]}}
 
   :dependencies [[org.clojure/clojure "1.9.0-alpha13" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.229" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.293" :scope "provided"]
 
                  ;; Build
                  [figwheel-sidecar "0.5.8" :scope "test"]
 
                  ;; runtime
                  [com.cognitect/transit-cljs "0.8.239"]
-                 [rum "0.10.6"]
+                 [rum "0.10.7"]
                  [cljsjs/react "15.3.1-0"]
                  [cljsjs/react-dom "15.3.1-0"]
                  [cljsjs/react-dom-server "15.3.1-0"]
