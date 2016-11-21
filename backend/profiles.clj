8ec32ad13084adadb29dd655460895acce76850c
Andrey Antukh <niwi@niwi.nz>
2016-11-21 18:12:48 +0100

    Set lower defaults resource requiremens for dev profile.

--- backend/profiles.clj
+++ backend/profiles.clj
@@ -1,8 +1,9 @@
 {:dev
  {:plugins [[lein-ancient "0.6.10"]]
   :dependencies [[clj-http "2.1.0"]]
+  :jvm-opts ^:replace ["-Xms500m" "-Xmx500m" "-XX:+UseG1GC"]
   :main ^:skip-aot uxbox.main}
 
  :prod
- {:jvm-opts ^:replace ["-Xms4g" "-Xmx4g" "-XX:+UseG1GC"
+ {:jvm-opts ^:replace ["-Xms1g" "-Xmx1g" "-XX:+UseG1GC"
                        "-XX:+AggressiveOpts" "-server"]}}
