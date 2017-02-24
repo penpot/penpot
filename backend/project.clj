4efe9ac5a95e95dec967d951d7152bc61bcc4fef
Andrey Antukh <niwi@niwi.nz>
2017-02-24 19:35:34 +0100

    Update dependencies (backend).

--- backend/project.clj
+++ backend/project.clj
@@ -11,12 +11,11 @@
   ;;            "-Djava.rmi.server.hostname=0.0.0.0"]
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [funcool/struct "1.0.0"]
                  [funcool/suricatta "1.3.1"]
-                 [funcool/promesa "1.7.0"]
-                 [funcool/catacumba "2.0.0-SNAPSHOT"]
-                 [funcool/cuerdas "2.0.2"]
-                 [funcool/datoteka "1.0.0-SNAPSHOT"]
+                 [funcool/promesa "1.8.0"]
+                 [funcool/catacumba "2.0.0"]
+                 [funcool/cuerdas "2.0.3"]
+                 [funcool/datoteka "1.0.0"]
 
                  [org.clojure/data.xml "0.1.0-beta2"]
                  [org.jsoup/jsoup "1.10.2"]
@@ -24,22 +23,22 @@
                  [hiccup "1.0.5"]
                  [org.im4java/im4java "1.4.0"]
 
-                 [org.slf4j/slf4j-simple "1.7.22"]
+                 [org.slf4j/slf4j-simple "1.7.23"]
                  [com.layerware/hugsql-core "0.4.7"
                   :exclusions [org.clojure/tools.reader]]
                  [niwinz/migrante "0.1.0"]
 
 
-                 [buddy/buddy-sign "1.4.0" :exclusions [org.clojure/tools.reader]]
+                 [buddy/buddy-sign "1.4.0"]
                  [buddy/buddy-hashers "1.2.0"]
 
                  [org.xerial.snappy/snappy-java "1.1.2.6"]
                  [com.github.spullara.mustache.java/compiler "0.9.4"]
-                 [org.postgresql/postgresql "9.4.1212"]
+                 [org.postgresql/postgresql "42.0.0"]
                  [org.quartz-scheduler/quartz "2.2.3"]
                  [org.quartz-scheduler/quartz-jobs "2.2.3"]
                  [commons-io/commons-io "2.5"]
-                 [com.draines/postal "2.0.2"]
+                 [com.draines/postal "2.0.2" :exclusions [commons-codec]]
 
                  [hikari-cp "1.7.5"]
                  [mount "0.1.11"]
