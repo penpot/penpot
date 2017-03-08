3385ae5a754a85fb3d2e2d74c6d735e9b20954e7
Sean Loughman <sean@studiospring.com.au>
2017-03-09 09:29:15 +1100

    Add text outline to ruler dimensions.
    Signed-off-by: Sean Loughman <sean@studiospring.com.au>

--- frontend/src/uxbox/main/ui/workspace/ruler.cljs
+++ frontend/src/uxbox/main/ui/workspace/ruler.cljs
@@ -32,22 +32,22 @@
                      (mth/precision 2))
         angle (-> (gpt/angle pt center)
                   (mth/precision 2))
-        transform1 (str "translate(" (+ (:x pt) 35) "," (- (:y pt) 10) ")")
-        transform2 (str "translate(" (+ (:x pt) 25) "," (- (:y pt) 30) ")")]
+        translate (-> (str "translate(" (+ (:x pt) 15) "," (- (:y pt) 10) ")"))
+        attrs (-> {:transform translate})
+        tspans (-> (list [:tspan {:x "0"}
+                          (str distance " px")]
+                         [:tspan {:x "0" :y "20"}
+                          (str angle "°")]))]
+
     [:g
-     [:rect {:fill "black"
-             :fill-opacity "0.4"
-             :rx "3"
-             :ry "3"
-             :width "90"
-             :height "50"
-             :transform transform2}]
-     [:text {:transform transform1
-             :fill "white"}
-      [:tspan {:x "0"}
-       (str distance " px")]
-      [:tspan {:x "0" :y "20"}
-       (str angle "°")]]]))
+     [:text
+      (assoc attrs :stroke "white"
+                   :stroke-width "3.4"
+                   :stroke-opacity "0.8")
+      tspans]
+     [:text
+      (assoc attrs :fill "black")
+      tspans]]))
 
 (mx/defc ruler-line
   {:mixins [mx/static]}
