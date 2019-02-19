655a7ad4fedab370f4da793902407a2f80a80c12
mathieu.brunot <mb.mathieu.brunot@gmail.com>
2019-02-19 17:00:16 +0100

    :sparkles: Add more builtin colors collection

--- frontend/src/uxbox/builtins/colors.cljs
+++ frontend/src/uxbox/builtins/colors.cljs
@@ -20,6 +20,7 @@
               "#808080"
               "#696969"
               "#000000"}}
+
    {:name "Silver"
     :id #uuid "00000000-0000-0000-0000-000000000002"
     :type :builtin
@@ -55,7 +56,54 @@
               "#000080"
               "#191970"
               "#8A2BE2"
-              "#4B0082"}}])
+              "#4B0082"}}
+
+   {:name "UXBOX"
+    :id #uuid "00000000-0000-0000-0000-000000000004"
+    :type :builtin
+    :created-at 4
+    :colors #{"#78dbbe"
+              "#b6dd75"
+              "#a599c6"
+              "#e6a16f"
+              "#de4762"
+              "#59b9e2"
+              "#ffffff"
+              "#000000"
+              "#90969d"
+              "#2C2C2C"
+              "#3d3f40"
+              "#181818"
+              "#a9adaf"
+              "#808386"
+              "#4a4e52"
+              "#e0e6e9"
+              "#8d9496"
+              "#4e4f50"
+              "#878c8e"}}
+
+   {:name "Common"
+    :id #uuid "00000000-0000-0000-0000-000000000005"
+    :type :builtin
+    :created-at 5
+    :colors #{"#007bff"
+              "#6610f2"
+              "#6f42c1"
+              "#e83e8c"
+              "#dc3545"
+              "#fd7e14"
+              "#ffc107"
+              "#28a745"
+              "#20c997"
+              "#17a2b8"}}
+
+   {:name "Social"
+    :id #uuid "00000000-0000-0000-0000-000000000006"
+    :type :builtin
+    :created-at 6
+    :colors #{"#365899"
+              "#1b95e0"
+              "#01e675"}}])
 
 (def collections
   (index-by collections-list :id))
