6498c0fe449eff18022d2a1b396c93c436ce13c6
mathieu.brunot <mb.mathieu.brunot@gmail.com>
2019-02-19 21:49:52 +0100

    :bug: Fix icons collection init

--- frontend/src/uxbox/library/icons.cljs
+++ frontend/src/uxbox/library/icons.cljs
@@ -23,79 +23,82 @@
             [uxbox.library.icons.material.toggle :as md-toggle]
             ))
 
-(def +collections+
+(def collections-list
   [{:name "Material design (actions)"
     :id #uuid "00000000-0000-0000-0000-100000000001"
     :created-at 1
-    :type :builtin
+    :builtin true
     :icons md-actions/+icons+}
    {:name "Material design (alerts)"
     :id #uuid "00000000-0000-0000-0000-100000000002"
     :created-at 2
-    :type :builtin
+    :builtin true
     :icons md-alerts/+icons+}
    {:name "Material design (Av)"
     :id #uuid "00000000-0000-0000-0000-100000000003"
     :created-at 3
-    :type :builtin
+    :builtin true
     :icons md-av/+icons+}
    {:name "Material design (Communication)"
     :id #uuid "00000000-0000-0000-0000-100000000004"
     :created-at 4
-    :type :builtin
+    :builtin true
     :icons md-comm/+icons+}
    {:name "Material design (Content)"
     :id #uuid "00000000-0000-0000-0000-100000000005"
     :created-at 5
-    :type :builtin
+    :builtin true
     :icons md-content/+icons+}
    {:name "Material design (Device)"
     :id #uuid "00000000-0000-0000-0000-100000000006"
     :created-at 6
-    :type :builtin
+    :builtin true
     :icons md-device/+icons+}
    {:name "Material design (Editor)"
     :id #uuid "00000000-0000-0000-0000-100000000007"
     :created-at 7
-    :type :builtin
+    :builtin true
     :icons md-editor/+icons+}
    {:name "Material design (File)"
     :id #uuid "00000000-0000-0000-0000-100000000008"
     :created-at 8
-    :type :builtin
+    :builtin true
     :icons md-file/+icons+}
    {:name "Material design (Hardware)"
     :id #uuid "00000000-0000-0000-0000-100000000009"
     :created-at 9
-    :type :builtin
+    :builtin true
     :icons md-hardware/+icons+}
    {:name "Material design (Image)"
     :id #uuid "00000000-0000-0000-0000-1000000000010"
     :created-at 10
-    :type :builtin
+    :builtin true
     :icons md-image/+icons+}
    {:name "Material design (Maps)"
     :id #uuid "00000000-0000-0000-0000-100000000011"
     :created-at 11
-    :type :builtin
+    :builtin true
     :icons md-maps/+icons+}
    {:name "Material design (Navigation)"
     :id #uuid "00000000-0000-0000-0000-100000000012"
     :created-at 12
-    :type :builtin
+    :builtin true
     :icons md-nav/+icons+}
    {:name "Material design (Notifications)"
     :id #uuid "00000000-0000-0000-0000-100000000013"
     :created-at 13
-    :type :builtin
+    :builtin true
     :icons md-not/+icons+}
    {:name "Material design (Social)"
     :id #uuid "00000000-0000-0000-0000-100000000014"
     :created-at 14
-    :type :builtin
+    :builtin true
     :icons md-social/+icons+}
    {:name "Material design (Toggle)"
     :id #uuid "00000000-0000-0000-0000-100000000015"
     :created-at 15
-    :type :builtin
+    :builtin true
     :icons md-toggle/+icons+}])
+
+(def collections
+  (index-by collections-list :id))
