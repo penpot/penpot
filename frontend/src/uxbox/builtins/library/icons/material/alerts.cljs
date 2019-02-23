39161b61c5f82d07e27f44125e01c113d85a8278
mathieu.brunot <mb.mathieu.brunot@gmail.com>
2019-02-23 16:12:29 +0100

    :bug: Fix icons uuid checks

--- frontend/src/uxbox/builtins/library/icons/material/alerts.cljs
+++ frontend/src/uxbox/builtins/library/icons/material/alerts.cljs
@@ -7,14 +7,14 @@
 
 (def +icons+
   [{:name "Error"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path {:xmlns "http://www.w3.org/2000/svg"
                   :d "M24 4c-11.04 0-20 8.95-20 20s8.96 20 20 20 20-8.95 20-20-8.96-20-20-20zm2 30h-4v-4h4v4zm0-8h-4v-12h4v12z"
                   :style {:stroke nil}}]}
    {:name "Warning"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path {:xmlns "http://www.w3.org/2000/svg"
--- frontend/src/uxbox/builtins/library/icons/material/hardware.cljs
+++ frontend/src/uxbox/builtins/library/icons/material/hardware.cljs
@@ -7,7 +7,7 @@
 
 (def +icons+
   [{:name "Cast"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -16,7 +16,7 @@
             "M42 6h-36c-2.21 0-4 1.79-4 4v6h4v-6h36v28h-14v4h14c2.21 0 4-1.79 4-4v-28c0-2.21-1.79-4-4-4zm-40 30v6h6c0-3.31-2.69-6-6-6zm0-8v4c5.52 0 10 4.48 10 10h4c0-7.73-6.27-14-14-14zm0-8v4c9.94 0 18 8.06 18 18h4c0-12.15-9.85-22-22-22z"}]}
 
    {:name "Cast Connected"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -25,7 +25,7 @@
             "M2 36v6h6c0-3.31-2.69-6-6-6zm0-8v4c5.52 0 10 4.48 10 10h4c0-7.73-6.27-14-14-14zm36-14h-28v3.27c7.92 2.56 14.17 8.81 16.73 16.73h11.27v-20zm-36 6v4c9.94 0 18 8.06 18 18h4c0-12.15-9.85-22-22-22zm40-14h-36c-2.21 0-4 1.79-4 4v6h4v-6h36v28h-14v4h14c2.21 0 4-1.79 4-4v-28c0-2.21-1.79-4-4-4z"}]}
 
    {:name "Computer"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -34,7 +34,7 @@
             "M40 36c2.21 0 3.98-1.79 3.98-4l.02-20c0-2.21-1.79-4-4-4h-32c-2.21 0-4 1.79-4 4v20c0 2.21 1.79 4 4 4h-8v4h48v-4h-8zm-32-24h32v20h-32v-20z"}]}
 
    {:name "Desktop Mac"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -43,7 +43,7 @@
             "M42 4h-36c-2.21 0-4 1.79-4 4v24c0 2.21 1.79 4 4 4h14l-4 6v2h16v-2l-4-6h14c2.21 0 4-1.79 4-4v-24c0-2.21-1.79-4-4-4zm0 24h-36v-20h36v20z"}]}
 
    {:name "Desktop Windows"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -52,7 +52,7 @@
             "M42 4h-36c-2.21 0-4 1.79-4 4v24c0 2.21 1.79 4 4 4h14v4h-4v4h16v-4h-4v-4h14c2.21 0 4-1.79 4-4v-24c0-2.21-1.79-4-4-4zm0 28h-36v-24h36v24z"}]}
 
    {:name "Dock"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -61,7 +61,7 @@
             "M16 46h16v-4h-16v4zm16-43.98l-16-.02c-2.21 0-4 1.79-4 4v28c0 2.21 1.79 4 4 4h16c2.21 0 4-1.79 4-4v-28c0-2.21-1.79-3.98-4-3.98zm0 27.98h-16v-20h16v20z"}]}
 
    {:name "Gamepad"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -70,7 +70,7 @@
             "M30 15v-11h-12v11l6 6 6-6zm-15 3h-11v12h11l6-6-6-6zm3 15v11h12v-11l-6-6-6 6zm15-15l-6 6 6 6h11v-12h-11z"}]}
 
    {:name "Headset"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -79,7 +79,7 @@
             "M24 2c-9.94 0-18 8.06-18 18v14c0 3.31 2.69 6 6 6h6v-16h-8v-4c0-7.73 6.27-14 14-14s14 6.27 14 14v4h-8v16h6c3.31 0 6-2.69 6-6v-14c0-9.94-8.06-18-18-18z"}]}
 
    {:name "Headset Mic"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -88,7 +88,7 @@
             "M24 2c-9.94 0-18 8.06-18 18v14c0 3.31 2.69 6 6 6h6v-16h-8v-4c0-7.73 6.27-14 14-14s14 6.27 14 14v4h-8v16h8v2h-14v4h12c3.31 0 6-2.69 6-6v-20c0-9.94-8.06-18-18-18z"}]}
 
    {:name "Keyboard"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -97,7 +97,7 @@
             "M40 10h-32c-2.21 0-3.98 1.79-3.98 4l-.02 20c0 2.21 1.79 4 4 4h32c2.21 0 4-1.79 4-4v-20c0-2.21-1.79-4-4-4zm-18 6h4v4h-4v-4zm0 6h4v4h-4v-4zm-6-6h4v4h-4v-4zm0 6h4v4h-4v-4zm-2 4h-4v-4h4v4zm0-6h-4v-4h4v4zm18 14h-16v-4h16v4zm0-8h-4v-4h4v4zm0-6h-4v-4h4v4zm6 6h-4v-4h4v4zm0-6h-4v-4h4v4z"}]}
 
    {:name "Keyboard Alt"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -106,7 +106,7 @@
             "M31 21c1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3 1.34 3 3 3zm-14 0c1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3 1.34 3 3 3zm7 14c5.22 0 9.65-3.34 11.3-8h-22.6c1.65 4.66 6.08 8 11.3 8zm-.01-32c-11.05 0-19.99 8.95-19.99 20s8.94 20 19.99 20c11.05 0 20.01-8.95 20.01-20s-8.96-20-20.01-20zm.01 36c-8.84 0-16-7.16-16-16s7.16-16 16-16 16 7.16 16 16-7.16 16-16 16z"}]}
 
    {:name "Keyboard Arrow Down"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -114,7 +114,7 @@
             :d "M14.83 16.42l9.17 9.17 9.17-9.17 2.83 2.83-12 12-12-12z"}]}
 
    {:name "Keyboard Arrow Left"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -122,7 +122,7 @@
             :d "M30.83 32.67l-9.17-9.17 9.17-9.17-2.83-2.83-12 12 12 12z"}]}
 
    {:name "Keyboard Arrow Right"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -130,7 +130,7 @@
             :d "M17.17 32.92l9.17-9.17-9.17-9.17 2.83-2.83 12 12-12 12z"}]}
 
    {:name "Keyboard Arrow Up"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -138,7 +138,7 @@
             :d "M14.83 30.83l9.17-9.17 9.17 9.17 2.83-2.83-12-12-12 12z"}]}
 
    {:name "Keyboard Capslock"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -147,7 +147,7 @@
             "M24 16.83l9.17 9.17 2.83-2.83-12-12-12 12 2.83 2.83 9.17-9.17zm-12 19.17h24v-4h-24v4z"}]}
 
    {:name "Keyboard Control"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -156,7 +156,7 @@
             "M12 20c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm24 0c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm-12 0c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4z"}]}
 
    {:name "Keyboard Hide"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -165,7 +165,7 @@
             "M40 6h-32c-2.21 0-3.98 1.79-3.98 4l-.02 20c0 2.21 1.79 4 4 4h32c2.21 0 4-1.79 4-4v-20c0-2.21-1.79-4-4-4zm-18 6h4v4h-4v-4zm0 6h4v4h-4v-4zm-6-6h4v4h-4v-4zm0 6h4v4h-4v-4zm-2 4h-4v-4h4v4zm0-6h-4v-4h4v4zm18 14h-16v-4h16v4zm0-8h-4v-4h4v4zm0-6h-4v-4h4v4zm6 6h-4v-4h4v4zm0-6h-4v-4h4v4zm-14 30l8-8h-16l8 8z"}]}
 
    {:name "Keyboard Return"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -174,7 +174,7 @@
             "M38 14v8h-26.34l7.17-7.17-2.83-2.83-12 12 12 12 2.83-2.83-7.17-7.17h30.34v-12z"}]}
 
    {:name "Keyboard Tab"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -183,7 +183,7 @@
             "M23.17 14.83l7.17 7.17h-28.34v4h28.34l-7.17 7.17 2.83 2.83 12-12-12-12-2.83 2.83zm16.83-2.83v24h4v-24h-4z"}]}
 
    {:name "Keyboard Voice"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -192,7 +192,7 @@
             "M24 30c3.31 0 5.98-2.69 5.98-6l.02-12c0-3.32-2.68-6-6-6-3.31 0-6 2.68-6 6v12c0 3.31 2.69 6 6 6zm10.6-6c0 6-5.07 10.2-10.6 10.2-5.52 0-10.6-4.2-10.6-10.2h-3.4c0 6.83 5.44 12.47 12 13.44v6.56h4v-6.56c6.56-.97 12-6.61 12-13.44h-3.4z"}]}
 
    {:name "Laptop"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -201,7 +201,7 @@
             "M40 36c2.2 0 4-1.8 4-4v-20c0-2.2-1.8-4-4-4h-32c-2.2 0-4 1.8-4 4v20c0 2.2 1.8 4 4 4h-8v4h48v-4h-8zm-32-24h32v20h-32v-20z"}]}
 
    {:name "Laptop Chromebook"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -210,7 +210,7 @@
             "M44 36v-30h-40v30h-4v4h48v-4h-4zm-16 0h-8v-2h8v2zm12-6h-32v-20h32v20z"}]}
 
    {:name "Laptop Mac"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -219,7 +219,7 @@
             "M40 36c2.21 0 3.98-1.79 3.98-4l.02-22c0-2.21-1.79-4-4-4h-32c-2.21 0-4 1.79-4 4v22c0 2.21 1.79 4 4 4h-8c0 2.21 1.79 4 4 4h40c2.21 0 4-1.79 4-4h-8zm-32-26h32v22h-32v-22zm16 28c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"}]}
 
    {:name "Laptop Windows"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -228,7 +228,7 @@
             "M40 36v-2c2.21 0 3.98-1.79 3.98-4l.02-20c0-2.21-1.79-4-4-4h-32c-2.21 0-4 1.79-4 4v20c0 2.21 1.79 4 4 4v2h-8v4h48v-4h-8zm-32-26h32v20h-32v-20z"}]}
 
    {:name "Memory"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -237,7 +237,7 @@
             "M30 18h-12v12h12v-12zm-4 8h-4v-4h4v4zm16-4v-4h-4v-4c0-2.21-1.79-4-4-4h-4v-4h-4v4h-4v-4h-4v4h-4c-2.21 0-4 1.79-4 4v4h-4v4h4v4h-4v4h4v4c0 2.21 1.79 4 4 4h4v4h4v-4h4v4h4v-4h4c2.21 0 4-1.79 4-4v-4h4v-4h-4v-4h4zm-8 12h-20v-20h20v20z"}]}
 
    {:name "Mouse"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -246,7 +246,7 @@
             "M26 2.14v15.86h14c0-8.16-6.11-14.88-14-15.86zm-18 27.86c0 8.84 7.16 16 16 16s16-7.16 16-16v-8h-32v8zm14-27.86c-7.89.98-14 7.7-14 15.86h14v-15.86z"}]}
 
    {:name "Phone android"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -255,7 +255,7 @@
             "M32 2h-16c-3.31 0-6 2.69-6 6v32c0 3.31 2.69 6 6 6h16c3.31 0 6-2.69 6-6v-32c0-3.31-2.69-6-6-6zm-4 40h-8v-2h8v2zm6.5-6h-21v-28h21v28z"}]}
 
    {:name "Phone iPhone"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -264,7 +264,7 @@
             "M31 2h-16c-2.76 0-5 2.24-5 5v34c0 2.76 2.24 5 5 5h16c2.76 0 5-2.24 5-5v-34c0-2.76-2.24-5-5-5zm-8 42c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm9-8h-18v-28h18v28z"}]}
 
    {:name "Phonelink"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -273,7 +273,7 @@
             "M8 12h36v-4h-36c-2.21 0-4 1.79-4 4v22h-4v6h28v-6h-20v-22zm38 4h-12c-1.1 0-2 .9-2 2v20c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-20c0-1.1-.9-2-2-2zm-2 18h-8v-14h8v14z"}]}
 
    {:name "Phonelink Off"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -282,7 +282,7 @@
             "M44 12v-4h-30.36l4 4h26.36zm-40.16-8.71l-2.55 2.55 3.63 3.63c-.56.69-.92 1.57-.92 2.53v22h-4v6h35.46l4.71 4.71 2.54-2.55-34.92-34.92-3.95-3.95zm4.16 9.26l21.46 21.45h-21.46v-21.45zm38 3.45h-12c-1.1 0-2 .9-2 2v8.36l4 4v-10.36h8v14h-4.36l6 6h.36c1.1 0 2-.9 2-2v-20c0-1.1-.9-2-2-2z"}]}
 
    {:name "Security"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -291,7 +291,7 @@
             "M24 2l-18 8v12c0 11.11 7.67 21.47 18 24 10.33-2.53 18-12.89 18-24v-12l-18-8zm0 21.98h14c-1.06 8.24-6.55 15.58-14 17.87v-17.85h-14v-11.4l14-6.22v17.6z"}]}
 
    {:name "Sim Card"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -300,7 +300,7 @@
             "M39.98 8c0-2.21-1.77-4-3.98-4h-16l-12 12v24c0 2.21 1.79 4 4 4h24.02c2.21 0 3.98-1.79 3.98-4l-.02-32zm-21.98 30h-4v-4h4v4zm16 0h-4v-4h4v4zm-16-8h-4v-8h4v8zm8 8h-4v-8h4v8zm0-12h-4v-4h4v4zm8 4h-4v-8h4v8z"}]}
 
    {:name "Smartphone"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -309,7 +309,7 @@
             "M34 2.02l-20-.02c-2.21 0-4 1.79-4 4v36c0 2.21 1.79 4 4 4h20c2.21 0 4-1.79 4-4v-36c0-2.21-1.79-3.98-4-3.98zm0 35.98h-20v-28h20v28z"}]}
 
    {:name "Speaker"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -318,7 +318,7 @@
             "M34 4h-20c-2.21 0-4 1.79-4 4v32c0 2.21 1.79 3.98 4 3.98l20 .02c2.21 0 4-1.79 4-4v-32c0-2.21-1.79-4-4-4zm-10 4c2.21 0 4 1.79 4 4s-1.79 4-4 4-4-1.79-4-4 1.79-4 4-4zm0 32c-5.52 0-10-4.48-10-10s4.48-10 10-10 10 4.48 10 10-4.48 10-10 10zm0-16c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6z"}]}
 
    {:name "Tablet"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -327,7 +327,7 @@
             "M42 8h-36c-2.21 0-4 1.79-4 4v24c0 2.21 1.79 4 4 4h36c2.21 0 3.98-1.79 3.98-4l.02-24c0-2.21-1.79-4-4-4zm-4 28h-28v-24h28v24z"}]}
 
    {:name "Tablet Android"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -336,7 +336,7 @@
             "M36 0h-24c-3.31 0-6 2.69-6 6v36c0 3.31 2.69 6 6 6h24c3.31 0 6-2.69 6-6v-36c0-3.31-2.69-6-6-6zm-8 44h-8v-2h8v2zm10.5-6h-29v-32h29v32z"}]}
 
    {:name "Tablet Mac"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -345,7 +345,7 @@
             "M37 0h-28c-2.76 0-5 2.24-5 5v38c0 2.76 2.24 5 5 5h28c2.76 0 5-2.24 5-5v-38c0-2.76-2.24-5-5-5zm-14 46c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm15-8h-30v-32h30v32z"}]}
 
    {:name "TV"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
@@ -354,7 +354,7 @@
             "M42 6h-36c-2.21 0-4 1.79-4 4v24c0 2.21 1.79 4 4 4h10v4h16v-4h10c2.21 0 3.98-1.79 3.98-4l.02-24c0-2.21-1.79-4-4-4zm0 28h-36v-24h36v24z"}]}
 
    {:name "Clock"
-    :id #uuid (uuid/random)
+    :id (uuid/random)
     :type :builtin
     :view-box [0 0 48 48]
     :data [:path
