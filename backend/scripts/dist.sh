f155f488887cba7e455f88f05f5c4d3ef2aac920
Andrey Antukh <niwi@niwi.nz>
2019-06-05 08:26:33 +0000

    misc: remove obsolete scripts from backend

--- backend/scripts/dist.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-DIR=`dirname $0`
-LEIN="$DIR/lein"
-
-LEIN_SNAPSHOTS_IN_RELEASE=1 $LEIN uberjar
--- backend/scripts/fixtures.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-DIR=`dirname $0`
-LEIN="$DIR/lein"
-
-$LEIN trampoline run -m uxbox.fixtures
