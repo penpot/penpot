a12f04e07a1e0d7e707f69bf0a7585f583cc8042
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-07-19 14:36:23 +0200

    :sparkles: Init DB JDBC URI from base DB params
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -4,5 +4,11 @@ set -e
 echo "Synchronize static data..."
 rsync -avr --delete ./resources/public/static/ ./data/static/
 
+if [ -z "$UXBOX_DATABASE_URI" ]; then
+    echo "Initializing database connection string..."
+    UXBOX_DATABASE_URI="\"jdbc:postgresql://$(echo ${UXBOX_DATABASE_SERVER} | tr -d '"'):${UXBOX_DATABASE_PORT}/$(echo ${UXBOX_DATABASE_NAME} | tr -d '"')\""
+    echo "Database connection string: $UXBOX_DATABASE_URI"
+fi
+
 echo "Setting up UXBOX Backend..."
 exec "$@"
