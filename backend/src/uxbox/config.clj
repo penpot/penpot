e2c664300cfce56127992d0ac61e68fe238ab98c
Andrey Antukh <niwi@niwi.nz>
2019-11-22 18:12:44 +0100

    :bug: Disable email sending by default.

--- backend/src/uxbox/config.clj
+++ backend/src/uxbox/config.clj
@@ -53,7 +53,7 @@
    :smtp-password (lookup-env env :uxbox-smtp-password nil)
    :smtp-tls (lookup-env env :uxbox-smtp-tls false)
    :smtp-ssl (lookup-env env :uxbox-smtp-ssl false)
-   :smtp-enabled (lookup-env env :uxbox-smtp-enabled true)
+   :smtp-enabled (lookup-env env :uxbox-smtp-enabled false)
 
    :registration-enabled (lookup-env env :uxbox-registration-enabled true)
    :secret (lookup-env env :uxbox-secret "5qjiAndGY3")})
