71afd6719f3af0044b0f94e4c43f34aa88f7c879
Andrey Antukh <niwi@niwi.nz>
2020-03-30 10:46:15 +0200

    :construction_worker: Adapt production build script to shadow-cljs.

--- frontend/scripts/build-app.sh
+++ frontend/scripts/build-app.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
 source ~/.bashrc
-export NODE_ENV=production;
-
 set -ex
 
 npm ci
 
+export NODE_ENV=production;
+
 npx gulp dist:clean || exit 1;
 npx gulp dist || exit 1;
 
-cp -r ./target/dist ./target/dist2
-mv ./target/dist2 ./target/dist/dbg
-
-clojure -Adev tools.clj dist:all || exit 1;
+shadow-cljs release main
 
 npx gulp dist:gzip || exit 1;
