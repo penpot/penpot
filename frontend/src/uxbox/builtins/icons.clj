9759cb9fd9bee7fbabe3b5c1bdece94580fca996
Andrey Antukh <niwi@niwi.nz>
2020-03-30 23:57:53 +0200

    :recycle: Don't embedd svg sprite into the index.html file.

--- frontend/src/uxbox/builtins/icons.clj
+++ frontend/src/uxbox/builtins/icons.clj
@@ -2,15 +2,14 @@
 ;; License, v. 2.0. If a copy of the MPL was not distributed with this
 ;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 ;;
-;; Copyright (c) 2015-2017 Andrey Antukh <niwi@niwi.nz>
-;; Copyright (c) 2015-2017 Juan de la Cruz <delacruzgarciajuan@gmail.com>
+;; Copyright (c) 2020 UXBOX Labs SL
 
 (ns uxbox.builtins.icons
   (:require [rumext.alpha]))
 
 (defmacro icon-xref
   [id]
-  (let [href (str "#icon-" (name id))]
+  (let [href (str "/images/svg-sprite/symbol/svg/sprite.symbol.svg#icon-" (name id))]
     `(rumext.alpha/html
       [:svg {:width 500 :height 500}
        [:use {:xlinkHref ~href}]])))
