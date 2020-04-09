d1896801746ba4309e71350ad252ea22bbc01f77
Andrés Moya <andres.moya@kaleidos.net>
2020-04-09 09:57:24 +0200

    :tada: Add space distribution of shapes

--- frontend/src/uxbox/main/ui/workspace/sidebar/align.cljs
+++ frontend/src/uxbox/main/ui/workspace/sidebar/align.cljs
@@ -12,6 +12,7 @@
    [uxbox.main.refs :as refs]
    [uxbox.main.store :as st]
    [uxbox.main.data.workspace :as dw]
+   [uxbox.util.i18n :as i18n :refer [t]]
    [uxbox.util.uuid :as uuid]))
 
 (mf/defc align-options
@@ -25,26 +26,67 @@
                    :else
                      (= uuid/zero (:frame-id (get objects (first selected)))))
 
+        disabled-distribute (cond
+                   (empty? selected) true
+                   (< (count selected) 2) true
+                   :else false)
+
+        locale (i18n/use-locale)
+
         on-align-button-clicked
-        (fn [axis] (when-not disabled (st/emit! (dw/align-objects axis))))]
+        (fn [axis] (when-not disabled (st/emit! (dw/align-objects axis))))
+
+        on-distribute-button-clicked
+        (fn [axis] (when-not disabled-distribute (st/emit! (dw/distribute-objects axis))))]
 
     [:div.align-options
-     [:div.align-button {:class (when disabled "disabled")
-                         :on-click #(on-align-button-clicked :hleft)}
-      i/shape-halign-left]
-     [:div.align-button {:class (when disabled "disabled")
-                         :on-click #(on-align-button-clicked :hcenter)}
-      i/shape-halign-center]
-     [:div.align-button {:class (when disabled "disabled")
-                         :on-click #(on-align-button-clicked :hright)}
-      i/shape-halign-right]
-     [:div.align-button {:class (when disabled "disabled")
-                         :on-click #(on-align-button-clicked :vtop)}
-      i/shape-valign-top]
-     [:div.align-button {:class (when disabled "disabled")
-                         :on-click #(on-align-button-clicked :vcenter)}
-      i/shape-valign-center]
-     [:div.align-button {:class (when disabled "disabled")
-                         :on-click #(on-align-button-clicked :vbottom)}
-      i/shape-valign-bottom]]))
+     [:div.align-group
+       [:div.align-button.tooltip.tooltip-bottom
+        {:alt (t locale "workspace.align.hleft")
+         :class (when disabled "disabled")
+         :on-click #(on-align-button-clicked :hleft)}
+        i/shape-halign-left]
+
+       [:div.align-button.tooltip.tooltip-bottom
+        {:alt (t locale "workspace.align.hcenter")
+         :class (when disabled "disabled")
+         :on-click #(on-align-button-clicked :hcenter)}
+        i/shape-halign-center]
+
+       [:div.align-button.tooltip.tooltip-bottom
+        {:alt (t locale "workspace.align.hright")
+         :class (when disabled "disabled")
+         :on-click #(on-align-button-clicked :hright)}
+        i/shape-halign-right]
+
+       [:div.align-button.tooltip.tooltip-bottom
+        {:alt (t locale "workspace.align.hdistribute")
+         :class (when disabled-distribute "disabled")
+         :on-click #(on-distribute-button-clicked :horizontal)}
+        i/shape-hdistribute]]
+
+     [:div.align-group
+       [:div.align-button.tooltip.tooltip-bottom
+        {:alt (t locale "workspace.align.vtop")
+         :class (when disabled "disabled")
+         :on-click #(on-align-button-clicked :vtop)}
+        i/shape-valign-top]
+
+       [:div.align-button.tooltip.tooltip-bottom
+        {:alt (t locale "workspace.align.vcenter")
+         :class (when disabled "disabled")
+         :on-click #(on-align-button-clicked :vcenter)}
+        i/shape-valign-center]
+
+       [:div.align-button.tooltip.tooltip-bottom
+        {:alt (t locale "workspace.align.vbottom")
+         :class (when disabled "disabled")
+         :on-click #(on-align-button-clicked :vbottom)}
+        i/shape-valign-bottom]
+
+       [:div.align-button.tooltip.tooltip-bottom
+        {:alt (t locale "workspace.align.vdistribute")
+         :class (when disabled-distribute "disabled")
+         :on-click #(on-distribute-button-clicked :vertical)}
+        i/shape-vdistribute]]]))
 
