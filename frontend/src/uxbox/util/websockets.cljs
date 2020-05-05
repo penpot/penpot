7940c43d3bf40833491a5fac03835b0fd76b72ae
Andrey Antukh <niwi@niwi.nz>
2020-05-05 15:31:49 +0200

    :recycle: Move uxbox.main.websockets to uxbox/util directory.

--- frontend/src/uxbox/util/websockets.cljs
+++ frontend/src/uxbox/util/websockets.cljs
@@ -2,9 +2,12 @@
 ;; License, v. 2.0. If a copy of the MPL was not distributed with this
 ;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 ;;
-;; Copyright (c) 2015-2017 Andrey Antukh <niwi@niwi.nz>
+;; This Source Code Form is "Incompatible With Secondary Licenses", as
+;; defined by the Mozilla Public License, v. 2.0.
+;;
+;; Copyright (c) 2020 UXBOX Labs SL
 
-(ns uxbox.main.websockets
+(ns uxbox.util.websockets
   "A interface to webworkers exposed functionality."
   (:require
    [goog.events :as ev]
