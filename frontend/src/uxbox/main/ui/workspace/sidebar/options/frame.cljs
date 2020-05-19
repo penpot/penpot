9681a4b32ed6356ee2401c4bf4ef8460976d9c2a
alonso.torres <alonso.torres@kaleidos.net>
2020-05-19 15:22:39 +0200

    :sparkles: Fixed tab space

--- frontend/src/uxbox/main/ui/workspace/sidebar/options/frame.cljs
+++ frontend/src/uxbox/main/ui/workspace/sidebar/options/frame.cljs
@@ -80,7 +80,7 @@
      [:div.element-set-content
 
       [:div.row-flex
-       [:div.custom-select.flex-grow {:on-click #(reset! show-presets-dropdown? true)}
+       [:div.presets.custom-select.flex-grow {:on-click #(reset! show-presets-dropdown? true)}
         [:span (tr "workspace.options.size-presets")]
         [:span.dropdown-button i/arrow-down]
         [:& dropdown {:show @show-presets-dropdown?
