cc2b6063038b6817f252ecf3e7ef78fbddaa7396
Andrey Antukh <niwi@niwi.nz>
2020-07-01 10:38:18 +0200

    :sparkles: Adapt devenv docker to new exporter app.

--- docker/devenv/files/start-tmux.sh
+++ docker/devenv/files/start-tmux.sh
@@ -8,16 +8,28 @@ pushd ~/uxbox/frontend/
 rm -rf node_modules;
 npm ci;
 popd
+pushd ~/uxbox/exporter/
+rm -rf node_modules;
+npm ci;
+popd
 
 tmux -2 new-session -d -s uxbox
 
 tmux new-window -t uxbox:1 -n 'shadow watch'
 tmux select-window -t uxbox:1
 tmux send-keys -t uxbox 'cd uxbox/frontend' enter C-l
-tmux send-keys -t uxbox 'shadow-cljs watch main' enter
+tmux send-keys -t uxbox 'npx shadow-cljs watch main' enter
 
-tmux new-window -t uxbox:2 -n 'backend'
+tmux new-window -t uxbox:2 -n 'exporter'
 tmux select-window -t uxbox:2
+tmux send-keys -t uxbox 'cd uxbox/exporter' enter C-l
+tmux send-keys -t uxbox 'npx shadow-cljs watch main' enter
+tmux split-window -v
+tmux send-keys -t uxbox 'cd uxbox/exporter' enter C-l
+tmux send-keys -t uxbox './bin/wait-and-start' enter
+
+tmux new-window -t uxbox:3 -n 'backend'
+tmux select-window -t uxbox:3
 tmux send-keys -t uxbox 'cd uxbox/backend' enter C-l
 tmux send-keys -t uxbox './bin/start-dev' enter
 
