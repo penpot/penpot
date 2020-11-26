7f85e54d9a2763d67d12f41cb88f5bdd30b7873c
Andrey Antukh <niwi@niwi.nz>
2020-11-26 09:54:44 +0100

    :bug: Fix wrong undo handling on masking objects.
    On undo a mask operation the created group does not
    disappear; this commit fixes this.

--- frontend/src/app/main/ui/workspace/context_menu.cljs
+++ frontend/src/app/main/ui/workspace/context_menu.cljs
@@ -10,21 +10,21 @@
 (ns app.main.ui.workspace.context-menu
   "A workspace specific context menu (mouse right click)."
   (:require
-   [beicon.core :as rx]
-   [okulary.core :as l]
-   [potok.core :as ptk]
-   [rumext.alpha :as mf]
-   [app.main.store :as st]
-   [app.main.refs :as refs]
-   [app.main.streams :as ms]
-   [app.main.ui.icons :as i]
-   [app.util.dom :as dom]
-   [app.util.i18n :refer [t] :as i18n]
    [app.main.data.workspace :as dw]
    [app.main.data.workspace.common :as dwc]
    [app.main.data.workspace.libraries :as dwl]
+   [app.main.refs :as refs]
+   [app.main.store :as st]
+   [app.main.streams :as ms]
+   [app.main.ui.components.dropdown :refer [dropdown]]
    [app.main.ui.hooks :refer [use-rxsub]]
-   [app.main.ui.components.dropdown :refer [dropdown]]))
+   [app.main.ui.icons :as i]
+   [app.util.dom :as dom]
+   [app.util.i18n :refer [t] :as i18n]
+   [beicon.core :as rx]
+   [okulary.core :as l]
+   [potok.core :as ptk]
+   [rumext.alpha :as mf]))
 
 (def menu-ref
   (l/derived :context-menu refs/workspace-local))
