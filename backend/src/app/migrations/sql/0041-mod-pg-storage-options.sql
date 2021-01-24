5518f561f06b7c004d643e3bfefd39853a8a5134
Andrey Antukh <niwi@niwi.nz>
2021-01-24 16:08:48 +0100

    :sparkles: Make postgresql TOAST storage more friendly to ZFS.

---
+++ backend/src/app/migrations/sql/0041-mod-pg-storage-options.sql
@@ -0,0 +1,50 @@
+ALTER TABLE file
+  ALTER COLUMN data SET STORAGE external,
+  ALTER COLUMN name SET STORAGE external;
+
+ALTER TABLE file_change
+  ALTER COLUMN data SET STORAGE external,
+  ALTER COLUMN changes SET STORAGE external;
+
+ALTER TABLE profile
+  ALTER COLUMN fullname SET STORAGE external,
+  ALTER COLUMN email SET STORAGE external,
+  ALTER COLUMN password SET STORAGE external,
+  ALTER COLUMN lang SET STORAGE external,
+  ALTER COLUMN theme SET STORAGE external,
+  ALTER COLUMN props SET STORAGE external;
+
+ALTER TABLE project
+  ALTER COLUMN name SET STORAGE external;
+
+ALTER TABLE team
+  ALTER COLUMN name SET STORAGE external;
+
+ALTER TABLE comment
+  ALTER COLUMN content SET STORAGE external;
+
+ALTER TABLE comment_thread
+  ALTER COLUMN participants SET STORAGE external,
+  ALTER COLUMN page_name SET STORAGE external;
+
+ALTER TABLE task
+  ALTER COLUMN queue SET STORAGE external,
+  ALTER COLUMN name SET STORAGE external,
+  ALTER COLUMN props SET STORAGE external,
+  ALTER COLUMN status SET STORAGE external,
+  ALTER COLUMN error SET STORAGE external;
+
+ALTER TABLE scheduled_task
+  ALTER COLUMN id SET STORAGE external,
+  ALTER COLUMN cron_expr SET STORAGE external;
+
+ALTER TABLE http_session
+  ALTER COLUMN id SET STORAGE external,
+  ALTER COLUMN user_agent SET STORAGE external;
+
+ALTER TABLE file_share_token
+  ALTER COLUMN token SET STORAGE external;
+
+ALTER TABLE file_media_object
+  ALTER COLUMN name SET STORAGE external,
+  ALTER COLUMN mtype SET STORAGE external;
