1c42ace096c8ba65cc2328beeb72613ddb32bdc3
Andrey Antukh <niwi@niwi.nz>
2021-01-26 16:49:06 +0100

    :bug: Properly capture stack overflow errors.

--- backend/src/app/http.clj
+++ backend/src/app/http.clj
@@ -104,14 +104,14 @@
     (fn [request]
       (try
         (handler request)
-        (catch Exception e
+        (catch Throwable e
           (try
             (let [cdata (errors/get-error-context request e)]
               (errors/update-thread-context! cdata)
               (log/errorf e "Unhandled exception: %s (id: %s)" (ex-message e) (str (:id cdata)))
               {:status 500
                :body "internal server error"})
-            (catch Exception e
+            (catch Throwable e
               (log/errorf e "Unhandled exception: %s" (ex-message e))
               {:status 500
                :body "internal server error"})))))))
