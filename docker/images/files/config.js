d186fdbe4790768ec69fa0fa23bb1f99ef66a43d
mathieu.brunot <mathieu.brunot@monogramm.io>
2021-02-08 13:25:42 +0100

    :whale: Frontend configuration on env var
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

---
+++ docker/images/files/config.js
@@ -0,0 +1,9 @@
+// Frontend configuration
+
+//var penpotPublicURI = "https://penpot.example.com";
+//var penpotDemoWarning = <true|false>;
+//var penpotAllowDemoUsers = <true|false>;
+//var penpotGoogleClientID = "<google-client-id-here>";
+//var penpotGitlabClientID = "<gitlab-client-id-here>";
+//var penpotGithubClientID = "<github-client-id-here>";
+//var penpotLoginWithLDAP = <true|false>;
