9fb8ba2ff1f3deeca1545094f14a61a6ed6e5cb6
Andrey Antukh <niwi@niwi.nz>
2021-03-01 13:16:06 +0100

    :tada: Add better reply-to handling on feedback module.

--- backend/src/app/config.clj
+++ backend/src/app/config.clj
@@ -90,6 +90,7 @@
 (s/def ::error-report-webhook ::us/string)
 (s/def ::feedback-destination ::us/string)
 (s/def ::feedback-enabled ::us/boolean)
+(s/def ::feedback-reply-to ::us/email)
 (s/def ::feedback-token ::us/string)
 (s/def ::github-client-id ::us/string)
 (s/def ::github-client-secret ::us/string)
@@ -163,6 +164,7 @@
                    ::error-report-webhook
                    ::feedback-destination
                    ::feedback-enabled
+                   ::feedback-reply-to
                    ::feedback-token
                    ::github-client-id
                    ::github-client-secret
