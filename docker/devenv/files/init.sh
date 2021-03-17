724738f51badfe6c20caea02b281aa945d6cc8d2
Andrey Antukh <niwi@niwi.nz>
2021-03-17 15:54:50 +0100

    :whale: Minor improvements on devenv docker image.

--- docker/devenv/files/init.sh
+++ docker/devenv/files/init.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+cp /root/.bashrc /home/penpot/.bashrc
+cp /root/.vimrc /home/penpot/.vimrc
+cp /root/.tmux.conf /home/penpot/.tmux.conf
+chown -R penpot:users /home/penpot
+
 set -e
 nginx
 tail -f /dev/null
