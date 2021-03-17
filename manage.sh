a16f4393b93a1c5ef64d7020fe1159de3b883e29
Andrey Antukh <niwi@niwi.nz>
2021-03-17 22:12:29 +0100

    :fire: Remove legacy code from manage.sh.

--- manage.sh
+++ manage.sh
@@ -87,15 +87,11 @@ function build-backend {
 }
 
 function build-app-bundle {
-    local build_enabled=${PENPOT_BUNDLE_BUILD:-"true"};
     local version="$CURRENT_VERSION";
-    local name="penpot-app-$CURRENT_BRANCH";
     local bundle_dir="./bundle-app";
 
-    if [ $build_enabled == "true" ]; then
-        build "frontend";
-        build "backend";
-    fi
+    build "frontend";
+    build "backend";
 
     rm -rf $bundle_dir
     mkdir -p $bundle_dir;
@@ -113,14 +109,10 @@ function build-app-bundle {
 }
 
 function build-exporter-bundle {
-    local build_enabled=${PENPOT_BUNDLE_BUILD:-"true"};
     local version="$CURRENT_VERSION";
-    local name="penpot-exporter-$CURRENT_BRANCH";
     local bundle_dir="./bundle-exporter";
 
-    if [ $build_enabled == "true" ]; then
-        build "exporter";
-    fi
+    build "exporter";
 
     rm -rf $bundle_dir;
     cp -r ./exporter/target $bundle_dir;
@@ -131,81 +123,6 @@ function build-exporter-bundle {
 
     echo $version > $bundle_dir/version.txt
 }
-
-function build-image {
-    local image=$1;
-    local tag=$2;
-    local docker_image="$ORGANIZATION/$image";
-
-    set -x
-    pushd ./docker/images;
-    docker buildx build --platform linux/amd64 -t $docker_image:$tag -f Dockerfile.$image .;
-    popd;
-}
-
-function build-images {
-    local version="$CURRENT_VERSION";
-    local bundle_file="penpot-app-$CURRENT_BRANCH.tar.lz4";
-    local bundle_exporter_file="penpot-exporter-$CURRENT_BRANCH.tar.lz4";
-
-    if [ ! -f $bundle_file ]; then
-        echo "File '$bundle_file' does not exists.";
-        exit 1;
-    fi
-
-    if [ ! -f $bundle_exporter_file ]; then
-        echo "File '$bundle_exporter_file' does not exists.";
-        exit 1;
-    fi
-
-    local bundle_file_path=`readlink -f $bundle_file`;
-    local bundle_exporter_file_path=`readlink -f $bundle_exporter_file`;
-
-    rm -rf ./docker/images/bundle-app;
-    rm -rf ./docker/images/bundle-exporter;
-
-    mkdir -p ./docker/images/bundle-app;
-    mkdir -p ./docker/images/bundle-exporter;
-
-    pushd ./docker/images/bundle-app;
-    tar -I lz4 -xvf $bundle_file_path;
-    popd
-
-    pushd ./docker/images/bundle-exporter;
-    tar -I lz4 -xvf $bundle_exporter_file_path;
-    popd
-
-    build-image "backend" $CURRENT_BRANCH;
-    build-image "frontend" $CURRENT_BRANCH;
-    build-image "exporter" $CURRENT_BRANCH;
-}
-
-# function publish-latest-images {
-#     if [ $CURRENT_BRANCH != "main" ]; then
-#         echo "Latest image can only be build from main branch.";
-#         exit 1;
-#     fi;
-
-#     set -x
-
-#     docker tag $ORGANIZATION/frontend:$CURRENT_BRANCH $ORGANIZATION/frontend:latest;
-#     docker tag $ORGANIZATION/backend:$CURRENT_BRANCH $ORGANIZATION/backend:latest;
-#     docker tag $ORGANIZATION/exporter:$CURRENT_BRANCH $ORGANIZATION/exporter:latest;
-
-#     # docker push $ORGANIZATION/frontend:$CURRENT_VERSION;
-#     # docker push $ORGANIZATION/backend:$CURRENT_VERSION;
-#     # docker push $ORGANIZATION/exporter:$CURRENT_VERSION;
-#     docker push $ORGANIZATION/frontend:latest;
-#     docker push $ORGANIZATION/backend:latest;
-#     docker push $ORGANIZATION/exporter:latest;
-# }
-
-# function publish-snapshot-images {
-#     set -x
-#     docker push $ORGANIZATION/frontend:$CURRENT_BRANCH;
-#     docker push $ORGANIZATION/backend:$CURRENT_BRANCH;
-#     docker push $ORGANIZATION/exporter:$CURRENT_BRANCH;
-# }
 
 function usage {
     echo "PENPOT build & release manager"
