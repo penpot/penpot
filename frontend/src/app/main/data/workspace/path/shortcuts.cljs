14475fdc672925b03490dcb27ef8bdad328e7738
alonso.torres <alonso.torres@kaleidos.net>
2021-04-26 18:30:56 +0200

    :sparkles: Add move path points with keyboard

--- frontend/src/app/main/data/workspace/path/shortcuts.cljs
+++ frontend/src/app/main/data/workspace/path/shortcuts.cljs
@@ -32,61 +32,95 @@
           (rx/empty))))))
 
 (def shortcuts
-  {:move-nodes     {:tooltip "V"
-                    :command "v"
-                    :fn #(st/emit! (drp/change-edit-mode :move))}
+  {:move-nodes      {:tooltip "V"
+                     :command "v"
+                     :fn #(st/emit! (drp/change-edit-mode :move))}
 
-   :draw-nodes     {:tooltip "P"
-                    :command "p"
-                    :fn #(st/emit! (drp/change-edit-mode :draw))}
+   :draw-nodes      {:tooltip "P"
+                     :command "p"
+                     :fn #(st/emit! (drp/change-edit-mode :draw))}
 
-   :add-node       {:tooltip "+"
-                    :command "+"
-                    :fn #(st/emit! (drp/add-node))}
+   :add-node        {:tooltip "+"
+                     :command "+"
+                     :fn #(st/emit! (drp/add-node))}
 
-   :delete-node    {:tooltip (ds/supr)
-                    :command ["del" "backspace"]
-                    :fn #(st/emit! (drp/remove-node))}
+   :delete-node     {:tooltip (ds/supr)
+                     :command ["del" "backspace"]
+                     :fn #(st/emit! (drp/remove-node))}
 
-   :merge-nodes    {:tooltip (ds/meta "J")
-                    :command (ds/c-mod "j")
-                    :fn #(st/emit! (drp/merge-nodes))}
+   :merge-nodes     {:tooltip (ds/meta "J")
+                     :command (ds/c-mod "j")
+                     :fn #(st/emit! (drp/merge-nodes))}
 
-   :join-nodes     {:tooltip "J"
-                    :command "j"
-                    :fn #(st/emit! (drp/join-nodes))}
+   :join-nodes      {:tooltip "J"
+                     :command "j"
+                     :fn #(st/emit! (drp/join-nodes))}
 
-   :separate-nodes {:tooltip "K"
-                    :command "k"
-                    :fn #(st/emit! (drp/separate-nodes))}
+   :separate-nodes  {:tooltip "K"
+                     :command "k"
+                     :fn #(st/emit! (drp/separate-nodes))}
 
-   :make-corner    {:tooltip "B"
-                    :command "b"
-                    :fn #(st/emit! (drp/make-corner))}
+   :make-corner     {:tooltip "B"
+                     :command "b"
+                     :fn #(st/emit! (drp/make-corner))}
 
-   :make-curve     {:tooltip (ds/meta "B")
-                    :command (ds/c-mod "b")
-                    :fn #(st/emit! (drp/make-curve))}
+   :make-curve      {:tooltip (ds/meta "B")
+                     :command (ds/c-mod "b")
+                     :fn #(st/emit! (drp/make-curve))}
 
-   :snap-nodes     {:tooltip (ds/meta "'")
-                    :command (ds/c-mod "'")
-                    :fn #(st/emit! (drp/toggle-snap))}
-   
-   :escape        {:tooltip (ds/esc)
-                   :command "escape"
-                   :fn #(st/emit! (esc-pressed))}
+   :snap-nodes      {:tooltip (ds/meta "'")
+                     :command (ds/c-mod "'")
+                     :fn #(st/emit! (drp/toggle-snap))}
 
-   :start-editing {:tooltip (ds/enter)
-                   :command "enter"
-                   :fn #(st/emit! (dw/start-editing-selected))}
+   :escape          {:tooltip (ds/esc)
+                     :command "escape"
+                     :fn #(st/emit! (esc-pressed))}
 
-   :undo          {:tooltip (ds/meta "Z")
-                   :command (ds/c-mod "z")
-                   :fn #(st/emit! (drp/undo-path))}
+   :start-editing   {:tooltip (ds/enter)
+                     :command "enter"
+                     :fn #(st/emit! (dw/start-editing-selected))}
 
-   :redo          {:tooltip (ds/meta "Y")
-                   :command [(ds/c-mod "shift+z") (ds/c-mod "y")]
-                   :fn #(st/emit! (drp/redo-path))}
+   :undo            {:tooltip (ds/meta "Z")
+                     :command (ds/c-mod "z")
+                     :fn #(st/emit! (drp/undo-path))}
+
+   :redo            {:tooltip (ds/meta "Y")
+                     :command [(ds/c-mod "shift+z") (ds/c-mod "y")]
+                     :fn #(st/emit! (drp/redo-path))}
+
+   ;; Arrow movement
+   :move-fast-up    {:tooltip (ds/shift ds/up-arrow)
+                     :command "shift+up"
+                     :fn #(st/emit! (drp/move-selected :up true))}
+
+   :move-fast-down  {:tooltip (ds/shift ds/down-arrow)
+                     :command "shift+down"
+                     :fn #(st/emit! (drp/move-selected :down true))}
+
+   :move-fast-right {:tooltip (ds/shift ds/right-arrow)
+                     :command "shift+right"
+                     :fn #(st/emit! (drp/move-selected :right true))}
+
+   :move-fast-left  {:tooltip (ds/shift ds/left-arrow)
+                     :command "shift+left"
+                     :fn #(st/emit! (drp/move-selected :left true))}
+
+   :move-unit-up    {:tooltip ds/up-arrow
+                     :command "up"
+                     :fn #(st/emit! (drp/move-selected :up false))}
+
+   :move-unit-down  {:tooltip ds/down-arrow
+                     :command "down"
+                     :fn #(st/emit! (drp/move-selected :down false))}
+
+   :move-unit-left  {:tooltip ds/right-arrow
+                     :command "right"
+                     :fn #(st/emit! (drp/move-selected :right false))}
+
+   :move-unit-right {:tooltip ds/left-arrow
+                     :command "left"
+                     :fn #(st/emit! (drp/move-selected :left false))}
+
    })
 
 (defn get-tooltip [shortcut]
