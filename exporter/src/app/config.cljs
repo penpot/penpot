e94e202cefb05c068d6a6d3dab5cb8587297eb39
Andrey Antukh <niwi@niwi.nz>
2021-05-09 16:42:42 +0200

    :bug: Fix unexpected exception bug on exporter.
    Puppetter bug, fixed upgrading it.

--- exporter/src/app/config.cljs
+++ exporter/src/app/config.cljs
@@ -7,6 +7,7 @@
 (ns app.config
   (:refer-clojure :exclude [get])
   (:require
+   [app.common.data :as d]
    ["process" :as process]
    [cljs.pprint]
    [cuerdas.core :as str]
@@ -35,20 +36,27 @@
   [prefix]
   (let [env    (unchecked-get process "env")
         kwd    (fn [s] (-> (str/kebab s) (str/keyword)))
-        prefix (str prefix "-")
+        prefix (str prefix "_")
         len    (count prefix)]
     (reduce (fn [res key]
-              (cond-> res
-                (str/starts-with? key prefix)
-                (assoc (kwd (subs key len))
-                       (unchecked-get env key))))
+              (let [val (unchecked-get env key)
+                    key (str/lower key)]
+                (cond-> res
+                  (str/starts-with? key prefix)
+                  (assoc (kwd (subs key len)) val))))
             {}
             (js/Object.keys env))))
 
+(defn- prepare-config
+  []
+  (let [env  (read-env "penpot")
+        env  (d/without-nils env)
+        data (merge defaults env)]
+    (us/conform ::config data)))
+
 (def config
-  (atom (->> (read-env "penpot")
-             (merge defaults)
-             (us/conform ::config))))
+  (atom (prepare-config)))
+
 
 (defn get
   "A configuration getter."
