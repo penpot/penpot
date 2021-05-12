9a8eca5218eaa46ace9dead4e852314800a9ecaf
Andrey Antukh <niwi@niwi.nz>
2021-05-12 10:21:53 +0200

    :recycle: Improves shortcuts lifecycle management.

--- frontend/src/app/main/data/shortcuts.cljs
+++ frontend/src/app/main/data/shortcuts.cljs
@@ -5,13 +5,21 @@
 ;; Copyright (c) UXBOX Labs SL
 
 (ns app.main.data.shortcuts
+  (:refer-clojure :exclude [meta reset!])
   (:require
    ["mousetrap" :as mousetrap]
+   [app.common.data :as d]
+   [app.common.spec :as us]
    [app.config :as cfg]
-   [app.util.logging :as log])
-  (:refer-clojure :exclude [meta]))
+   [app.util.logging :as log]
+   [cljs.spec.alpha :as s]
+   [potok.core :as ptk]))
 
 (log/set-level! :warn)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Helpers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def mac-command "\u2318")
 (def mac-option  "\u2325")
@@ -44,30 +52,8 @@
   [shortcut]
   (c-mod (a-mod shortcut)))
 
-(defn bind-shortcuts
-  ([shortcuts-config]
-   (bind-shortcuts
-    shortcuts-config
-    mousetrap/bind
-    (fn [key cb]
-      (fn [event]
-        (log/debug :msg (str "Shortcut" key))
-        (.preventDefault event)
-        (cb event)))))
-
-  ([shortcuts-config bind-fn cb-fn]
-   (doseq [[key {:keys [command disabled fn type]}] shortcuts-config]
-     (when-not disabled
-       (if (vector? command)
-         (doseq [cmd (seq command)]
-           (bind-fn cmd (cb-fn key fn) type))
-         (bind-fn command (cb-fn key fn) type))))))
-
-(defn remove-shortcuts
-    []
-    (mousetrap/reset))
-
-(defn meta [key]
+(defn meta
+  [key]
   ;; If the key is "+" we need to surround with quotes
   ;; otherwise will not be very readable
   (let [key (if (and (not (cfg/check-platform? :macos))
@@ -80,37 +66,120 @@
        "Ctrl+")
      key)))
 
-(defn shift [key]
+(defn shift
+  [key]
   (str
    (if (cfg/check-platform? :macos)
      mac-shift
      "Shift+")
    key))
 
-(defn alt [key]
+(defn alt
+  [key]
   (str
    (if (cfg/check-platform? :macos)
      mac-option
      "Alt+")
    key))
 
-(defn meta-shift [key]
+(defn meta-shift
+  [key]
   (-> key meta shift))
 
-(defn meta-alt [key]
+(defn meta-alt
+  [key]
   (-> key meta alt))
 
-(defn supr []
+(defn supr
+  []
   (if (cfg/check-platform? :macos)
     mac-delete
     "Supr"))
 
-(defn esc []
+(defn esc
+  []
   (if (cfg/check-platform? :macos)
     mac-esc
     "Escape"))
 
-(defn enter []
+(defn enter
+  []
   (if (cfg/check-platform? :macos)
     mac-enter
     "Enter"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Events
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; --- EVENT: push
+
+(s/def ::tooltip ::us/string)
+(s/def ::fn fn?)
+
+(s/def ::command
+  (s/or :str ::us/string
+        :vec vector?))
+
+(s/def ::shortcut
+  (s/keys :req-un [::command]
+          :opt-un [::fn
+                   ::tooltip]))
+
+(s/def ::shortcuts
+  (s/map-of ::us/keyword
+            ::shortcut))
+
+(defn- wrap-cb
+  [key cb]
+  (fn [event]
+    (log/debug :msg (str "Shortcut" key))
+    (.preventDefault event)
+    (cb event)))
+
+(defn- bind!
+  [shortcuts]
+  (->> shortcuts
+       (remove #(:disabled (second %)))
+       (run! (fn [[key {:keys [command fn type]}]]
+               (if (vector? command)
+                 (run! #(mousetrap/bind % (wrap-cb key fn) type) command)
+                 (mousetrap/bind command (wrap-cb key fn) type))))))
+
+(defn- reset!
+  ([]
+   (mousetrap/reset))
+  ([shortcuts]
+   (mousetrap/reset)
+   (bind! shortcuts)))
+
+(defn push-shortcuts
+  [key shortcuts]
+  (us/assert ::us/keyword key)
+  (us/assert ::shortcuts shortcuts)
+  (ptk/reify ::push-shortcuts
+    ptk/UpdateEvent
+    (update [_ state]
+      (-> state
+          (update :shortcuts (fnil conj '()) [key shortcuts])))
+
+    ptk/EffectEvent
+    (effect [_ state stream]
+      (let [[key shortcuts] (peek (:shortcuts state))]
+        (reset! shortcuts)))))
+
+(defn pop-shortcuts
+  [key]
+  (ptk/reify ::pop-shortcuts
+    ptk/UpdateEvent
+    (update [_ state]
+      (update state :shortcuts (fn [shortcuts]
+                                 (let [current-key (first (peek shortcuts))]
+                                   (if (= key current-key)
+                                     (pop shortcuts)
+                                     shortcuts)))))
+    ptk/EffectEvent
+    (effect [_ state stream]
+      (let [[key* shortcuts] (peek (:shortcuts state))]
+        (when (not= key key*)
+          (reset! shortcuts))))))
