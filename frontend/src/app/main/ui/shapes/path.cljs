a81b6db09355418dd1bda8ea56bed9d25e360d45
alonso.torres <alonso.torres@kaleidos.net>
2022-02-08 12:30:52 +0100

    :bug: Fix problem with svg icons

--- frontend/src/app/main/ui/shapes/path.cljs
+++ frontend/src/app/main/ui/shapes/path.cljs
@@ -7,7 +7,6 @@
 (ns app.main.ui.shapes.path
   (:require
    [app.common.logging :as log]
-   [app.main.ui.context :as muc]
    [app.main.ui.shapes.attrs :as attrs]
    [app.main.ui.shapes.custom-stroke :refer [shape-custom-stroke]]
    [app.util.object :as obj]
@@ -19,10 +18,7 @@
 (mf/defc path-shape
   {::mf/wrap-props false}
   [props]
-  (let [shape   (-> (unchecked-get props "shape")
-                    (dissoc :fill-color)
-                    (dissoc :fill-opacity)
-                    (dissoc :fill-image))
+  (let [shape   (unchecked-get props "shape")
         content (:content shape)
         pdata   (mf/with-memo [content]
                   (try
@@ -34,8 +30,6 @@
                                  :cause e)
                        "")))
 
-        render-id  (mf/use-ctx muc/render-ctx)
-        shape (assoc shape :fill-image (str "fill-image-" render-id))
         props   (-> (attrs/extract-style-attrs shape)
                     (obj/set! "d" pdata))]
 
