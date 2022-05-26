d5df4659924ee5dc5e05e2c9e08f46735f417d5a
Andrés Moya <andres.moya@kaleidos.net>
2022-05-26 12:41:28 +0200

    :globe_with_meridians: Add Norwegian, Persian and Chinese (Traditional)

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -44,7 +44,7 @@ marked.use({renderer});
 // Templates
 
 function readLocales() {
-  const langs = ["ar", "ca", "de", "el", "en", "es", "fr", "he", "pl", "pt_BR", "ro", "ru", "tr", "zh_CN"];
+  const langs = ["ar", "ca", "de", "el", "en", "es", "fa", "fr", "he", "nb_NO", "pl", "pt_BR", "ro", "ru", "tr", "zh_CN", "zh_Hant"];
   const result = {};
 
   for (let lang of langs) {
