e7fa5fabe5b5e18d0d887e9e5aa27cc654510a3f
Pablo Alba <pablo.alba@kaleidos.net>
2022-06-23 15:22:42 +0200

    :bug: Fix redirect url

--- frontend/src/app/main/errors.cljs
+++ frontend/src/app/main/errors.cljs
@@ -21,6 +21,7 @@
    [app.util.router :as rt]
    [app.util.storage :refer [storage]]
    [app.util.timers :as ts]
+   [cuerdas.core :as str]
    [potok.core :as ptk]))
 
 (defn on-error
@@ -54,7 +55,8 @@
         uri (. (. js/document -location) -href)]
     (st/emit! (du/logout {:capture-redirect true}))
     (ts/schedule 500 #(st/emit! (msg/warn msg)))
-    (ts/schedule 1000 #(swap! storage assoc :redirect-url uri))))
+    (when (or (str/includes? uri "workspace") (str/includes? uri "dashboard"))
+      (ts/schedule 1000 #(swap! storage assoc :redirect-url uri)))))
 
 ;; Error that happens on an active business model validation does not
 ;; passes an validation (example: profile can't leave a team). From
