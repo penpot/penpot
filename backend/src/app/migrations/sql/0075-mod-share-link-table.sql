d021ac02265db23cd1af7674895056e73d07c5f3
Andrey Antukh <niwi@niwi.nz>
2022-06-29 12:30:17 +0200

    :bug: Fix share link migration for backward compatibilty

--- backend/src/app/migrations/sql/0075-mod-share-link-table.sql
+++ backend/src/app/migrations/sql/0075-mod-share-link-table.sql
@@ -1,4 +1,5 @@
 ALTER TABLE share_link
   ADD COLUMN who_comment text NOT NULL DEFAULT('team'),
-  ADD COLUMN who_inspect text NOT NULL DEFAULT('team'),
-  DROP COLUMN flags;
+  ADD COLUMN who_inspect text NOT NULL DEFAULT('team');
+
+--- TODO: remove flags column in 1.15.x
