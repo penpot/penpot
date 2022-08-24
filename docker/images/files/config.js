8ce8b3fdefbce19a96754beb1013b75948e3d0d2
Andrey Antukh <niwi@niwi.nz>
2022-08-24 10:59:56 +0200

    :paperclip: Update docker images related files

--- docker/images/files/config.js
+++ docker/images/files/config.js
@@ -1,13 +1,2 @@
 // Frontend configuration
-
-//var penpotPublicURI = "https://penpot.example.com";
-//var penpotDemoWarning = <true|false>;
-//var penpotAllowDemoUsers = <true|false>;
-//var penpotGoogleClientID = "<google-client-id-here>";
-//var penpotGitlabClientID = "<gitlab-client-id-here>";
-//var penpotGithubClientID = "<github-client-id-here>";
-//var penpotOIDCClientID = "<oidc-client-id-here>";
-//var penpotLoginWithLDAP = <true|false>;
-//var penpotRegistrationEnabled = <true|false>;
-//var penpotAnalyticsEnabled = <true|false>;
 //var penpotFlags = "";
