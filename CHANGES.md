c54354f143911581605f4a9cd1e29a9e318e374a
Pablo Alba <pablo.alba@kaleidos.net>
2022-08-30 17:51:41 +0200

    :bug: Fix opening profile URL while signed out takes to "your account" section

--- CHANGES.md
+++ CHANGES.md
@@ -11,6 +11,7 @@
 - Fix validation error on text position [Taiga #4010](https://tree.taiga.io/project/penpot/issue/4010)
 - Fix objects jitter while scrolling [Github #2167](https://github.com/penpot/penpot/issues/2167)
 - Fix on color-picker, click+drag adds lots of recent colors [Taiga #4013](https://tree.taiga.io/project/penpot/issue/4013)
+- Fix opening profile URL while signed out takes to "your account" section[Taiga #3976](https://tree.taiga.io/project/penpot/issue/3976)
 
 ## 1.15.0-beta
 
--- frontend/src/app/main/ui/settings.cljs
+++ frontend/src/app/main/ui/settings.cljs
@@ -7,14 +7,16 @@
 (ns app.main.ui.settings
   (:require
    [app.main.refs :as refs]
+   [app.main.store :as st]
    [app.main.ui.settings.change-email]
    [app.main.ui.settings.delete-account]
    [app.main.ui.settings.feedback :refer [feedback-page]]
    [app.main.ui.settings.options :refer [options-page]]
    [app.main.ui.settings.password :refer [password-page]]
    [app.main.ui.settings.profile :refer [profile-page]]
-   [app.main.ui.settings.sidebar :refer [sidebar]]
-   [app.util.i18n :as i18n :refer [tr]]
+   [app.main.ui.settings.sidebar :refer [sidebar]]      
+   [app.util.i18n :as i18n :refer [tr]]   
+   [app.util.router :as rt]
    [rumext.alpha :as mf]))
 
 (mf/defc header
@@ -29,6 +31,10 @@
   (let [section (get-in route [:data :name])
         profile (mf/deref refs/profile)
         locale  (mf/deref i18n/locale)]
+    (mf/use-effect
+     #(when (nil? profile)
+        (st/emit! (rt/nav :auth-login))))
+     
     [:section.dashboard-layout
      [:& sidebar {:profile profile
                   :locale locale
