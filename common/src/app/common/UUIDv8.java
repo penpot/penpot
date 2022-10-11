2d80cc3edeaa528603eea7a3224914c8f1870649
Andrey Antukh <niwi@niwi.nz>
2022-10-11 15:03:37 +0200

    :paperclip: Set correct license holder on common module sources

--- common/src/app/common/UUIDv8.java
+++ common/src/app/common/UUIDv8.java
@@ -3,7 +3,7 @@
   License, v. 2.0. If a copy of the MPL was not distributed with this
   file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-  Copyright (c) UXBOX Labs SL
+  Copyright (c) KALEIDOS INC
 
   This file contains a UUIDv8 with conformance with
   https://datatracker.ietf.org/doc/html/draft-peabody-dispatch-new-uuid-format
