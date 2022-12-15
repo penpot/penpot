02f29ed4d0a2a7d1c94430de6c29c21c2c3521ad
Andrey Antukh <niwi@niwi.nz>
2022-12-15 08:18:29 +0100

    :tada: Add webhooks to the API doc

--- backend/src/app/rpc/doc.clj
+++ backend/src/app/rpc/doc.clj
@@ -9,6 +9,7 @@
   (:require
    [app.common.data :as d]
    [app.config :as cf]
+   [app.loggers.webhooks :as-alias webhooks]
    [app.rpc :as-alias rpc]
    [app.util.services :as sv]
    [app.util.template :as tmpl]
@@ -35,6 +36,7 @@
                :name (d/name name)
                :module (-> (:ns mdata) (str/split ".") last)
                :auth (:auth mdata true)
+               :webhook (::webhooks/event? mdata false)
                :docs (::sv/docstring mdata)
                :deprecated (::deprecated mdata)
                :added (::added mdata)
@@ -51,6 +53,7 @@
      (->> (:queries methods)
           (map (partial gen-doc :query))
           (sort-by (juxt :module :name)))
+
      :mutation-methods
      (->> (:mutations methods)
           (map (partial gen-doc :query))
