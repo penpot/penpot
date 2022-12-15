4b26b6fc0239a43b84bc70603bebaef606fd2049
Andrey Antukh <niwi@niwi.nz>
2022-12-15 12:26:41 +0100

    :bug: Fix incorrect output on webhook internal queries

--- backend/src/app/loggers/webhooks.clj
+++ backend/src/app/loggers/webhooks.clj
@@ -25,11 +25,11 @@
 
 (defn- lookup-webhooks-by-team
   [pool team-id]
-  (db/exec! pool ["select * from webhook where team_id=? and is_active=true" team-id]))
+  (db/exec! pool ["select w.* from webhook as w where team_id=? and is_active=true" team-id]))
 
 (defn- lookup-webhooks-by-project
   [pool project-id]
-  (let [sql [(str "select * from webhook as w"
+  (let [sql [(str "select w.* from webhook as w"
                   "  join project as p on (p.team_id = w.team_id)"
                   " where p.id = ? and w.is_active = true")
              project-id]]
@@ -37,7 +37,7 @@
 
 (defn- lookup-webhooks-by-file
   [pool file-id]
-  (let [sql [(str "select * from webhook as w"
+  (let [sql [(str "select w.* from webhook as w"
                   "  join project as p on (p.team_id = w.team_id)"
                   "  join file as f on (f.project_id = p.id)"
                   " where f.id = ? and w.is_active = true")
