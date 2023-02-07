1c4dcf1574c19cb903a3ccaebc29db3a91407cf9
Andrey Antukh <niwi@niwi.nz>
2023-02-07 15:06:35 +0100

    :sparkles: Add minor improvements to logging on docker images

--- backend/scripts/run.template.sh
+++ backend/scripts/run.template.sh
@@ -18,5 +18,7 @@ if [ -f ./environ ]; then
     source ./environ
 fi
 
+export JVM_OPTS="-Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager -Dlog4j2.configurationFile=log4j2.xml -XX:-OmitStackTraceInFastThrow $JVM_OPTS"
+
 set -x
 exec $JAVA_CMD $JVM_OPTS "$@" -jar penpot.jar -m app.main
