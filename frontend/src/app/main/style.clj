94a98a1866557d6c62334fae18c70410b8831e3d
alonso.torres <alonso.torres@kaleidos.net>
2023-03-06 14:13:21 +0100

    :sparkles: Adds CSS modules to the build pipeline

---
+++ frontend/src/app/main/style.clj
@@ -0,0 +1,21 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; Copyright (c) KALEIDOS INC
+
+(ns app.main.style
+  "A fonts loading macros."
+  (:require
+   [app.common.data :as d]
+   [clojure.data.json :as json]))
+
+(defmacro css
+  [selector]
+  (let [;; Get the associated styles will be module.cljs => module.css.json
+        filename    (:file (meta *ns*))
+        styles-file (str "./src/" (subs filename 0 (- (count filename) 4)) "css.json")
+        data        (-> (slurp styles-file)
+                        (json/read-str))
+        result (get data (d/name selector))]
+    `~result))
