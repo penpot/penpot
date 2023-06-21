e8dde477a576199aa862fc1500de6fd7f95b657c
Andrés Moya <andres.moya@kaleidos.net>
2023-06-21 17:04:11 +0200

    :bug: Fix restore remote component

--- frontend/src/app/main/data/workspace/libraries.cljs
+++ frontend/src/app/main/data/workspace/libraries.cljs
@@ -453,9 +453,9 @@
   (ptk/reify ::restore-component
     ptk/WatchEvent
     (watch [it state _]
-      (let [page-id         (:current-page-id state)
-            current-page    (dm/get-in state [:workspace-data :pages-index page-id])
-            objects         (wsh/lookup-page-objects state page-id)
+      (let [page-id      (:current-page-id state)
+            current-page (dm/get-in state [:workspace-data :pages-index page-id])
+            objects      (wsh/lookup-page-objects state page-id)
             library-data (wsh/get-file state library-id)
             {:keys [changes shape]} (dwlh/prepare-restore-component library-data component-id current-page it)
             parent-id (:parent-id shape)
@@ -464,14 +464,13 @@
                       (update-in [parent-id :shapes]
                                  #(conj % (:id shape))))
 
-
             ;; Adds a resize-parents operation so the groups are updated. We add all the new objects
             new-objects-ids (->> changes :redo-changes (filter #(= (:type %) :add-obj)) (mapv :id))
             changes (-> changes
                         (pcb/with-objects objects)
                         (pcb/resize-parents new-objects-ids))]
-        (rx/of (dch/commit-changes changes))))))
 
+        (rx/of (dch/commit-changes (assoc changes :file-id library-id)))))))
 
 (defn instantiate-component
   "Create a new shape in the current page, from the component with the given id
@@ -480,7 +479,6 @@
   (dm/assert! (uuid? file-id))
   (dm/assert! (uuid? component-id))
   (dm/assert! (gpt/point? position))
-
   (ptk/reify ::instantiate-component
     ptk/WatchEvent
     (watch [it state _]
