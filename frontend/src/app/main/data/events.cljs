9b9c5822d123979a26abf977bdd41d16a5f6c4cc
Andrey Antukh <niwi@niwi.nz>
2023-07-20 10:56:57 +0200

    :paperclip: Add minor improvement to events ns error logging

--- frontend/src/app/main/data/events.cljs
+++ frontend/src/app/main/data/events.cljs
@@ -222,9 +222,7 @@
                   :body (http/transit-data {:events events})}]
       (->> (http/send! params)
            (rx/mapcat rp/handle-response)
-           (rx/catch (fn [_]
-                       (l/error :hint "unexpected error on persisting audit events")
-                       (rx/of nil)))))
+           (rx/catch (fn [_] (rx/of nil)))))
 
     (rx/of nil)))
 
