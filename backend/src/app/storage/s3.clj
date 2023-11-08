f370f28ca62590e4e23a96c8be8cd9be07d95b68
Andrey Antukh <niwi@niwi.nz>
2023-11-08 13:33:19 +0100

    :sparkles: Add improvements to the tmp file deletion scheduling

--- backend/src/app/storage/s3.clj
+++ backend/src/app/storage/s3.clj
@@ -313,7 +313,7 @@
     ;; to the filesystem and then read with buffered inputstream; if
     ;; not, read the contento into memory using bytearrays.
     (if (> ^long size (* 1024 1024 2))
-      (let [path (tmp/tempfile :prefix "penpot.storage.s3.")
+      (let [path (tmp/tempfile :prefix "penpot.storage.s3." :min-age "6h")
             rxf  (AsyncResponseTransformer/toFile ^Path path)]
         (->> (.getObject ^S3AsyncClient client
                          ^GetObjectRequest gor
