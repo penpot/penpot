ac04d707eb17ea283a902c6bd10e6ff571325efe
Eva <eva.marco@kaleidos.net>
2024-01-15 13:59:55 +0100

    :bug: Fix typography dropdown menus being clipped out in the left sidebar
    Co-authored-by: Belén Albeza <belen@hey.com>

--- frontend/src/app/main/ui/workspace/sidebar/assets.cljs
+++ frontend/src/app/main/ui/workspace/sidebar/assets.cljs
@@ -153,7 +153,7 @@
                                    :option-handler on-section-filter-change
                                    :data-test      "typographies"}]))]
 
-    [:div  {:class (stl/css :assets-bar)}
+    [:article  {:class (stl/css :assets-bar)}
      [:div {:class (stl/css :assets-header)}
       (when-not ^boolean read-only?
         [:button {:class (stl/css :libraries-button)
@@ -190,6 +190,7 @@
      [:& (mf/provider cmm/assets-filters) {:value filters}
       [:& (mf/provider cmm/assets-toggle-ordering) {:value toggle-ordering}
        [:& (mf/provider cmm/assets-toggle-list-style) {:value toggle-list-style}
-        [:div {:class (stl/css :libraries-wrapper)}
+        ;; [:div {:class (stl/css :libraries-wrapper)}
+        [:*
          [:& assets-local-library {:filters filters}]
          [:& assets-libraries {:filters filters}]]]]]]))
