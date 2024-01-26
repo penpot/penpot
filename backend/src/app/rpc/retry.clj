dabb9d0a82b8b5daa4d348d62bd56f36c89ae8c9
Andrey Antukh <niwi@niwi.nz>
2024-01-26 15:09:01 +0100

    :sparkles: Improve internal API of retry mechanism

--- backend/src/app/rpc/retry.clj
+++ backend/src/app/rpc/retry.clj
@@ -6,8 +6,8 @@
 
 (ns app.rpc.retry
   (:require
+   [app.common.exceptions :as ex]
    [app.common.logging :as l]
-   [app.db :as db]
    [app.util.services :as sv])
   (:import
    org.postgresql.util.PSQLException))
@@ -15,11 +15,28 @@
 (defn conflict-exception?
   "Check if exception matches a insertion conflict on postgresql."
   [e]
-  (and (instance? PSQLException e)
-       (= "23505" (.getSQLState ^PSQLException e))))
+  (when-let [cause (ex/instance? PSQLException e)]
+    (= "23505" (.getSQLState ^PSQLException cause))))
 
 (def ^:private always-false
   (constantly false))
+
+(defn invoke!
+  [{:keys [::max-retries] :or {max-retries 3} :as cfg} f & args]
+  (loop [rnum 1]
+    (let [match? (get cfg ::when always-false)
+          result (try
+                   (apply f cfg args)
+                   (catch Throwable cause
+                     (if (and (match? cause) (<= rnum max-retries))
+                       ::retry
+                       (throw cause))))]
+      (if (= ::retry result)
+        (let [label (get cfg ::label "anonymous")]
+          (l/warn :hint "retrying operation" :label label :retry rnum)
+          (recur (inc rnum)))
+        result))))
+
 
 (defn wrap-retry
   [_ f {:keys [::sv/name] :as mdata}]
@@ -29,36 +46,10 @@
           matches?    (get mdata ::when always-false)]
       (l/dbg :hint "wrapping retry" :name name :max-retries max-retries)
       (fn [cfg params]
-        ((fn recursive-invoke [retry]
-           (try
-             (f cfg params)
-             (catch Throwable cause
-               (if (matches? cause)
-                 (let [current-retry (inc retry)]
-                   (l/wrn :hint "retrying operation" :retry current-retry :service name)
-                   (if (<= current-retry max-retries)
-                     (recursive-invoke current-retry)
-                     (throw cause)))
-                 (throw cause))))) 1)))
+        (-> cfg
+            (assoc ::max-retries max-retries)
+            (assoc ::when matches?)
+            (assoc ::label name)
+            (invoke! f params))))
     f))
 
-(defn invoke
-  [{:keys [::db/conn ::max-retries] :or {max-retries 3} :as cfg} f & args]
-  (assert (db/connection? conn) "invalid database connection")
-  (loop [rnum 1]
-    (let [match? (get cfg ::when always-false)
-          result (let [spoint (db/savepoint conn)]
-                   (try
-                     (let [result (apply f cfg args)]
-                       (db/release! conn spoint)
-                       result)
-                     (catch Throwable cause
-                       (db/rollback! conn spoint)
-                       (if (and (match? cause) (<= rnum max-retries))
-                         ::retry
-                         (throw cause)))))]
-      (if (= ::retry result)
-        (let [label (get cfg ::label "anonymous")]
-          (l/warn :hint "retrying operation" :label label :retry rnum)
-          (recur (inc rnum)))
-        result))))
