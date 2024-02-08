a6d304b0f64dce026fd1a8c529733105c17ff910
Andrey Antukh <niwi@niwi.nz>
2024-02-08 15:21:31 +0100

    :sparkles: Improve snapshot related internal API
    This commit also adds the ability to take snapshot of all files
    of the team in a single run/transaction.

--- backend/src/app/rpc/commands/files_snapshot.clj
+++ backend/src/app/rpc/commands/files_snapshot.clj
@@ -12,14 +12,17 @@
    [app.common.uuid :as uuid]
    [app.config :as cf]
    [app.db :as db]
+   [app.db.sql :as-alias sql]
    [app.main :as-alias main]
    [app.media :as media]
    [app.rpc :as-alias rpc]
+   [app.rpc.commands.files :as files]
    [app.rpc.commands.profile :as profile]
    [app.rpc.doc :as-alias doc]
    [app.storage :as sto]
    [app.util.services :as sv]
-   [app.util.time :as dt]))
+   [app.util.time :as dt]
+   [cuerdas.core :as str]))
 
 (defn check-authorized!
   [{:keys [::db/pool]} profile-id]
@@ -57,76 +60,119 @@
    ::sm/params schema:get-file-snapshots}
   [cfg {:keys [::rpc/profile-id] :as params}]
   (check-authorized! cfg profile-id)
-  (db/run! cfg #(get-file-snapshots % params)))
+  (db/run! cfg get-file-snapshots params))
 
 (defn restore-file-snapshot!
   [{:keys [::db/conn ::sto/storage] :as cfg} {:keys [file-id id]}]
   (let [storage  (media/configure-assets-storage storage conn)
-        params   {:id id :file-id file-id}
-        options  {:columns [:id :data :revn :features]}
-        snapshot (db/get* conn :file-change params options)]
+        file     (files/get-minimal-file conn file-id {::db/for-update true})
+        snapshot (db/get* conn :file-change
+                          {:file-id file-id
+                           :id id}
+                          {::db/for-share true})]
 
-    (when (and (some? snapshot)
-               (some? (:data snapshot)))
+    (when-not snapshot
+      (ex/raise :type :not-found
+                :code :snapshot-not-found
+                :hint "unable to find snapshot with the provided label"
+                :id id
+                :file-id file-id))
 
-      (l/dbg :hint "restoring snapshot"
-             :file-id (str file-id)
-             :snapshot-id (str (:id snapshot)))
+    (when-not (:data snapshot)
+      (ex/raise :type :precondition
+                :code :snapshot-without-data
+                :hint "snapshot has no data"
+                :label (:label snapshot)
+                :file-id file-id))
 
-      (db/update! conn :file
-                  {:data (:data snapshot)
-                   :revn (:revn snapshot)
-                   :features (:features snapshot)}
-                  {:id file-id})
+    (l/dbg :hint "restoring snapshot"
+           :file-id (str file-id)
+           :label (:label snapshot)
+           :snapshot-id (str (:id snapshot)))
 
-      ;; clean object thumbnails
-      (let [sql (str "update file_tagged_object_thumbnail "
-                     "   set deleted_at = now() "
-                     " where file_id=? returning media_id")
-            res (db/exec! conn [sql file-id])]
+    (db/update! conn :file
+                {:data (:data snapshot)
+                 :revn (inc (:revn file))
+                 :features (:features snapshot)}
+                {:id file-id})
 
-        (doseq [media-id (into #{} (keep :media-id) res)]
-          (sto/touch-object! storage media-id)))
+    ;; clean object thumbnails
+    (let [sql (str "update file_tagged_object_thumbnail "
+                   "   set deleted_at = now() "
+                   " where file_id=? returning media_id")
+          res (db/exec! conn [sql file-id])]
 
-      ;; clean object thumbnails
-      (let [sql (str "update file_thumbnail "
-                     "   set deleted_at = now() "
-                     " where file_id=? returning media_id")
-            res (db/exec! conn [sql file-id])]
-        (doseq [media-id (into #{} (keep :media-id) res)]
-          (sto/touch-object! storage media-id)))
+      (doseq [media-id (into #{} (keep :media-id) res)]
+        (sto/touch-object! storage media-id)))
 
-      {:id (:id snapshot)})))
+    ;; clean object thumbnails
+    (let [sql (str "update file_thumbnail "
+                   "   set deleted_at = now() "
+                   " where file_id=? returning media_id")
+          res (db/exec! conn [sql file-id])]
+      (doseq [media-id (into #{} (keep :media-id) res)]
+        (sto/touch-object! storage media-id)))
 
-(def ^:private schema:restore-file-snapshot
-  [:map
-   [:file-id ::sm/uuid]
-   [:id ::sm/uuid]])
+    {:id (:id snapshot)
+     :label (:label snapshot)}))
+
+(defn- resolve-snapshot-by-label
+  [conn file-id label]
+  (->> (db/query conn :file-change
+                 {:file-id file-id
+                  :label label}
+                 {::sql/order-by [[:created-at :desc]]
+                  ::sql/columns [:file-id :id :label]})
+       (first)))
+
+(def ^:private
+  schema:restore-file-snapshot
+  [:and
+   [:map
+    [:file-id ::sm/uuid]
+    [:id {:optional true} ::sm/uuid]
+    [:label {:optional true} :string]]
+   [::sm/contains-any #{:id :label}]])
 
 (sv/defmethod ::restore-file-snapshot
   {::doc/added "1.20"
    ::doc/skip true
    ::sm/params schema:restore-file-snapshot}
-  [cfg {:keys [::rpc/profile-id] :as params}]
+  [cfg {:keys [::rpc/profile-id file-id id label] :as params}]
   (check-authorized! cfg profile-id)
-  (db/tx-run! cfg #(restore-file-snapshot! % params)))
+  (db/tx-run! cfg (fn [{:keys [::db/conn] :as cfg}]
+                    (let [params (cond-> params
+                                   (and (not id) (string? label))
+                                   (merge (resolve-snapshot-by-label conn file-id label)))]
+                      (restore-file-snapshot! cfg params)))))
 
 (defn take-file-snapshot!
   [{:keys [::db/conn]} {:keys [file-id label]}]
-  (when-let [file (db/get* conn :file {:id file-id})]
-    (let [id    (uuid/next)
-          label (or label (str "Snapshot at " (dt/format-instant (dt/now) :rfc1123)))]
-      (l/debug :hint "persisting file snapshot"
-               :file-id (str file-id)
-               :label label)
-      (db/insert! conn :file-change
-                  {:id id
-                   :revn (:revn file)
-                   :data (:data file)
-                   :features (:features file)
-                   :file-id (:id file)
-                   :label label})
-      {:id id})))
+  (let [file  (db/get conn :file {:id file-id})
+        id    (uuid/next)]
+
+    (l/debug :hint "creating file snapshot"
+             :file-id (str file-id)
+             :label label)
+
+    (db/insert! conn :file-change
+                {:id id
+                 :revn (:revn file)
+                 :data (:data file)
+                 :features (:features file)
+                 :file-id (:id file)
+                 :label label}
+                {::db/return-keys false})
+
+    {:id id :label label}))
+
+(defn generate-snapshot-label
+  []
+  (let [ts (-> (dt/now)
+               (dt/format-instant)
+               (str/replace #"[T:\.]" "-")
+               (str/rtrim "Z"))]
+    (str "snapshot-" ts)))
 
 (def ^:private schema:take-file-snapshot
   [:map [:file-id ::sm/uuid]])
@@ -137,5 +183,8 @@
    ::sm/params schema:take-file-snapshot}
   [cfg {:keys [::rpc/profile-id] :as params}]
   (check-authorized! cfg profile-id)
-  (db/tx-run! cfg #(take-file-snapshot! % params)))
+  (db/tx-run! cfg (fn [cfg]
+                    (let [params (update params :label (fn [label]
+                                                         (or label (generate-snapshot-label))))]
+                      (take-file-snapshot! cfg params)))))
 
