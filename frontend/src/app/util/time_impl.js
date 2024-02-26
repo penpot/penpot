628454d13cf8adc8f35ce65b5017c4419b0f95cf
Andrey Antukh <niwi@niwi.nz>
2024-02-27 00:02:07 +0100

    :sparkles: Add missing translations for date functions

--- frontend/src/app/util/time_impl.js
+++ frontend/src/app/util/time_impl.js
@@ -10,11 +10,25 @@ import {es} from "date-fns/locale/es";
 import {faIR} from "date-fns/locale/fa-IR";
 import {fr} from "date-fns/locale/fr";
 import {he} from "date-fns/locale/he";
+import {pt} from "date-fns/locale/pt";
 import {ptBR} from "date-fns/locale/pt-BR";
 import {ro} from "date-fns/locale/ro";
 import {ru} from "date-fns/locale/ru";
 import {tr} from "date-fns/locale/tr";
 import {zhCN} from "date-fns/locale/zh-CN";
+import {nl} from "date-fns/locale/nl";
+import {eu} from "date-fns/locale/eu";
+import {gl} from "date-fns/locale/gl";
+import {hr} from "date-fns/locale/hr";
+import {it} from "date-fns/locale/it";
+import {nb} from "date-fns/locale/nb";
+import {pl} from "date-fns/locale/pl";
+import {id} from "date-fns/locale/id";
+import {uk} from "date-fns/locale/uk";
+import {cs} from "date-fns/locale/cs";
+import {lv} from "date-fns/locale/lv";
+import {ko} from "date-fns/locale/ko";
+import {ja} from "date-fns/locale/ja";
 
 export const locales = {
   "ar": arSA,
@@ -22,15 +36,35 @@ export const locales = {
   "de": de,
   "el": el,
   "en": enUS,
+  "en_us": enUS,
   "es": es,
+  "es_es": es,
   "fa": faIR,
+  "fa_ir": faIR,
   "fr": fr,
   "he": he,
+  "pt": pt,
+  "pt_pt": pt,
   "pt_br": ptBR,
   "ro": ro,
   "ru": ru,
   "tr": tr,
-  "zh_cn": zhCN
+  "zh_cn": zhCN,
+  "nl": nl,
+  "eu": eu,
+  "gl": gl,
+  "hr": hr,
+  "it": it,
+  "nb": nb,
+  "nb_no": nb,
+  "pl": pl,
+  "id": id,
+  "uk": uk,
+  "cs": cs,
+  "lv": lv,
+  "ko": ko,
+  "ja": ja,
+  "ja_jp": ja,
 };
 
 export const format = fmt1.format;
