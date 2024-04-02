da5d402fafe06f951e8d2b6eb142ac0e870da630
Andrey Antukh <niwi@niwi.nz>
2024-04-02 14:29:59 +0200

    :sparkles: Move executor service initialization to a separared ns

--- backend/src/app/worker.clj
+++ backend/src/app/worker.clj
@@ -22,46 +22,9 @@
    [clojure.spec.alpha :as s]
    [cuerdas.core :as str]
    [integrant.core :as ig]
-   [promesa.exec :as px])
-  (:import
-   java.util.concurrent.Executor
-   java.util.concurrent.ThreadPoolExecutor))
+   [promesa.exec :as px]))
 
 (set! *warn-on-reflection* true)
-
-(s/def ::executor #(instance? Executor %))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Executor
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defmethod ig/pre-init-spec ::executor [_]
-  (s/keys :req []))
-
-(defmethod ig/init-key ::executor
-  [_ _]
-  (let [factory  (px/thread-factory :prefix "penpot/default/")
-        executor (px/cached-executor :factory factory :keepalive 60000)]
-    (l/inf :hint "starting executor")
-    (reify
-      java.lang.AutoCloseable
-      (close [_]
-        (l/inf :hint "stoping executor")
-        (px/shutdown! executor))
-
-      clojure.lang.IDeref
-      (deref [_]
-        {:active (.getPoolSize ^ThreadPoolExecutor executor)
-         :running (.getActiveCount ^ThreadPoolExecutor executor)
-         :completed (.getCompletedTaskCount ^ThreadPoolExecutor executor)})
-
-      Executor
-      (execute [_ runnable]
-        (.execute ^Executor executor ^Runnable runnable)))))
-
-(defmethod ig/halt-key! ::executor
-  [_ instance]
-  (.close ^java.lang.AutoCloseable instance))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; TASKS REGISTRY
@@ -94,65 +57,6 @@
                  (assoc registry tname (wrap-with-metrics f metrics tname))))
              {}
              tasks))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; EXECUTOR MONITOR
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(s/def ::name ::us/keyword)
-
-(defmethod ig/pre-init-spec ::monitor [_]
-  (s/keys :req [::name ::executor ::mtx/metrics]))
-
-(defmethod ig/prep-key ::monitor
-  [_ cfg]
-  (merge {::interval (dt/duration "2s")}
-         (d/without-nils cfg)))
-
-(defmethod ig/init-key ::monitor
-  [_ {:keys [::executor ::mtx/metrics ::interval ::name]}]
-  (letfn [(monitor! [executor prev-completed]
-            (let [labels        (into-array String [(d/name name)])
-                  stats         (deref executor)
-
-                  completed     (:completed stats)
-                  completed-inc (- completed prev-completed)
-                  completed-inc (if (neg? completed-inc) 0 completed-inc)]
-
-              (mtx/run! metrics
-                        :id :executor-active-threads
-                        :labels labels
-                        :val (:active stats))
-
-              (mtx/run! metrics
-                        :id :executor-running-threads
-                        :labels labels
-                        :val (:running stats))
-
-              (mtx/run! metrics
-                        :id :executors-completed-tasks
-                        :labels labels
-                        :inc completed-inc)
-
-              completed-inc))]
-
-    (px/thread
-      {:name "penpot/executors-monitor" :virtual true}
-      (l/inf :hint "monitor: started" :name name)
-      (try
-        (loop [completed 0]
-          (px/sleep interval)
-          (recur (long (monitor! executor completed))))
-        (catch InterruptedException _cause
-          (l/trc :hint "monitor: interrupted" :name name))
-        (catch Throwable cause
-          (l/err :hint "monitor: unexpected error" :name name :cause cause))
-        (finally
-          (l/inf :hint "monitor: terminated" :name name))))))
-
-(defmethod ig/halt-key! ::monitor
-  [_ thread]
-  (px/interrupt! thread))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; DISPATCHER
