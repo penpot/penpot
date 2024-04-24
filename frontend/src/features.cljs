6fbdc4ff0788e8c225d14bbd6acea8c1c2663342
alonso.torres <alonso.torres@kaleidos.net>
2024-04-24 10:25:47 +0200

    :bug: Fix problem activating feature

--- frontend/src/features.cljs
+++ frontend/src/features.cljs
@@ -26,8 +26,7 @@
   (clj->js (features/get-team-enabled-features @st/state)))
 
 (defn ^:export plugins []
-  (when (not (features/active-feature? @st/state "plugins/runtime"))
-    (plugins/init!))
-  (tm/schedule-on-idle #(st/emit! (features/toggle-feature "plugins/runtime")))
+  (st/emit! (features/enable-feature "plugins/runtime"))
+  (plugins/init!)
   nil)
 
