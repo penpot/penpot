832c1db63ba379653d36e3e2ca5341a158822ee3
Eva Marco <evamarcod@gmail.com>
2024-05-06 17:29:27 +0200

    :sparkles: Add login page as Page Object Model

--- frontend/playwright/helpers/index.js
+++ frontend/playwright/helpers/index.js
@@ -1,14 +1,14 @@
 export const interceptRPC = async (page, path, jsonFilename, options = {}) => {
   const interceptConfig = {
     status: 200,
-    ...options
+    ...options,
   };
 
-  await page.route(`**/api/rpc/command/${path}`, (route) => {
-    route.fulfill({
+  await page.route(`**/api/rpc/command/${path}`, async (route) => {
+    await route.fulfill({
       ...interceptConfig,
       contentType: "application/transit+json",
-      path: `playwright/fixtures/${jsonFilename}`,
+      path: `playwright/data/${jsonFilename}`,
     });
   });
 };
