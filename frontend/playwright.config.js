f74330dffe2e31115d301ac2259b3ad78ee6f6db
Belén Albeza <belen@hey.com>
2024-05-22 16:45:12 +0200

    :sparkles: Add more timeout for playwright expects on CI

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -19,6 +19,10 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests by default; can be overriden with --workers */
   workers: 1,
+  /* Timeout for expects (longer in CI) */
+  expect: {
+    timeout: process.env.CI ? 20000 : 5000,
+  },
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
