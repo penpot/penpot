67ababf1ed5c88c9d2c28952b727a96b86d2e3eb
Belén Albeza <belen@hey.com>
2024-06-04 11:25:09 +0200

    :bug: Fix link in penpot logo spanning way below it (in viewer)

---
+++ frontend/playwright/ui/pages/ViewerPage.js
@@ -0,0 +1,48 @@
+import { BaseWebSocketPage } from "./BaseWebSocketPage";
+
+export class ViewerPage extends BaseWebSocketPage {
+  static anyFileId = "c7ce0794-0992-8105-8004-38f280443849";
+  static anyPageId = "c7ce0794-0992-8105-8004-38f28044384a";
+
+  /**
+   * This should be called on `test.beforeEach`.
+   *
+   * @param {Page} page
+   * @returns
+   */
+  static async init(page) {
+    await BaseWebSocketPage.initWebSockets(page);
+  }
+
+  async setupLoggedInUser() {
+    await this.mockRPC("get-profile", "logged-in-user/get-profile-logged-in.json");
+  }
+
+  async setupEmptyFile() {
+    await this.mockRPC(/get\-view\-only\-bundle\?/, "viewer/get-view-only-bundle-empty-file.json");
+    await this.mockRPC("get-comment-threads?file-id=*", "workspace/get-comment-threads-empty.json");
+    await this.mockRPC(
+      "get-file-fragment?file-id=*&fragment-id=*",
+      "viewer/get-file-fragment-empty-file.json",
+    );
+  }
+
+  #ws = null;
+
+  constructor(page) {
+    super(page);
+  }
+
+  async goToViewer() {
+    await this.page.goto(
+      `/#/view/${ViewerPage.anyFileId}?page-id=${ViewerPage.anyPageId}&section=interactions&index=0`,
+    );
+
+    this.#ws = await this.waitForNotificationsWebSocket();
+    await this.#ws.mockOpen();
+  }
+
+  async cleanUp() {
+    await this.#ws.mockClose();
+  }
+}
