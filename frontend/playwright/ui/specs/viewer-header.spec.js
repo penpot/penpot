724bc24063d696b17c0e276814795f5a9af8f941
Belén Albeza <belen@hey.com>
2024-06-07 13:44:54 +0200

    :sparkles: Add test for #7805

--- frontend/playwright/ui/specs/viewer-header.spec.js
+++ frontend/playwright/ui/specs/viewer-header.spec.js
@@ -5,6 +5,18 @@ test.beforeEach(async ({ page }) => {
   await ViewerPage.init(page);
 });
 
+const singleBoardFileId = "dd5cc0bb-91ff-81b9-8004-77df9cd3edb1";
+const singleBoardPageId = "dd5cc0bb-91ff-81b9-8004-77df9cd3edb2";
+
+const setupFileWithSingleBoard = async (viewer) => {
+  await viewer.mockRPC(/get\-view\-only\-bundle\?/, "viewer/get-view-only-bundle-single-board.json");
+  await viewer.mockRPC("get-comment-threads?file-id=*", "workspace/get-comment-threads-empty.json");
+  await viewer.mockRPC(
+    "get-file-fragment?file-id=*&fragment-id=*",
+    "viewer/get-file-fragment-single-board.json",
+  );
+};
+
 test("Clips link area of the logo", async ({ page }) => {
   const viewerPage = new ViewerPage(page);
   await viewerPage.setupLoggedInUser();
@@ -20,4 +32,17 @@ test("Clips link area of the logo", async ({ page }) => {
   const { x, y } = await logoLink.boundingBox();
   await viewerPage.page.mouse.click(x, y + 100);
   await expect(page.url()).toBe(viewerUrl);
+});
+
+test("Updates URL with zoom type", async ({ page }) => {
+  const viewer = new ViewerPage(page);
+  await viewer.setupLoggedInUser();
+  await setupFileWithSingleBoard(viewer);
+
+  await viewer.goToViewer({ fileId: singleBoardFileId, pageId: singleBoardPageId });
+
+  await viewer.page.getByTitle("Zoom").click();
+  await viewer.page.getByText(/Fit/).click();
+
+  await expect(viewer.page).toHaveURL(/&zoom=fit/);
 });
