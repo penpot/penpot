a98f59469eb3d319ae18c2717f2e0a6469e1dcbd
Florian Schroedl <flo.schroedl@gmail.com>
2024-07-01 09:56:45 +0200

    Add - to allowed token-name

--- frontend/src/app/main/ui/workspace/tokens/form.cljs
+++ frontend/src/app/main/ui/workspace/tokens/form.cljs
@@ -30,7 +30,7 @@
   Caution: This will allow a trailing dot like `token-name.`,
   But we will trim that in the `finalize-name`,
   to not throw too many errors while the user is editing."
-  #"([a-zA-Z0-9]+\.?)*")
+  #"([a-zA-Z0-9-]+\.?)*")
 
 (def valid-token-name-schema
   (m/-simple-schema
