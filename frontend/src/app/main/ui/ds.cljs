2abbb0d3591cd533ad0604f5d480e79f6b2bfc4b
Eva Marco <evamarcod@gmail.com>
2024-07-05 09:01:35 +0200

    :sparkles: Add new heading component to the DS

--- frontend/src/app/main/ui/ds.cljs
+++ frontend/src/app/main/ui/ds.cljs
@@ -6,17 +6,25 @@
 
 (ns app.main.ui.ds
   (:require
+   [app.main.ui.ds.foundations.heading :refer [heading*]]
    [app.main.ui.ds.foundations.icon :refer [icon* icon-list]]
    [app.main.ui.ds.foundations.raw-svg :refer [raw-svg* raw-svg-list]]
+   [app.main.ui.ds.foundations.text :refer [text*]]
+   [app.main.ui.ds.foundations.typography :refer [typography-list]]
    [app.main.ui.ds.storybook :as sb]))
 
 (def default
   "A export used for storybook"
-  #js {:Icon icon*
+  #js {:Heading heading*
+       :Icon icon*
        :RawSvg raw-svg*
+       :Text text*
        ;; meta / misc
-       :meta #js {:icons icon-list :svgs raw-svg-list}
+       :meta #js {:icons icon-list
+                  :svgs raw-svg-list
+                  :typography (clj->js typography-list)}
        :storybook #js {:StoryGrid sb/story-grid*
                        :StoryGridCell sb/story-grid-cell*
+                       :StoryGridRow sb/story-grid-row*
                        :StoryHeader sb/story-header*
                        :StoryWrapper sb/story-wrapper*}})
