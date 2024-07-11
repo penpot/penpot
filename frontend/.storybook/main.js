a03eeb63e7b4fe9cdb39ca2a50b88a56ea5e7174
Belén Albeza <belen@hey.com>
2024-07-11 15:41:24 +0200

    :arrow_up: Upgrade to storybook v8

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,14 +1,22 @@
+import viteConfig from "../vite.config";
+
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   staticDirs: ["../resources/public"],
   addons: ["@storybook/addon-essentials"],
+  core: {
+    builder: "@storybook/builder-vite",
+    options: {
+      viteConfigPath: "../vite.config.js",
+    },
+  },
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+
+  docs: {},
 };
 export default config;
--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,7 +1,6 @@
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
