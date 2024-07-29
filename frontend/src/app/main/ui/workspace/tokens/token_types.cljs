f69db7ce9e7c88030be181d6de3b0a239aea36a6
Florian Schroedl <flo.schroedl@gmail.com>
2024-07-29 18:43:47 +0200

    Cleanup

--- frontend/src/app/main/ui/workspace/tokens/token_types.cljs
+++ frontend/src/app/main/ui/workspace/tokens/token_types.cljs
@@ -1,3 +1,9 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; Copyright (c) KALEIDOS INC
+
 (ns app.main.ui.workspace.tokens.token-types
   (:require
    [app.common.data :as d :refer [ordered-map]]
