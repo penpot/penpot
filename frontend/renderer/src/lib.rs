cd6a739abbaac19f8c84b539635be4226e0c84f7
Belén Albeza <belen@hey.com>
2024-09-10 16:58:18 +0200

    :wrench: Add dummy rust project + build scripts with wasm-pack

---
+++ frontend/renderer/src/lib.rs
@@ -0,0 +1,27 @@
+use wasm_bindgen::prelude::*;
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn print(msg: &str) {
+    log(msg);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
