3182ff1e15294cca5e7f900b94c28054b31ab8e0
Florian Schroedl <flo.schroedl@gmail.com>
2024-09-30 15:08:18 +0200

    Cleanup

--- frontend/src/app/main/ui/workspace/tokens/token.cljs
+++ frontend/src/app/main/ui/workspace/tokens/token.cljs
@@ -6,10 +6,6 @@
    [clojure.set :as set]
    [cuerdas.core :as str]))
 
-(defn get-workspace-token
-  [token-id state]
-  (get-in state [:workspace-data :tokens token-id]))
-
 (def parseable-token-value-regexp
   "Regexp that can be used to parse a number value out of resolved token value.
   This regexp also trims whitespace around the value."
