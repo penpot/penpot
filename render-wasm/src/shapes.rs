52a3fa1e0c3edcb15fc8665ccd65f6800f58fb43
Belén Albeza <belen@hey.com>
2024-10-30 16:51:06 +0100

    apply transform matrix in wasm (wip)

--- render-wasm/src/shapes.rs
+++ render-wasm/src/shapes.rs
@@ -1,12 +1,39 @@
-use crate::render::{render_rect, State};
+use euclid;
 use skia_safe as skia;
 
-#[derive(Debug, Clone, Copy)]
-pub struct Selrect {
+use crate::render::{render_rect, State};
+
+pub struct WorldSpace;
+pub type TransformMatrix = euclid::Transform2D<f32, WorldSpace, WorldSpace>;
+pub type Point = euclid::Point2D<f32, WorldSpace>;
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Rect {
     pub x1: f32,
     pub y1: f32,
     pub x2: f32,
     pub y2: f32,
+}
+
+impl Rect {
+    fn new(top_left: Point, bottom_right: Point) -> Self {
+        Rect {
+            x1: top_left.x,
+            y1: top_left.y,
+            x2: bottom_right.x,
+            y2: bottom_right.y,
+        }
+    }
+
+    #[inline]
+    fn top_left(&self) -> Point {
+        Point::new(self.x1, self.y1)
+    }
+
+    #[inline]
+    fn bottom_right(&self) -> Point {
+        Point::new(self.x2, self.y2)
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -19,14 +46,30 @@ pub struct Transform {
     pub f: f32,
 }
 
+impl Into<TransformMatrix> for Transform {
+    fn into(self) -> TransformMatrix {
+        TransformMatrix::new(self.a, self.b, self.c, self.d, self.e, self.f)
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 pub struct Shape {
-    pub selrect: Selrect,
+    pub selrect: Rect,
     pub transform: Transform,
 }
 
+impl Shape {
+    pub fn transformed_selrect(&self) -> Rect {
+        let matrix: TransformMatrix = self.transform.into();
+        let top_left = matrix.transform_point(self.selrect.top_left());
+        let bottom_right = matrix.transform_point(self.selrect.bottom_right());
+
+        Rect::new(top_left, bottom_right)
+    }
+}
+
 pub static mut SHAPES_BUFFER: [Shape; 2048] = [Shape {
-    selrect: Selrect {
+    selrect: Rect {
         x1: 0.0,
         y1: 0.0,
         x2: 0.0,
@@ -49,13 +92,9 @@ pub(crate) fn draw_all(state: &mut State) {
     }
 
     for shape in shapes {
-        let r = skia::Rect::new(
-            shape.selrect.x1,
-            shape.selrect.y1,
-            shape.selrect.x2,
-            shape.selrect.y2,
-        );
-        println!("{:?}", shape.transform);
+        let selrect = shape.transformed_selrect();
+
+        let r = skia::Rect::new(selrect.x1, selrect.y1, selrect.x2, selrect.y2);
         render_rect(&mut state.surface, r, skia::Color::RED);
     }
 }
