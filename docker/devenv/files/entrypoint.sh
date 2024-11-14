058c3707c818a1ef9772597c133975d13f6c8070
Andrey Antukh <niwi@niwi.nz>
2024-11-14 17:23:14 +0100

    :sparkles: Improve consistency on devenv initialization

--- docker/devenv/files/entrypoint.sh
+++ docker/devenv/files/entrypoint.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-export PATH=/usr/lib/jvm/openjdk/bin:/usr/local/nodejs/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
-
-source /usr/local/emsdk/emsdk_env.sh;
-source /usr/local/cargo/env
-
-export JAVA_OPTS=${JAVA_OPTS:-"-Xmx1000m -Xms200m"};
-
 set -e
 usermod -u ${EXTERNAL_UID:-1000} penpot
 
