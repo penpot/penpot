b81ffa422f84c8badf1d4f73d3775f2ec1969cc0
Andrey Antukh <niwi@niwi.nz>
2024-11-14 14:38:40 +0100

    :paperclip: Update devenv entrypoint

--- docker/devenv/files/entrypoint.sh
+++ docker/devenv/files/entrypoint.sh
@@ -2,6 +2,11 @@
 
 export PATH=/usr/lib/jvm/openjdk/bin:/usr/local/nodejs/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
 
+source /usr/local/emsdk/emsdk_env.sh;
+source /usr/local/cargo/env
+
+export JAVA_OPTS=${JAVA_OPTS:-"-Xmx1000m -Xms200m"};
+
 set -e
 usermod -u ${EXTERNAL_UID:-1000} penpot
 
