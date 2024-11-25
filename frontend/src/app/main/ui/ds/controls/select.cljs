85c4c1c7db0b7b48d2e19a5538c948e1ca497f99
Juanfran <juanfran.ag@gmail.com>
2024-11-25 15:11:54 +0100

    :sparkles: new combobox component to the ds with component testing

--- frontend/src/app/main/ui/ds/controls/select.cljs
+++ frontend/src/app/main/ui/ds/controls/select.cljs
@@ -9,69 +9,13 @@
    [app.common.data.macros :as dm]
    [app.main.style :as stl])
   (:require
+   [app.main.ui.ds.controls.shared.options-dropdown :refer [options-dropdown*]]
    [app.main.ui.ds.foundations.assets.icon :refer [icon* icon-list] :as i]
    [app.util.array :as array]
    [app.util.dom :as dom]
    [app.util.keyboard :as kbd]
    [app.util.object :as obj]
    [rumext.v2 :as mf]))
-
-(mf/defc option*
-  {::mf/props :obj
-   ::mf/private true}
-  [{:keys [id label icon aria-label on-click selected set-ref focused] :rest props}]
-  [:> :li {:value id
-           :class (stl/css-case :option true
-                                :option-with-icon (some? icon)
-                                :option-current focused)
-           :aria-selected selected
-
-           :ref (fn [node]
-                  (set-ref node id))
-           :role "option"
-           :id id
-           :on-click on-click
-           :data-id id}
-
-   (when (some? icon)
-     [:> icon*
-      {:id icon
-       :size "s"
-       :class (stl/css :option-icon)
-       :aria-hidden (when label true)
-       :aria-label  (when (not label) aria-label)}])
-
-   [:span {:class (stl/css :option-text)} label]
-   (when selected
-     [:> icon*
-      {:id i/tick
-       :size "s"
-       :class (stl/css :option-check)
-       :aria-hidden (when label true)}])])
-
-(mf/defc options-dropdown*
-  {::mf/props :obj
-   ::mf/private true}
-  [{:keys [set-ref on-click options selected focused] :rest props}]
-  (let [props (mf/spread-props props
-                               {:class (stl/css :option-list)
-                                :tab-index "-1"
-                                :role "listbox"})]
-    [:> "ul" props
-     (for [option ^js options]
-       (let [id    (obj/get option "id")
-             label (obj/get option "label")
-             aria-label (obj/get option "aria-label")
-             icon (obj/get option "icon")]
-         [:> option* {:selected (= id selected)
-                      :key id
-                      :id id
-                      :label label
-                      :icon icon
-                      :aria-label aria-label
-                      :set-ref set-ref
-                      :focused (= id focused)
-                      :on-click on-click}]))]))
 
 (def ^:private schema:select-option
   [:and
