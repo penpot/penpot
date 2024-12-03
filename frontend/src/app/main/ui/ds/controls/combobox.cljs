55361178da2efb933ad12c6433ea840997d0706d
Juanfran <juanfran.ag@gmail.com>
2024-12-03 13:21:06 +0100

    wip

--- frontend/src/app/main/ui/ds/controls/combobox.cljs
+++ frontend/src/app/main/ui/ds/controls/combobox.cljs
@@ -14,75 +14,14 @@
    [app.util.dom :as dom]
    [app.util.keyboard :as kbd]
    [app.util.object :as obj]
-   [rumext.v2 :as mf]))
+   [rumext.v2 :as mf]
+   [app.main.ui.ds.controls.shared.options-dropdown :refer [options-dropdown*]]))
 
 (def listbox-id-index (atom 0))
-
-(mf/defc option*
-  {::mf/props :obj
-   ::mf/private true}
-  [{:keys [id label icon aria-label on-click selected set-ref focused] :rest props}]
-  [:> :li {:value id
-           :class (stl/css-case :option true
-                                :option-with-icon (some? icon)
-                                :option-current focused)
-           :aria-selected selected
-
-           :ref (fn [node]
-                  (set-ref node id))
-           :role "option"
-           :id id
-           :on-click on-click
-           :data-id id}
-
-   (when (some? icon)
-     [:> icon*
-      {:id icon
-       :size "s"
-       :class (stl/css :option-icon)
-       :aria-hidden (when label true)
-       :aria-label  (when (not label) aria-label)}])
-
-   [:span {:class (stl/css :option-text)} label]
-   (when selected
-     [:> icon*
-      {:id i/tick
-       :size "s"
-       :class (stl/css :option-check)
-       :aria-hidden (when label true)}])])
-
-(mf/defc options-dropdown*
-  {::mf/props :obj
-   ::mf/private true}
-  [{:keys [set-ref on-click options selected focused] :rest props}]
-  (let [props (mf/spread-props props
-                               {:class (stl/css :option-list)
-                                :tab-index "-1"
-                                :role "listbox"})]
-    [:> "ul" props
-     (for [option ^js options]
-       (let [id    (obj/get option "id")
-             label (obj/get option "label")
-             aria-label (obj/get option "aria-label")
-             icon (obj/get option "icon")]
-         [:> option* {:selected (= id selected)
-                      :key id
-                      :id id
-                      :label label
-                      :icon icon
-                      :aria-label aria-label
-                      :set-ref set-ref
-                      :focused (= id focused)
-                      :on-click on-click}]))]))
 
 (defn- get-option
   [options id]
   (array/find #(= id (obj/get % "id")) options))
-
-(defn- get-selected-option-id
-  [options default]
-  (let [option (get-option options default)]
-    (obj/get option "id")))
 
 (defn- handle-focus-change
   [options focused* new-index options-nodes-refs]
