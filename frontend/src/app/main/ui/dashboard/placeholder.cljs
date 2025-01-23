877c721ae99e0ca956da3cc06555bb6223c91ff9
Pablo Alba <pablo.alba@kaleidos.net>
2025-01-23 15:13:18 +0100

    :tada: Add A/B test for first state of a project

--- frontend/src/app/main/ui/dashboard/placeholder.cljs
+++ frontend/src/app/main/ui/dashboard/placeholder.cljs
@@ -8,14 +8,40 @@
   (:require-macros [app.main.style :as stl])
   (:require
    [app.config :as cf]
+   [app.main.ui.dashboard.import :as udi]
    [app.main.ui.ds.product.empty-placeholder :refer [empty-placeholder*]]
    [app.main.ui.ds.product.loader :refer [loader*]]
    [app.main.ui.icons :as i]
+   [app.util.dom :as dom]
    [app.util.i18n :as i18n :refer [tr]]
    [rumext.v2 :as mf]))
 
+(mf/defc empty-placeholder-projects*
+  {::mf/wrap-props false}
+  [{:keys [on-create on-finish-import project-id] :as props}]
+  (let [file-input       (mf/use-ref nil)
+        on-add-library   (mf/use-fn #(dom/open-new-window "https://penpot.app/penpothub/libraries-templates"))
+        on-import-files  (mf/use-fn #(dom/click (mf/ref-val file-input)))]
+
+    [:div {:class (stl/css :empty-project-container)}
+     [:div {:class (stl/css :empty-project-card) :on-click on-create :title (tr "dashboard.add-file")}
+      [:div {:class (stl/css :empty-project-card-title)} (tr "dashboard.empty-project.create")]
+      [:div {:class (stl/css :empty-project-card-subtitle)} (tr "dashboard.empty-project.start")]]
+
+     [:div {:class (stl/css :empty-project-card) :on-click on-import-files :title (tr "dashboard.empty-project.import")}
+      [:div {:class (stl/css :empty-project-card-title)} (tr "dashboard.empty-project.import")]
+      [:div {:class (stl/css :empty-project-card-subtitle)} (tr "dashboard.empty-project.import-penpot")]]
+
+     [:div {:class (stl/css :empty-project-card) :on-click on-add-library :title (tr "dashboard.empty-project.go-to-libraries")}
+      [:div {:class (stl/css :empty-project-card-title)} (tr "dashboard.empty-project.add-library")]
+      [:div {:class (stl/css :empty-project-card-subtitle)} (tr "dashboard.empty-project.explore")]]
+
+     [:& udi/import-form {:ref file-input
+                          :project-id project-id
+                          :on-finish-import on-finish-import}]]))
+
 (mf/defc empty-placeholder
-  [{:keys [dragging? limit origin create-fn can-edit]}]
+  [{:keys [dragging? limit origin create-fn can-edit project-id on-finish-import]}]
   (let [on-click
         (mf/use-fn
          (mf/deps create-fn)
@@ -45,16 +71,18 @@
                             :tag-name "span"}])]
 
       :else
-      [:div {:class (stl/css :grid-empty-placeholder)}
-       (if (cf/external-feature-flag "add-file-01" "test")
-         [:button {:class (stl/css :create-new)
-                   :on-click on-click
-                   :on-mouse-enter on-mouse-enter
-                   :on-mouse-leave on-mouse-leave}
-          (if @show-text (tr "dashboard.add-file") i/add)]
-         [:button {:class (stl/css :create-new)
-                   :on-click on-click}
-          i/add])])))
+      (if (cf/external-feature-flag "add-file-02" "test")
+        [:> empty-placeholder-projects* {:on-create on-click :on-finish-import on-finish-import :project-id project-id}]
+        [:div {:class (stl/css :grid-empty-placeholder)}
+         (if (cf/external-feature-flag "add-file-01" "test")
+           [:button {:class (stl/css :create-new)
+                     :on-click on-click
+                     :on-mouse-enter on-mouse-enter
+                     :on-mouse-leave on-mouse-leave}
+            (if @show-text (tr "dashboard.add-file") i/add)]
+           [:button {:class (stl/css :create-new)
+                     :on-click on-click}
+            i/add])]))))
 
 (mf/defc loading-placeholder
   []
