6ba8e5ba59b7dbe60f37ecb0d00caaf7b54e6990
Andrey Antukh <niwi@niwi.nz>
2025-01-30 11:35:57 +0100

    WIP

--- backend/src/app/rpc/commands/viewer.clj
+++ backend/src/app/rpc/commands/viewer.clj
@@ -26,21 +26,9 @@
       (update :pages (fn [pages] (filterv #(contains? allowed %) pages)))
       (update :pages-index select-keys allowed)))
 
-(defn- get-view-only-bundle
-  [{:keys [::db/conn] :as cfg} {:keys [profile-id file-id ::perms] :as params}]
+(defn- get-viewer-bundle
+  [{:keys [::db/conn] :as cfg} {:keys [profile-id file-id share-id ::perms] :as params}]
   (let [file    (files/get-file cfg file-id)
-
-        project (db/get conn :project
-                        {:id (:project-id file)}
-                        {:columns [:id :name :team-id]})
-
-        team    (-> (db/get conn :team {:id (:team-id project)})
-                    (teams/decode-row))
-
-        members    (teams/get-team-members conn (:team-id project))
-        member-ids (into #{} (map :id) members)
-
-        perms   (assoc perms :in-team (contains? member-ids profile-id))
 
         _       (-> (cfeat/get-team-enabled-features cf/flags team)
                     (cfeat/check-client-features! (:features params))
@@ -54,41 +42,41 @@
                   (update :data select-keys [:id :options :pages :pages-index :components]))
 
         libs    (files/get-file-libraries conn file-id)
-        links   (->> (db/query conn :share-link {:file-id file-id})
-                     (mapv (fn [row]
-                             (-> row
-                                 (update :pages db/decode-pgarray #{})
-                                 ;; NOTE: the flags are deprecated but are still present
-                                 ;; on the table on old rows. The flags are pgarray and
-                                 ;; for avoid decoding it (because they are no longer used
-                                 ;; on frontend) we just dissoc the column attribute from
-                                 ;; row.
-                                 (dissoc :flags)))))
+
+        links   (if (= :membership (:type perms))
+                  (db/query conn :share-link {:file-id file-id})
+                  (db/query conn :share-link {:file-id file-id :id share-id}))
+
+        links   (mapv (fn [row]
+                        (-> row
+                            (update :pages db/decode-pgarray #{})
+                            ;; NOTE: the flags are deprecated but are still present
+                            ;; on the table on old rows. The flags are pgarray and
+                            ;; for avoid decoding it (because they are no longer used
+                            ;; on frontend) we just dissoc the column attribute from
+                            ;; row.
+                            (dissoc :flags)))
+                      links)
 
         fonts   (db/query conn :team-font-variant
                           {:team-id (:id team)
                            :deleted-at nil})]
 
-    {:users members
-     :profiles members
-     :fonts fonts
-     :project project
+    {:fonts fonts
      :share-links links
      :libraries libs
-     :file file
-     :team (assoc team :permissions perms)
-     :permissions perms}))
+     :file file}))
 
-(def schema:get-view-only-bundle
+(def schema:get-viewer-bundle
   [:map {:title "get-view-only-bundle"}
    [:file-id ::sm/uuid]
    [:share-id {:optional true} ::sm/uuid]
    [:features {:optional true} ::cfeat/features]])
 
-(sv/defmethod ::get-view-only-bundle
+(sv/defmethod ::get-viewer-bundle
   {::rpc/auth false
-   ::doc/added "1.17"
-   ::sm/params schema:get-view-only-bundle}
+   ::doc/added "2.4"
+   ::sm/params schema:get-viewer-bundle}
   [system {:keys [::rpc/profile-id file-id share-id] :as params}]
   (db/run! system
            (fn [{:keys [::db/conn] :as system}]
@@ -107,40 +95,48 @@
                (get-view-only-bundle system params)))))
 
 
-(sv/defmethod ::get-viewer-team
+(def ^:private sql:get-team-and-project
+  "SELECT t.*,
+          p.id AS project_id,
+          p.name AS project_name
+     FROM team AS t
+    INNER JOIN project AS p ON (p.team_id = t.id)
+    INNER JOIN file AS f ON (f.project_id = p.id)
+    WHERE f.id = ?")
+
+(def ^:private schema:get-viewer-info
+  [:map {:title "get-viewer-info"}
+   [:file-id ::sm/uuid]
+   [:share-id {:optional true} ::sm/uuid]])
+
+(sv/defmethod ::get-viewer-info
+  "Get bootstrap data for team, for logged or unlogged user"
   {::rpc/auth false
-   ::doc/added "1.17"
-   ;; ::sm/params schema:get-view-only-teams
-   }
+   ::doc/added "2.5"
+   ::sm/params schema:get-viewer-info}
   [system {:keys [::rpc/profile-id file-id share-id] :as params}]
+  (db/run! system
+           (fn [{:keys [::db/conn]}]
+             (let [perms (files/get-permissions conn profile-id file-id share-id)]
+               ;; When we have neither profile nor share, we just return a not
+               ;; found response to the user.
+               (when-not perms
+                 (ex/raise :type :not-found
+                           :code :object-not-found
+                           :hint "object not found"))
 
+               (let [data       (db/exec-one! conn [sql:get-team-and-project file-id])
+                     team       (-> (dissoc data :project-id :project-name)
+                                    (teams/decode-row))
 
+                     project    {:id (:project-id data)
+                                 :name (:project-name data)}
 
+                     members    (teams/get-team-members conn (:team-id project))
+                     member-ids (into #{} (map :id) members)
+                     perms      (assoc perms :in-team (contains? member-ids profile-id))]
 
-
-;; (def schema:get-teams
-;;   [:map {:title "get-view-only-bundle"}
-;;    [:file-id ::sm/uuid]
-;;    [:share-id {:optional true} ::sm/uuid]
-;;    [:features {:optional true} ::cfeat/features]])
-
-;; (sv/defmethod ::get-view-only-teams
-;;   {::rpc/auth false
-;;    ::doc/added "1.17"
-;;    ::sm/params schema:get-view-only-teams}
-;;   [system {:keys [::rpc/profile-id share-id] :as params}]
-;;   (db/run! system
-;;            (fn [{:keys [::db/conn] :as system}]
-
-;;              (let [permissions (files/get-permissions conn profile-id file-id share-id)]
-
-;;                ;; When we have neither profile nor share, we just return a not
-;;                ;; found response to the user.
-;;                (when-not permissions
-;;                  (ex/raise :type :not-found
-;;                            :code :object-not-found
-;;                            :hint "object not found"))
-
-
-;;                (teams/get-teams conn profile-id share-id)))))
-
+                 {:permissions perms
+                  :team (assoc team :permissions perms)
+                  :project project
+                  :members members})))))
