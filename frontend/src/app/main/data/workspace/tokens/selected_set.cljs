e46fb9dba736cbe382eb3801cfaef1398f18d81d
Andrey Antukh <niwi@niwi.nz>
2025-02-04 14:30:00 +0100

    :paperclip: Add missing copyright header

--- frontend/src/app/main/data/workspace/tokens/selected_set.cljs
+++ frontend/src/app/main/data/workspace/tokens/selected_set.cljs
@@ -1,3 +1,9 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; Copyright (c) KALEIDOS INC
+
 (ns app.main.data.workspace.tokens.selected-set
   "The user selected token set in the ui, stored by the `:name` of the set.
   Will default to the first set."
