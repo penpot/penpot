ca65f5ad9a97a1e1c7940fe387f9b9de8efbc0a3
Xavier Julian <xavier.julian@kaleidos.net>
2025-02-21 13:32:28 +0100

    :lipstick: Improve theme modal with UX enhancements

--- frontend/playwright/ui/specs/tokens.spec.js
+++ frontend/playwright/ui/specs/tokens.spec.js
@@ -255,14 +255,8 @@ test.describe("Tokens: Tokens Tab", () => {
   });
 
   test("User edits theme and activates it in the sidebar", async ({ page }) => {
-    const {
-      workspacePage,
-      tokensUpdateCreateModal,
-      tokenThemesSetsSidebar,
-      tokensSidebar,
-      tokenContextMenuForToken,
-      tokenThemeUpdateCreateModal,
-    } = await setupTokensFile(page);
+    const { tokenThemesSetsSidebar, tokenThemeUpdateCreateModal } =
+      await setupTokensFile(page);
 
     await expect(tokenThemesSetsSidebar).toBeVisible();
 
@@ -315,7 +309,8 @@ test.describe("Tokens: Tokens Tab", () => {
     ).toBeVisible();
 
     await tokenThemeUpdateCreateModal
-      .getByRole("button", { name: "Close" })
+      .getByRole("button")
+      .getByText("close")
       .click();
     await expect(tokenThemeUpdateCreateModal).not.toBeVisible();
 
@@ -617,7 +612,7 @@ test.describe("Tokens: Themes modal", () => {
     ).toHaveCount(1);
   });
 
-  test("Create theme", async ({ page }) => {
+  test("Add new theme", async ({ page }) => {
     const { tokenThemeUpdateCreateModal, workspacePage } =
       await setupTokensFile(page);
 
@@ -625,7 +620,7 @@ test.describe("Tokens: Themes modal", () => {
 
     await tokenThemeUpdateCreateModal
       .getByRole("button", {
-        name: "Create theme",
+        name: "Add new theme",
       })
       .click();
 
