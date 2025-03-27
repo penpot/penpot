3c4be537d9bf32a3438a4b10804a2aaa7fc36602
Elena Torro <elenatorro@gmail.com>
2025-03-27 17:17:36 +0100

    :bug: Revert offset change to fix paragraph rendering

--- render-wasm/src/render/text.rs
+++ render-wasm/src/render/text.rs
@@ -2,10 +2,12 @@ use super::{RenderState, Shape, SurfaceId};
 use crate::shapes::TextContent;
 
 pub fn render(render_state: &mut RenderState, shape: &Shape, text: &TextContent) {
+    let mut offset_y = 0.0;
     for mut skia_paragraph in text.to_paragraphs(&render_state.fonts().font_collection()) {
         skia_paragraph.layout(shape.width());
 
-        let xy = (shape.selrect().x(), shape.selrect.y());
+        let xy = (shape.selrect().x(), shape.selrect.y() + offset_y);
         skia_paragraph.paint(render_state.surfaces.canvas(SurfaceId::Fills), xy);
+        offset_y += skia_paragraph.height();
     }
 }
