e5bc369e56bbd00cd5c0fdcd86c92ee99f4dc7f8
Marina López <marina.lopez.yap@gmail.com>
2025-05-26 12:56:40 +0200

    :sparkles: Visual indicators subscription for teams and project settings (#6546)
    * :sparkles: Visual indicators subscription for teams and project settings
    
    * :paperclip: Fixes PR feedback
    
    ---------
    
    Co-authored-by: Andrey Antukh <niwi@niwi.nz>

--- frontend/src/app/main/ui/dashboard/team.cljs
+++ frontend/src/app/main/ui/dashboard/team.cljs
@@ -22,6 +22,10 @@
    [app.main.ui.components.file-uploader :refer [file-uploader]]
    [app.main.ui.components.forms :as fm]
    [app.main.ui.dashboard.change-owner]
+   [app.main.ui.dashboard.subscription :refer [team*
+                                               members-cta*
+                                               show-subscription-members-main-banner?
+                                               show-subscription-invitations-main-banner?]]
    [app.main.ui.dashboard.team-form]
    [app.main.ui.ds.foundations.assets.icon :refer [icon*]]
    [app.main.ui.icons :as i]
@@ -537,10 +541,22 @@
 
   [:*
    [:& header {:section :dashboard-team-members :team team}]
-   [:section {:class (stl/css :dashboard-container :dashboard-team-members)}
+   [:section {:class (stl/css-case
+                      :dashboard-container true
+                      :dashboard-team-members true
+                      :dashboard-top-cta (show-subscription-members-main-banner? team profile))}
+    (when (and (contains? cfg/flags :subscriptions)
+               (show-subscription-members-main-banner? team profile))
+      [:> members-cta* {:banner-is-expanded true :team team :profile profile}])
     [:> team-members*
      {:profile profile
-      :team team}]]])
+      :team team}]
+    (when (and
+           (contains? cfg/flags :subscriptions)
+           (or
+            (and (= (:type (:subscription team)) "professional") (< (count (:members team)) 8))
+            (= (:status (:subscription team)) "trialing")))
+      [:> members-cta* {:banner-is-expanded false :team team}])]])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; INVITATIONS SECTION
@@ -803,8 +819,18 @@
   [:*
    [:& header {:section :dashboard-team-invitations
                :team team}]
-   [:section {:class (stl/css :dashboard-team-invitations)}
-    [:> invitation-section* {:team team}]]])
+   [:section {:class (stl/css-case
+                      :dashboard-team-invitations true
+                      :dashboard-top-cta (show-subscription-invitations-main-banner? team))}
+    (when (and (contains? cfg/flags :subscriptions)
+               (show-subscription-invitations-main-banner? team))
+      [:> members-cta* {:banner-is-expanded true :team team}])
+    [:> invitation-section* {:team team}]
+    (when (and (contains? cfg/flags :subscriptions)
+               (or
+                (and (= (:type (:subscription team)) "professional") (< (count (:members team)) 8))
+                (= (:status (:subscription team)) "trialing")))
+      [:> members-cta* {:banner-is-expanded false :team team}])]])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; WEBHOOKS SECTION
@@ -1159,5 +1185,8 @@
        [:div {:class (stl/css :block-content)}
         document-icon
         [:span {:class (stl/css :block-text)}
-         (tr "labels.num-of-files" (i18n/c (:files stats)))]]]]]))
+         (tr "labels.num-of-files" (i18n/c (:files stats)))]]]
+
+      (when (contains? cfg/flags :subscriptions)
+        [:> team* {:is-owner (:is-owner permissions) :team team}])]]))
 
