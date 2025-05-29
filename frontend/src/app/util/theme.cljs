196381d8103a88cb92ce6e1b464fd9b6176f9f71
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2025-05-29 20:38:48 +0000

    :fire: Remove old code for theme support

--- frontend/src/app/util/theme.cljs
+++ frontend/src/app/util/theme.cljs
@@ -3,53 +3,16 @@
 ;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 ;;
 ;; Copyright (c) KALEIDOS INC
-;; Copyright (c) Mathieu BRUNOT <mathieu.brunot@monogramm.io>
 
 (ns app.util.theme
-  "A theme manager."
   (:require
-   [app.config :as cfg]
-   [app.util.dom :as dom]
    [app.util.globals :as globals]
-   [app.util.storage :as storage]
    [beicon.v2.core :as rx]
-   [potok.v2.core :as ptk]
-   [rumext.v2 :as mf]))
+   [potok.v2.core :as ptk]))
 
-(defonce theme (get storage/global ::theme cfg/default-theme))
-(defonce theme-sub (rx/subject))
-(defonce themes #js {})
-
-(defn init!
-  [data]
-  (set! themes data))
-
-(defn set-current-theme!
-  [v]
-  (when (not= theme v)
-    (when-some [el (dom/get-element "theme")]
-      (set! (.-href el) (str "css/main-" v ".css")))
-    (swap! storage/global assoc ::theme v)
-    (set! theme v)
-    (rx/push! theme-sub v)))
-
-(defn set-default-theme!
-  []
-  (set-current-theme! cfg/default-theme))
-
-(defn use-theme
-  []
-  (let [[theme set-theme] (mf/useState theme)]
-    (mf/useEffect (fn []
-                    (let [sub (rx/sub! theme-sub #(set-theme %))]
-                      #(rx/dispose! sub)))
-                  #js [])
-    theme))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Set the preferred color scheme based on the user's system settings.
-;; TODO: this is unrelated to the theme support above, which seems unused as
-;;       of v2.7
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defonce ^:private color-scheme-mq
