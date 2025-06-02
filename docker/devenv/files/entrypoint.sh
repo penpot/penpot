46f330fef308b07f8ab61d6f937a5e60b35f0b2e
Andrey Antukh <niwi@niwi.nz>
2025-06-02 19:10:48 +0200

    :sparkles: Move several logic from init to entrypoint on devenv
    For make commands consistent independently if they are executed
    inside devenv or from manage.sh

--- docker/devenv/files/entrypoint.sh
+++ docker/devenv/files/entrypoint.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+EMSDK_QUIET=1 . /home/emsdk/emsdk_env.sh;
+
 usermod -u ${EXTERNAL_UID:-1000} penpot;
+
+cp /root/.bashrc /home/penpot/.bashrc
+cp /root/.vimrc /home/penpot/.vimrc
+cp /root/.tmux.conf /home/penpot/.tmux.conf
+
+chown -R penpot:users /home/penpot
 
 exec "$@"
--- docker/devenv/files/init.sh
+++ docker/devenv/files/init.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-cp /root/.bashrc /home/penpot/.bashrc
-cp /root/.vimrc /home/penpot/.vimrc
-cp /root/.tmux.conf /home/penpot/.tmux.conf
-chown -R penpot:users /home/penpot
-chown -R penpot:users /home/cargo;
-
 set -e
 nginx
 tail -f /dev/null
