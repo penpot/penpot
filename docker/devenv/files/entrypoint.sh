1909189ce06c18cdb9f7943bee19b3751bd7388a
Andrey Antukh <niwi@niwi.nz>
2025-06-02 22:29:39 +0200

    :sparkles: Use different approach for setup cargo home

--- docker/devenv/files/entrypoint.sh
+++ docker/devenv/files/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-EMSDK_QUIET=1 . /home/emsdk/emsdk_env.sh;
+EMSDK_QUIET=1 . /usr/local/emsdk/emsdk_env.sh;
 
 usermod -u ${EXTERNAL_UID:-1000} penpot;
 
@@ -11,5 +11,9 @@ cp /root/.vimrc /home/penpot/.vimrc
 cp /root/.tmux.conf /home/penpot/.tmux.conf
 
 chown -R penpot:users /home/penpot
+rsync -ar --chown=penpot:users /usr/local/cargo/ /home/penpot/.cargo/
+
+export PATH="/home/penpot/.cargo/bin:$PATH"
+export CARGO_HOME="/home/penpot/.cargo"
 
 exec "$@"
