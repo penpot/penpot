fc655224afb72fc0c4e222c7da07e8185488e4b5
Andrey Antukh <niwi@niwi.nz>
2025-06-09 16:17:10 +0200

    :zap: Add memorization to icon-button* ds component

--- frontend/src/app/main/ui/ds/buttons/icon_button.cljs
+++ frontend/src/app/main/ui/ds/buttons/icon_button.cljs
@@ -9,6 +9,7 @@
    [app.common.data.macros :as dm]
    [app.main.style :as stl])
   (:require
+   [app.common.data :as d]
    [app.main.ui.ds.foundations.assets.icon :refer [icon* icon-list]]
    [app.main.ui.ds.tooltip.tooltip :refer [tooltip*]]
    [rumext.v2 :as mf]))
@@ -24,18 +25,31 @@
     [:maybe [:enum "primary" "secondary" "ghost" "destructive" "action"]]]])
 
 (mf/defc icon-button*
-  {::mf/schema schema:icon-button}
+  {::mf/schema schema:icon-button
+   ::mf/memo true}
   [{:keys [class icon icon-class variant aria-label children] :rest props}]
-  (let [variant (or variant "primary")
-        tooltip-id (mf/use-id)
-        class (dm/str class " " (stl/css-case :icon-button true
-                                              :icon-button-primary (= variant "primary")
-                                              :icon-button-secondary (= variant "secondary")
-                                              :icon-button-ghost (= variant "ghost")
-                                              :icon-button-action (= variant "action")
-                                              :icon-button-destructive (= variant "destructive")))
-        props (mf/spread-props props {:class class
-                                      :aria-labelledby tooltip-id})]
+  (let [variant
+        (d/nilv variant "primary")
+
+        tooltip-id
+        (mf/use-id)
+
+        class
+        (dm/str class " "
+                (stl/css-case :icon-button true
+                              :icon-button-primary (= variant "primary")
+                              :icon-button-secondary (= variant "secondary")
+                              :icon-button-ghost (= variant "ghost")
+                              :icon-button-action (= variant "action")
+                              :icon-button-destructive (= variant "destructive")))
+
+        props
+        (mf/spread-props props
+                         {:class class
+                          :aria-labelledby tooltip-id})]
+
     [:> tooltip* {:tooltip-content aria-label
                   :id tooltip-id}
-     [:> "button" props [:> icon* {:icon-id icon :aria-hidden true :class icon-class}] children]]))
+     [:> "button" props
+      [:> icon* {:icon-id icon :aria-hidden true :class icon-class}]
+      children]]))
