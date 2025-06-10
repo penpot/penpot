011e14f8d8fc8ed9dae1f7891cbb08fed98760f9
Elena Torro <elenatorro@gmail.com>
2025-06-10 13:37:18 +0200

    :wrench: Add text to path methods for future usage

--- render-wasm/src/render/text.rs
+++ render-wasm/src/render/text.rs
@@ -1,25 +1,15 @@
 use super::{RenderState, Shape, SurfaceId};
-use skia_safe::{self as skia, canvas::SaveLayerRec, textlayout::Paragraph};
+use skia_safe::{textlayout::Paragraph, Paint, Path};
 
 pub fn render(
     render_state: &mut RenderState,
     shape: &Shape,
     paragraphs: &[Vec<Paragraph>],
     surface_id: Option<SurfaceId>,
-    paint: Option<skia::Paint>,
 ) {
-    let use_save_layer = paint.is_some();
-    let mask_paint = paint.unwrap_or_default();
-    let mask = SaveLayerRec::default().paint(&mask_paint);
     let canvas = render_state
         .surfaces
         .canvas(surface_id.unwrap_or(SurfaceId::Fills));
-
-    // Skip save_layer when no custom paint is provided to avoid isolating content unnecessarily.
-    // This ensures inner strokes and fills can blend correctly on the same surface.
-    if use_save_layer {
-        canvas.save_layer(&mask);
-    }
 
     for group in paragraphs {
         let mut offset_y = 0.0;
@@ -29,7 +19,47 @@ pub fn render(
             offset_y += skia_paragraph.height();
         }
     }
-    if use_save_layer {
-        canvas.restore();
+}
+
+// Render text paths (unused)
+#[allow(dead_code)]
+pub fn render_as_path(
+    render_state: &mut RenderState,
+    paths: &Vec<(Path, Paint)>,
+    surface_id: Option<SurfaceId>,
+) {
+    let canvas = render_state
+        .surfaces
+        .canvas(surface_id.unwrap_or(SurfaceId::Fills));
+
+    for (path, paint) in paths {
+        // Note: path can be empty
+        canvas.draw_path(path, paint);
     }
 }
+
+// How to use it?
+// Type::Text(text_content) => {
+//     self.surfaces
+//         .apply_mut(&[SurfaceId::Fills, SurfaceId::Strokes], |s| {
+//             s.canvas().concat(&matrix);
+//         });
+
+//     let text_content = text_content.new_bounds(shape.selrect());
+//     let paths = text_content.get_paths(antialias);
+
+//     shadows::render_text_drop_shadows(self, &shape, &paths, antialias);
+//     text::render(self, &paths, None, None);
+
+//     for stroke in shape.strokes().rev() {
+//         shadows::render_text_path_stroke_drop_shadows(
+//             self, &shape, &paths, stroke, antialias,
+//         );
+//         strokes::render_text_paths(self, &shape, stroke, &paths, None, None, antialias);
+//         shadows::render_text_path_stroke_inner_shadows(
+//             self, &shape, &paths, stroke, antialias,
+//         );
+//     }
+
+//     shadows::render_text_inner_shadows(self, &shape, &paths, antialias);
+// }
