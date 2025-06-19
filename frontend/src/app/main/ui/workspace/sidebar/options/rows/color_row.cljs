70a29c43ec42831e5d2cd9394aa1e32d5e0929fd
Belén Albeza <belen.albeza@kaleidos.net>
2025-06-19 08:57:38 +0200

    :wrench: Fix race condition in colorpicket gradient tests (#6722)

--- frontend/src/app/main/ui/workspace/sidebar/options/rows/color_row.cljs
+++ frontend/src/app/main/ui/workspace/sidebar/options/rows/color_row.cljs
@@ -268,6 +268,7 @@
                              :on-focus on-focus
                              :on-blur on-blur
                              :on-change handle-opacity-change
+                             :data-testid "opacity-input"
                              :default 100
                              :min 0
                              :max 100}]])]
