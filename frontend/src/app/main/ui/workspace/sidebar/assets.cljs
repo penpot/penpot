2db0cc0cbf745e07af983271694463003c0bce7a
Juanfran <juanfran.ag@gmail.com>
2025-07-21 15:07:30 +0200

    :bug: Fix ESC key not closing Add/Manage Libraries modal

--- frontend/src/app/main/ui/workspace/sidebar/assets.cljs
+++ frontend/src/app/main/ui/workspace/sidebar/assets.cljs
@@ -129,8 +129,7 @@
         (mf/use-fn
          (mf/deps file-id)
          (fn []
-           (modal/show! :libraries-dialog {:file-id file-id})
-           (modal/allow-click-outside!)))
+           (modal/show! :libraries-dialog {:file-id file-id})))
 
         on-open-menu
         (mf/use-fn  #(swap! filters* update :open-menu not))
