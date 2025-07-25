0442b6ac25a0fd88f8a557ab1e0a3aa3ef0d521b
Andrey Antukh <niwi@niwi.nz>
2025-07-25 09:01:32 +0200

    :sparkles: Add reader tag support for geom matrix

--- backend/src/data_readers.clj
+++ backend/src/data_readers.clj
@@ -1,4 +1,5 @@
 {penpot/inst app.common.time/inst
  penpot/cron app.util.cron/cron
  penpot/duration app.common.time/duration
+ penpot/matrix app.common.geom.matrix/decode-matrix
  penpot/point app.common.geom.point/decode-point}
