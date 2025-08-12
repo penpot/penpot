537c5ca7b83591c0f9e4523a40a8ca126a4c7e71
Pablo Alba <pablo.alba@kaleidos.net>
2025-08-12 17:56:03 +0200

    :bug: Fix missing selection after swap (#7104)

--- frontend/src/app/main/data/workspace/libraries.cljs
+++ frontend/src/app/main/data/workspace/libraries.cljs
@@ -988,12 +988,15 @@
                             (into parents-of-swapped)
                             (conj (:id new-shape)))]
 
-        (rx/of
-         (dwu/start-undo-transaction undo-id)
-         (dch/commit-changes changes)
-         (dws/select-shape (:id new-shape) true)
-         (ptk/data-event :layout/update {:ids all-parents :undo-group undo-group})
-         (dwu/commit-undo-transaction undo-id))))))
+        (rx/merge
+         (rx/of
+          (dwu/start-undo-transaction undo-id)
+          (dch/commit-changes changes)
+          (ptk/data-event :layout/update {:ids all-parents :undo-group undo-group})
+          (dwu/commit-undo-transaction undo-id)
+          (dws/deselect-all))
+         (->> (rx/of (dws/select-shape (:id new-shape) false))
+              (rx/delay 1)))))))
 
 (defn component-multi-swap
   "Swaps several components with another one"
