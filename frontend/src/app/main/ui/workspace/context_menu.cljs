1d3a1a094a365feb3ec9fa3364131a02f246e0d0
Pablo Alba <pablo.alba@kaleidos.net>
2025-09-02 11:11:19 +0200

    :bug: Missing component copy options on the context menu

--- frontend/src/app/main/ui/workspace/context_menu.cljs
+++ frontend/src/app/main/ui/workspace/context_menu.cljs
@@ -568,6 +568,7 @@
   [{:keys [shapes]}]
   (let [single?                    (= (count shapes) 1)
         objects                    (deref refs/workspace-page-objects)
+        shapes                     (keep (d/getf objects) shapes)
         can-make-component         (every? true? (map #(ctn/valid-shape-for-component? objects %) shapes))
         heads                      (filter ctk/instance-head? shapes)
         components-menu-entries    (cmm/generate-components-menu-entries heads)
