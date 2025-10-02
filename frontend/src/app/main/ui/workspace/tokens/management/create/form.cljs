a780e7e14610364f7df0ef808a9be6daf28f8d66
Eva Marco <evamarcod@gmail.com>
2025-10-02 11:17:12 +0200

    :bug: Fix edit modal title

--- frontend/src/app/main/ui/workspace/tokens/management/create/form.cljs
+++ frontend/src/app/main/ui/workspace/tokens/management/create/form.cljs
@@ -548,7 +548,7 @@ custom-input-token-value-props: Custom props passed to the custom-input-token-va
      [:div {:class (stl/css :token-rows)}
       [:> heading* {:level 2 :typography "headline-medium" :class (stl/css :form-modal-title)}
        (if (= action "edit")
-         (tr "workspace.tokens.edit-token")
+         (tr "workspace.tokens.edit-token" token-type)
          (tr "workspace.tokens.create-token" token-type))]
 
       [:div {:class (stl/css :input-row)}
