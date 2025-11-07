a11b0f54d7bb94d4afcfa302f687022a4dfe8255
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2025-11-07 11:52:53 +0100

    :bug: Fix changing properties resizes the text box height

--- frontend/src/app/main/data/workspace/texts.cljs
+++ frontend/src/app/main/data/workspace/texts.cljs
@@ -56,16 +56,19 @@
   ([shape]
    (resize-wasm-text-modifiers shape (:content shape)))
 
-  ([{:keys [id points selrect] :as shape} content]
+  ([{:keys [id points selrect grow-type] :as shape} content]
    (wasm.api/use-shape id)
    (wasm.api/set-shape-text-content id content)
    (wasm.api/set-shape-text-images id content)
 
    (let [dimension (wasm.api/get-text-dimensions)
-         resize-v  (gpt/point
-                    (/ (:width dimension) (-> selrect :width))
-                    (/ (:height dimension) (-> selrect :height)))
-
+         width-scale (if (#{:fixed :auto-height} grow-type)
+                       1.0
+                       (/ (:width dimension) (:width selrect)))
+         height-scale (if (= :fixed grow-type)
+                        1.0
+                        (/ (:height dimension) (:height selrect)))
+         resize-v  (gpt/point width-scale height-scale)
          origin    (first points)]
 
      {id
