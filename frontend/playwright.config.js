ef12a28411adc5d658b496d97147143ce8fd9729
Andrey Antukh <niwi@niwi.nz>
2025-11-17 14:05:04 +0100

    WIP

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -14,7 +14,8 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  forbidOnly: false,
+  // forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests by default; can be overriden with --workers */
