e9cf87002881c6c1eab3900cf5d2418938be38d0
Juanfran <juanfran.ag@gmail.com>
2026-01-22 15:01:47 +0100

    :bug: Fix plugins ui lint

--- plugins/libs/ui/eslint.config.mjs
+++ plugins/libs/ui/eslint.config.mjs
@@ -1,12 +1,39 @@
-import nx from '@nx/eslint-plugin';
-import baseConfig from '../../eslint.base.config.js';
+// @ts-check
+import baseConfig, { compat } from '../../eslint.base.config.js';
+import storybook from 'eslint-plugin-storybook';
+import unusedImports from 'eslint-plugin-unused-imports';
+import reactHooks from 'eslint-plugin-react-hooks';
 
 export default [
   ...baseConfig,
-  ...nx.configs['flat/react'],
+  reactHooks.configs.flat.recommended,
+  ...storybook.configs['flat/recommended'],
+  ...compat.config({ extends: ['plugin:@nx/react-typescript'] }).map((config) => ({
+    ...config,
+    files: ['**/*.ts', '**/*.tsx'],
+  })),
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    // Override or add rules here
-    rules: {},
+    ignores: ['**/.storybook/**'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
   },
 ];
+
