ca4d00df6998a050892a0328359a899e13d81c5c
Andrey Antukh <niwi@niwi.nz>
2026-02-04 15:36:07 +0100

    :bug: Fix latest error report related migration

--- backend/src/app/migrations/sql/0144-mod-server-error-report-table.sql
+++ backend/src/app/migrations/sql/0144-mod-server-error-report-table.sql
@@ -1,4 +1,11 @@
 ALTER TABLE server_error_report DROP CONSTRAINT server_error_report_pkey;
+
+DELETE FROM server_error_report a
+USING server_error_report b
+WHERE a.id = b.id
+  AND a.ctid < b.ctid;
+
 ALTER TABLE server_error_report ADD PRIMARY KEY (id);
+
 CREATE INDEX server_error_report__version__idx
     ON server_error_report ( version );
