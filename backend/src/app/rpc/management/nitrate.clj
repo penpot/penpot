e7aecc2a14516346b9c487d1b9078d8b145f07ef
Pablo Alba <pablo.alba@kaleidos.net>
2026-02-06 13:12:39 +0100

    :sparkles: Add managed profiles endpoint to nitrate api

--- backend/src/app/rpc/management/nitrate.clj
+++ backend/src/app/rpc/management/nitrate.clj
@@ -9,7 +9,7 @@
   organization management and token validation endpoints."
   (:require
    [app.common.schema :as sm]
-   [app.common.types.profile :refer [schema:profile]]
+   [app.common.types.profile :refer [schema:profile, schema:basic-profile]]
    [app.common.types.team :refer [schema:team]]
    [app.common.uuid :as uuid]
    [app.db :as db]
@@ -80,3 +80,35 @@
                          :team-id id
                          :organization-id organization-id
                          :organization-name organization-name})))
+
+
+;; ---- API: get-managed-profiles
+
+(def ^:private sql:get-managed-profiles
+  "SELECT DISTINCT p.id, p.fullname as name, p.email
+     FROM profile p
+     JOIN team_profile_rel tpr_member
+       ON tpr_member.profile_id = p.id
+    WHERE p.id <> ?
+      AND EXISTS (
+        SELECT 1
+          FROM team_profile_rel tpr_owner
+          JOIN team t
+            ON t.id = tpr_owner.team_id
+         WHERE tpr_owner.profile_id = ?
+           AND tpr_owner.team_id = tpr_member.team_id
+           AND tpr_owner.is_owner IS TRUE
+           AND t.is_default IS FALSE
+           AND t.deleted_at IS NULL);")
+
+(def schema:managed-profile-result
+  [:vector schema:basic-profile])
+
+(sv/defmethod ::get-managed-profiles
+  "List profiles that belong to teams for which current user is owner"
+  {::doc/added "2.14"
+   ::sm/params [:map]
+   ::sm/result schema:managed-profile-result}
+  [cfg {:keys [::rpc/profile-id]}]
+  (let [current-user-id (-> (profile/get-profile cfg profile-id) :id)]
+    (db/exec! cfg [sql:get-managed-profiles current-user-id current-user-id])))
