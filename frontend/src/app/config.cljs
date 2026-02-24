a75de11e701e3b8541f9d57717549fc19bdf9e9c
Luis de Dios <luis.dedios@kaleidos.net>
2026-02-24 21:34:19 +0100

    :sparkles: Improve MCP section in the dashboard

--- frontend/src/app/config.cljs
+++ frontend/src/app/config.cljs
@@ -151,7 +151,7 @@
   (let [f (obj/get global "initializeExternalConfigInfo")]
     (when (fn? f) (f))))
 
-(def mcp-server-url (-> public-uri u/ensure-path-slash (u/join "mcp") str))
+(def mcp-server-url (-> public-uri u/ensure-path-slash (u/join "mcp/stream") str))
 (def mcp-help-center-uri "https://help.penpot.app/technical-guide/")
 
 ;; --- Helper Functions
