db24ddd2a0fd5e9fecb6ff0ba8c3fa14e498a3e8
Alexis Morin <the.alexis.morin@gmail.com>
2026-02-23 20:03:18 -0500

    Add Canadian French to language selector
    Signed-off-by: Alexis Morin <the.alexis.morin@gmail.com>

--- frontend/src/app/util/i18n.cljs
+++ frontend/src/app/util/i18n.cljs
@@ -31,6 +31,7 @@
    {:label "Dutch (community)" :value "nl"}
    {:label "Euskera (community)" :value "eu"}
    {:label "Français (community)" :value "fr"}
+   {:label "Français - Canada (community)" :value "fr_ca"}
    {:label "Gallego (Community)" :value "gl"}
    {:label "Hausa (Community)" :value "ha"}
    {:label "Hrvatski (Community)" :value "hr"}
