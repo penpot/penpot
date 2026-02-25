3610485fe481cb1298f6de5f18946a6f344a060f
Pablo Alba <pablo.alba@kaleidos.net>
2026-02-25 17:04:46 +0100

    :sparkles: Show subscription type nitrate

--- frontend/src/app/main/data/nitrate.cljs
+++ frontend/src/app/main/data/nitrate.cljs
@@ -2,6 +2,9 @@
   (:require
    [app.main.data.modal :as modal]
    [app.main.repo :as rp]
+   [app.main.router :as rt]
+   [app.main.store :as st]
+   [app.util.dom :as dom]
    [beicon.v2.core :as rx]
    [potok.v2.core :as ptk]))
 
@@ -13,5 +16,13 @@
       (->> (rp/cmd! ::get-nitrate-connectivity {})
            (rx/map (fn [connectivity]
                      (modal/show popup-type (or connectivity {}))))))))
+
+(defn go-to-nitrate-cc
+  []
+  (st/emit! (dom/open-new-window "/control-center/")))
+
+(defn go-to-nitrate-billing
+  []
+  (st/emit! (rt/nav-raw :href "/control-center/licenses/billing")))
 
 
