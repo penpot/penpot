60d93f8888917da2ab1116ec4021529793fb2a7f
Belén Albeza <belen@hey.com>
2026-03-10 15:24:22 +0100

    :sparkles: Add regression test for bug 13302 (highlight token)

--- frontend/playwright/ui/specs/tokens/general.spec.js
+++ frontend/playwright/ui/specs/tokens/general.spec.js
@@ -22,3 +22,30 @@ test.describe("Tokens tab - common tests", () => {
   });
 });
 
+test("BUG 13302 - Dimension token not being highlighted after applying it", async ({ page }) => {
+  const workspacePage = new WasmWorkspacePage(page);
+  await workspacePage.setupEmptyFile();
+
+  await workspacePage.mockGetFile("tokens/get-file-13302.json");
+  await workspacePage.mockRPC("update-file?id=*", "tokens/update-file-13302.json");
+
+  await workspacePage.goToWorkspace({
+    fileId: "6886b62b-1979-8195-8007-8d0b92d3116a",
+    pageId: "6886b62b-1979-8195-8007-8d0b92d3116b",
+  });
+
+  await workspacePage.clickLeafLayer("Rectangle");
+
+  await workspacePage.sidebar.getByRole("tab", { name: "Tokens" }).click();
+
+  await workspacePage.tokensSidebar
+    .getByRole("button", { name: "Dimensions 1" })
+    .click();
+
+  const token = workspacePage.tokensSidebar.getByRole("checkbox", { name: "any-dimensions-token" });
+  await token.click();
+
+  // FIXME: this is a bug somewhere else in tokens, this should be fully applied
+  // for a dimensions token
+  await expect(token).toBeChecked({ indeterminate: true });
+});
