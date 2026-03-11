77f9d75f3586962f78b338ce9397333c90bdbd38
Luis de Dios <luis.dedios@kaleidos.net>
2026-03-11 23:54:27 +0100

    :tada: Show notification when the plugin is loaded in another tab

--- frontend/src/app/main/data/workspace/mcp.cljs
+++ frontend/src/app/main/data/workspace/mcp.cljs
@@ -10,10 +10,12 @@
    [app.common.uri :as u]
    [app.config :as cf]
    [app.main.broadcast :as mbc]
+   [app.main.data.notifications :as ntf]
    [app.main.data.plugins :as dp]
    [app.main.repo :as rp]
    [app.main.store :as st]
    [app.plugins.register :refer [mcp-plugin-id]]
+   [app.util.i18n :refer [tr]]
    [beicon.v2.core :as rx]
    [potok.v2.core :as ptk]))
 
@@ -50,6 +52,18 @@
            (rx/take 1)
            (rx/map #(ptk/data-event ::connect))))))
 
+(defn manage-notification
+  [mcp-enabled? mcp-connected?]
+  (if mcp-enabled?
+    (if mcp-connected?
+      (st/emit! (ntf/hide))
+      (st/emit! (ntf/dialog :content (tr "notifications.mcp.active-tab-switching.text")
+                            :cancel {:label (tr "notifications.mcp.active-tab-switching.dismiss")
+                                     :callback #(st/emit! (ntf/hide))}
+                            :accept {:label (tr "notifications.mcp.active-tab-switching.switch")
+                                     :callback #(st/emit! (connect-mcp))})))
+    (st/emit! (ntf/hide))))
+
 (defn update-mcp-status
   [value]
   (ptk/reify ::update-mcp-status
@@ -58,7 +72,9 @@
       (update-in state [:profile :props] assoc :mcp-enabled value))
 
     ptk/WatchEvent
-    (watch [_ _ _]
+    (watch [_ state _]
+      (let [mcp-connected?  (-> state :workspace-local :mcp :connected)]
+        (manage-notification value mcp-connected?))
       (case value
         true  (rx/of (ptk/data-event ::connect))
         false (rx/of (ptk/data-event ::disconnect))
@@ -69,7 +85,12 @@
   (ptk/reify ::update-mcp-plugin-connection
     ptk/UpdateEvent
     (update [_ state]
-      (update-in state [:workspace-local :mcp] assoc :connected value))))
+      (update-in state [:workspace-local :mcp] assoc :connected value))
+
+    ptk/WatchEvent
+    (watch [_ state _]
+      (let [mcp-enabled? (-> state :profile :props :mcp-enabled)]
+        (manage-notification mcp-enabled? value)))))
 
 (defn init-mcp!
   [stream]
