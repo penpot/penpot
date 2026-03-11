9188dad9a60067e91b222f01198acf5072f63555
Andrey Antukh <niwi@niwi.nz>
2026-03-11 12:21:28 +0100

    :sparkles: Add improvements to AGENTS.md

--- common/src/app/common/svg/path/arc_to_bezier.js
+++ common/src/app/common/svg/path/arc_to_bezier.js
@@ -14,7 +14,7 @@
 goog.provide("app.common.svg.path.arc_to_bezier");
 
 // https://raw.githubusercontent.com/fontello/svgpath/master/lib/a2c.js
-goog.scope(function() {
+goog.scope(function () {
   const self = app.common.svg.path.arc_to_bezier;
 
   var TAU = Math.PI * 2;
@@ -27,19 +27,22 @@ goog.scope(function() {
   // we can use simplified math (without length normalization)
   //
   function unit_vector_angle(ux, uy, vx, vy) {
-    var sign = (ux * vy - uy * vx < 0) ? -1 : 1;
-    var dot  = ux * vx + uy * vy;
+    var sign = ux * vy - uy * vx < 0 ? -1 : 1;
+    var dot = ux * vx + uy * vy;
 
     // Add this to work with arbitrary vectors:
     // dot /= Math.sqrt(ux * ux + uy * uy) * Math.sqrt(vx * vx + vy * vy);
 
     // rounding errors, e.g. -1.0000000000000002 can screw up this
-    if (dot >  1.0) { dot =  1.0; }
-    if (dot < -1.0) { dot = -1.0; }
+    if (dot > 1.0) {
+      dot = 1.0;
+    }
+    if (dot < -1.0) {
+      dot = -1.0;
+    }
 
     return sign * Math.acos(dot);
   }
-
 
   // Convert from endpoint to center parameterization,
   // see http://www.w3.org/TR/SVG11/implnote.html#ArcImplementationNotes
@@ -53,11 +56,11 @@ goog.scope(function() {
     // points. After that, rotate it to line up ellipse axes with coordinate
     // axes.
     //
-    var x1p =  cos_phi*(x1-x2)/2 + sin_phi*(y1-y2)/2;
-    var y1p = -sin_phi*(x1-x2)/2 + cos_phi*(y1-y2)/2;
+    var x1p = (cos_phi * (x1 - x2)) / 2 + (sin_phi * (y1 - y2)) / 2;
+    var y1p = (-sin_phi * (x1 - x2)) / 2 + (cos_phi * (y1 - y2)) / 2;
 
-    var rx_sq  =  rx * rx;
-    var ry_sq  =  ry * ry;
+    var rx_sq = rx * rx;
+    var ry_sq = ry * ry;
     var x1p_sq = x1p * x1p;
     var y1p_sq = y1p * y1p;
 
@@ -66,33 +69,33 @@ goog.scope(function() {
     // Compute coordinates of the centre of this ellipse (cx', cy')
     // in the new coordinate system.
     //
-    var radicant = (rx_sq * ry_sq) - (rx_sq * y1p_sq) - (ry_sq * x1p_sq);
+    var radicant = rx_sq * ry_sq - rx_sq * y1p_sq - ry_sq * x1p_sq;
 
     if (radicant < 0) {
       // due to rounding errors it might be e.g. -1.3877787807814457e-17
       radicant = 0;
     }
 
-    radicant /=   (rx_sq * y1p_sq) + (ry_sq * x1p_sq);
+    radicant /= rx_sq * y1p_sq + ry_sq * x1p_sq;
     radicant = Math.sqrt(radicant) * (fa === fs ? -1 : 1);
 
-    var cxp = radicant *  rx/ry * y1p;
-    var cyp = radicant * -ry/rx * x1p;
+    var cxp = ((radicant * rx) / ry) * y1p;
+    var cyp = ((radicant * -ry) / rx) * x1p;
 
     // Step 3.
     //
     // Transform back to get centre coordinates (cx, cy) in the original
     // coordinate system.
     //
-    var cx = cos_phi*cxp - sin_phi*cyp + (x1+x2)/2;
-    var cy = sin_phi*cxp + cos_phi*cyp + (y1+y2)/2;
+    var cx = cos_phi * cxp - sin_phi * cyp + (x1 + x2) / 2;
+    var cy = sin_phi * cxp + cos_phi * cyp + (y1 + y2) / 2;
 
     // Step 4.
     //
     // Compute angles (theta1, delta_theta).
     //
-    var v1x =  (x1p - cxp) / rx;
-    var v1y =  (y1p - cyp) / ry;
+    var v1x = (x1p - cxp) / rx;
+    var v1y = (y1p - cyp) / ry;
     var v2x = (-x1p - cxp) / rx;
     var v2y = (-y1p - cyp) / ry;
 
@@ -106,7 +109,7 @@ goog.scope(function() {
       delta_theta += TAU;
     }
 
-    return [ cx, cy, theta1, delta_theta ];
+    return [cx, cy, theta1, delta_theta];
   }
 
   //
@@ -114,24 +117,33 @@ goog.scope(function() {
   // see http://math.stackexchange.com/questions/873224
   //
   function approximate_unit_arc(theta1, delta_theta) {
-    var alpha = 4/3 * Math.tan(delta_theta/4);
+    var alpha = (4 / 3) * Math.tan(delta_theta / 4);
 
     var x1 = Math.cos(theta1);
     var y1 = Math.sin(theta1);
     var x2 = Math.cos(theta1 + delta_theta);
     var y2 = Math.sin(theta1 + delta_theta);
 
-    return [ x1, y1, x1 - y1*alpha, y1 + x1*alpha, x2 + y2*alpha, y2 - x2*alpha, x2, y2 ];
+    return [
+      x1,
+      y1,
+      x1 - y1 * alpha,
+      y1 + x1 * alpha,
+      x2 + y2 * alpha,
+      y2 - x2 * alpha,
+      x2,
+      y2,
+    ];
   }
 
   function calculate_beziers(x1, y1, x2, y2, fa, fs, rx, ry, phi) {
-    var sin_phi = Math.sin(phi * TAU / 360);
-    var cos_phi = Math.cos(phi * TAU / 360);
+    var sin_phi = Math.sin((phi * TAU) / 360);
+    var cos_phi = Math.cos((phi * TAU) / 360);
 
     // Make sure radii are valid
     //
-    var x1p =  cos_phi*(x1-x2)/2 + sin_phi*(y1-y2)/2;
-    var y1p = -sin_phi*(x1-x2)/2 + cos_phi*(y1-y2)/2;
+    var x1p = (cos_phi * (x1 - x2)) / 2 + (sin_phi * (y1 - y2)) / 2;
+    var y1p = (-sin_phi * (x1 - x2)) / 2 + (cos_phi * (y1 - y2)) / 2;
 
     // console.log("L", x1p, y1p)
 
@@ -145,7 +157,6 @@ goog.scope(function() {
       return [];
     }
 
-
     // Compensate out-of-range radii
     //
     rx = Math.abs(rx);
@@ -157,24 +168,19 @@ goog.scope(function() {
       ry *= Math.sqrt(lambda);
     }
 
-
     // Get center parameters (cx, cy, theta1, delta_theta)
     //
     var cc = get_arc_center(x1, y1, x2, y2, fa, fs, rx, ry, sin_phi, cos_phi);
 
-
     var result = [];
     var theta1 = cc[2];
     var delta_theta = cc[3];
-
-
 
     // Split an arc to multiple segments, so each segment
     // will be less than τ/4 (= 90°)
     //
     var segments = Math.max(Math.ceil(Math.abs(delta_theta) / (TAU / 4)), 1);
     delta_theta /= segments;
-
 
     for (var i = 0; i < segments; i++) {
       var item = approximate_unit_arc(theta1, delta_theta);
@@ -195,8 +201,8 @@ goog.scope(function() {
         y *= ry;
 
         // rotate
-        var xp = cos_phi*x - sin_phi*y;
-        var yp = sin_phi*x + cos_phi*y;
+        var xp = cos_phi * x - sin_phi * y;
+        var yp = sin_phi * x + cos_phi * y;
 
         // translate
         curve[i + 0] = xp + cc[0];
