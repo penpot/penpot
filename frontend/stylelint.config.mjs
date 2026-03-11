bd1cf158c4992f7abd125894fd25706886ece1fe
Xavier Julian <xavier.julian@kaleidos.net>
2026-03-11 15:33:08 +0100

    :sparkles: Fix default standard scss errors with extends - WIP

--- frontend/stylelint.config.mjs
+++ frontend/stylelint.config.mjs
@@ -2,7 +2,7 @@ import postcssScss from "postcss-scss";
 
 /** @type {import("stylelint").Config} */
 export default {
-  extends: ["stylelint-config-standard"],
+  extends: ["stylelint-config-standard-scss"],
   plugins: ["stylelint-scss", "stylelint-use-logical-spec"],
   overrides: [
     {
@@ -11,42 +11,47 @@ export default {
     },
   ],
   rules: {
+    // TODO: Enable rules secuentially to avoid a huge amount of warnings at once
     "at-rule-no-unknown": null,
+    "declaration-property-value-no-unknown": null,
+    // scss
+    "scss/comment-no-empty": false,
     "scss/at-rule-no-unknown": true,
-    "selector-pseudo-element-no-unknown": true,
-    // Using quotes
-    "font-family-name-quotes": "always-unless-keyword",
-    "function-url-quotes": "always",
-    "selector-attribute-quotes": "always",
-    // Disallow vendor prefixes
-    "at-rule-no-vendor-prefix": true,
-    "media-feature-name-no-vendor-prefix": true,
-    "property-no-vendor-prefix": true,
-    "selector-no-vendor-prefix": true,
-    "value-no-vendor-prefix": true,
-    // Specificity
-    "no-descending-specificity": null,
-    "max-nesting-depth": 3,
-    "selector-max-compound-selectors": 3,
-    "selector-max-specificity": "1,2,1",
-    // Miscellanea
-    "color-named": "never",
-    "declaration-no-important": true,
-    "declaration-property-unit-allowed-list": {
-      "font-size": ["rem"],
-      "/^animation/": ["s"],
-    },
-    // 'order/properties-alphabetical-order': true,
-    "selector-max-type": 1,
-    "selector-type-no-unknown": true,
-    // Notation
-    "font-weight-notation": "numeric",
-    // URLs
-    "function-url-no-scheme-relative": true,
-    "liberty/use-logical-spec": "always",
-    "selector-class-pattern": null,
-    "alpha-value-notation": null,
-    "color-function-notation": null,
-    "value-keyword-case": null,
+    "scss/load-no-partial-leading-underscore": false, // TODO: should be changed to true to follow scss conventions
+    // "selector-pseudo-element-no-unknown": true,
+    // // Using quotes
+    // "font-family-name-quotes": "always-unless-keyword",
+    // "function-url-quotes": "always",
+    // "selector-attribute-quotes": "always",
+    // // Disallow vendor prefixes
+    // "at-rule-no-vendor-prefix": true,
+    // "media-feature-name-no-vendor-prefix": true,
+    // "property-no-vendor-prefix": true,
+    // "selector-no-vendor-prefix": true,
+    // "value-no-vendor-prefix": true,
+    // // Specificity
+    // "no-descending-specificity": null,
+    // "max-nesting-depth": 3,
+    // "selector-max-compound-selectors": 3,
+    // "selector-max-specificity": "1,2,1",
+    // // Miscellanea
+    // "color-named": "never",
+    // "declaration-no-important": true,
+    // "declaration-property-unit-allowed-list": {
+    //   "font-size": ["rem"],
+    //   "/^animation/": ["s"],
+    // },
+    // // 'order/properties-alphabetical-order': true,
+    // "selector-max-type": 1,
+    // "selector-type-no-unknown": true,
+    // // Notation
+    // "font-weight-notation": "numeric",
+    // // URLs
+    // "function-url-no-scheme-relative": true,
+    // "liberty/use-logical-spec": "always",
+    // "selector-class-pattern": null,
+    // "alpha-value-notation": null,
+    // "color-function-notation": null,
+    // "value-keyword-case": null,
   },
 };
