df6b52b046f06a0ac552274627b9029049188e23
Andrey Antukh <niwi@niwi.nz>
2026-03-23 18:25:07 +0000

    :bug: Fix dissoc error when detaching stroke color from library
    The detach-value function in color-row was only passing index to
    on-detach, but the stroke's on-color-detach handler expects both
    index and color arguments. This caused a protocol error when trying
    to dissoc from a number instead of a map.
    
    Signed-off-by: Andrey Antukh <niwi@niwi.nz>

--- frontend/src/app/main/ui/workspace/sidebar/options/rows/color_row.cljs
+++ frontend/src/app/main/ui/workspace/sidebar/options/rows/color_row.cljs
@@ -205,10 +205,10 @@
 
         detach-value
         (mf/use-fn
-         (mf/deps on-detach index)
+         (mf/deps on-detach index color)
          (fn [_]
            (when on-detach
-             (on-detach index))))
+             (on-detach index color))))
 
         handle-select
         (mf/use-fn
