718bc1763472867a7fa20cc2a3bc49bd48b73042
Eva Marco <evamarcod@gmail.com>
2026-03-24 14:23:27 +0100

    :recycle: Update some translation strings

--- frontend/src/app/main/ui/workspace/sidebar/options/menus/token_typography_row.cljs
+++ frontend/src/app/main/ui/workspace/sidebar/options/menus/token_typography_row.cljs
@@ -16,41 +16,57 @@
    [cuerdas.core :as str]
    [rumext.v2 :as mf]))
 
-(defn- resolved-value-tooltip [token-name resolved-value]
-  (mf/html
+(mf/defc resolved-value-tooltip*
+  {::mf/private true}
+  [{:keys [token-name resolved-value]}]
+  [:*
+   [:span (dm/str (tr "workspace.tokens.token-name") ": ")]
+   [:span {:class (stl/css :token-name-tooltip)} token-name]
    [:div
-    [:span (dm/str (tr "workspace.tokens.token-name") ": ")]
-    [:span {:class (stl/css :token-name-tooltip)} token-name]
-    [:div
-     [:span (tr "inspect.tabs.styles.token-resolved-value")]
-     [:ul
-      (for [[k v] resolved-value]
-        [:li {:key (d/name k)}
-         [:span {:class (stl/css :resolved-key)} (str "- " (d/name k) ": ")]
-         [:span {:class (stl/css :resolved-value)}
-          (if (sequential? v)
-            (str/join ", " (map #(dm/str "\"" % "\"") v))
-            (dm/str v))]])]]]))
+    [:span (tr "inspect.tabs.styles.token-resolved-value")]
+    [:ul
+     (for [[k v] resolved-value]
+       [:li {:key (d/name k)}
+        [:span {:class (stl/css :resolved-key)} (str "- " (d/name k) ": ")]
+        [:span {:class (stl/css :resolved-value)}
+         (if (sequential? v)
+           (str/join ", " (map #(dm/str "\"" % "\"") v))
+           (dm/str v))]])]]])
 
 (mf/defc token-typography-row*
-  [{:keys [token-name tokens detach-token] :rest props}]
-  (let [token (->> (:typography tokens)
-                   (d/seek #(= (:name %) token-name)))
-        on-detach
-        (mf/use-fn
-         (mf/deps token-name)
-         (fn []
-           (detach-token token-name)))
-
-        element-ref (mf/use-ref nil)
+  [{:keys [token-name active-tokens detach-token] :rest props}]
+  (let [ element-ref (mf/use-ref nil)
         id (mf/use-id)
 
+        token (->> (:typography active-tokens)
+                   (d/seek #(= (:name %) token-name)))
+
+        has-errors (some? (:errors token))
+        display-name (or (:name token) token-name)
+
         resolved-value (:resolved-value token)
+        not-active (or (nil? token)
+                       (empty? (:typography active-tokens)))
+        on-detach
+        (mf/use-fn
+         (mf/deps display-name)
+         (fn []
+           (detach-token display-name)))
 
-        tooltip-content
-        #(resolved-value-tooltip token-name resolved-value)]
+        tooltip-content (cond
+                          not-active
+                          (tr "not-active-token.no-name")
+                          has-errors
+                          (tr "options.deleted-token")
+                          :else
+                           (mf/html [:> resolved-value-tooltip* {:token-name token-name
+                                                                 :resolved-value resolved-value}]))]
 
-    [:div {:class (stl/css :token-typography-row)}
+    [:div {:class (stl/css-case :token-typography-row true
+                                :token-typography-row-with-errors has-errors
+                                :token-typography-row-not-active not-active)}
+     (when (or has-errors not-active)
+       [:div {:class (stl/css :error-dot)}])
      [:> icon* {:icon-id i/text-typography
                 :class (stl/css :icon)}]
      [:> tooltip* {:content tooltip-content
@@ -61,10 +77,10 @@
       [:span {:aria-labelledby (dm/str id)
               :class (stl/css :token-name)
               :ref element-ref}
-       token-name]]
+       display-name]]
 
      [:> icon-button* {:variant "action"
-                       :aria-label (tr "ds.inputs.token-field.detach-token")
+                       :aria-label (tr "token-actions.detach-token")
                        :tooltip-class (stl/css :detach-button)
                        :tooltip-placement "top-left"
                        :on-click on-detach
