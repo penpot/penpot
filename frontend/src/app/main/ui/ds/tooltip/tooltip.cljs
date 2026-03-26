5fca9457cf6b3c08b2c117ce99bd180bb47828dd
Andrey Antukh <niwi@niwi.nz>
2026-03-26 12:45:42 +0100

    :recycle: Extract use-portal-container hook to reduce duplication (#8798)
    The dedicated-container portal pattern was repeated across 6 components.
    Extract it into a reusable use-portal-container hook under app.main.ui.hooks.

--- frontend/src/app/main/ui/ds/tooltip/tooltip.cljs
+++ frontend/src/app/main/ui/ds/tooltip/tooltip.cljs
@@ -9,6 +9,7 @@
    [app.main.style :as stl])
   (:require
    [app.common.data :as d]
+   [app.main.ui.hooks :as hooks]
    [app.util.dom :as dom]
    [app.util.keyboard :as kbd]
    [app.util.timers :as ts]
@@ -159,6 +160,8 @@
 
         tooltip-ref (mf/use-ref nil)
 
+        container (hooks/use-portal-container)
+
         id
         (d/nilv id internal-id)
 
@@ -283,4 +286,4 @@
            [:div {:class (stl/css :tooltip-content)} content]
            [:div {:class (stl/css :tooltip-arrow)
                   :id "tooltip-arrow"}]]])
-        (.-body js/document)))]))
+        container))]))
