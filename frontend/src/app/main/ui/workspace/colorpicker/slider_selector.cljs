2c9b598e536c28c29164f415bcb296b8d7ceef3e
Andrey Antukh <niwi@niwi.nz>
2026-04-02 16:26:32 +0000

    :recycle: migrate slider-selector to modern rumext * format
    Rename component to slider-selector*, convert vertical? to is-vertical
    and reverse? to is-reverse throughout the component body.

--- frontend/src/app/main/ui/workspace/colorpicker/slider_selector.cljs
+++ frontend/src/app/main/ui/workspace/colorpicker/slider_selector.cljs
@@ -13,8 +13,8 @@
    [app.util.object :as obj]
    [rumext.v2 :as mf]))
 
-(mf/defc slider-selector
-  [{:keys [value class min-value max-value vertical? reverse? on-change on-start-drag on-finish-drag type]}]
+(mf/defc slider-selector*
+  [{:keys [value class min-value max-value is-vertical is-reverse on-change on-start-drag on-finish-drag type]}]
   (let [min-value (or min-value 0)
         max-value (or max-value 1)
         dragging? (mf/use-state false)
@@ -42,17 +42,17 @@
           (when on-change
             (let [{:keys [left right top bottom]} (-> ev dom/get-target dom/get-bounding-rect)
                   {:keys [x y]} (-> ev dom/get-client-position)
-                  unit-value (if vertical?
+                  unit-value (if is-vertical
                                (mth/clamp (/ (- bottom y) (- bottom top)) 0 1)
                                (mth/clamp (/ (- x left) (- right left)) 0 1))
-                  unit-value (if reverse?
+                  unit-value (if is-reverse
                                (mth/abs (- unit-value 1.0))
                                unit-value)
                   value (+ min-value (* unit-value (- max-value min-value)))]
               (on-change value))))]
 
 
-    [:div {:class (dm/str class (stl/css-case :vertical vertical?
+    [:div {:class (dm/str class (stl/css-case :vertical is-vertical
                                               :slider-selector true
                                               :hue (= type :hue)
                                               :opacity (= type :opacity)
@@ -66,7 +66,7 @@
      (let [value-percent (* (/ (- value min-value)
                                (- max-value min-value)) 100)
 
-           value-percent (if reverse?
+           value-percent (if is-reverse
                            (mth/abs (- value-percent 100))
                            value-percent)
            value-percent-str (str value-percent "%")
@@ -75,4 +75,4 @@
            style-horizontal (obj/merge! #js {:left value-percent-str} style-common)
            style-vertical   (obj/merge! #js {:bottom value-percent-str} style-common)]
        [:div {:class (stl/css :handler)
-              :style (if vertical? style-vertical style-horizontal)}])]))
+              :style (if is-vertical style-vertical style-horizontal)}])]))
