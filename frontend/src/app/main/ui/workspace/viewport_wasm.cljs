175f122a0f14e4f75b24c11201186aa68f28ae4a
Andrey Antukh <niwi@niwi.nz>
2026-04-02 16:04:23 +0000

    :recycle: Convert viewport-scrollbars to modern * component format
    Convert viewport-scrollbars to viewport-scrollbars* using {:keys [...]}
    destructuring and update call sites in viewport.cljs and viewport_wasm.cljs
    to use [:> ...].

--- frontend/src/app/main/ui/workspace/viewport_wasm.cljs
+++ frontend/src/app/main/ui/workspace/viewport_wasm.cljs
@@ -777,7 +777,7 @@
             :view-only (not show-grid-editor?)}])]
 
        [:g.scrollbar-wrapper {:clipPath "url(#clip-handlers)"}
-        [:& scroll-bars/viewport-scrollbars
+        [:> scroll-bars/viewport-scrollbars*
          {:objects base-objects
           :zoom zoom
           :vbox vbox
