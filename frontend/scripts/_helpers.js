1d55f275ced13178853a251440d8439dd7fe4a77
Andrey Antukh <niwi@niwi.nz>
2026-04-22 10:01:38 +0200

    :sparkles: Allow render entrypoint load alternative config
    The render entrypoint is used by exporter

--- frontend/scripts/_helpers.js
+++ frontend/scripts/_helpers.js
@@ -207,6 +207,7 @@ async function generateManifest() {
     rasterizer_main: "./js/rasterizer.js",
 
     config: "./js/config.js?version=" + VERSION_TAG,
+    config_render: "./js/config-render.js?version=" + VERSION_TAG,
     polyfills: "./js/polyfills.js?version=" + VERSION_TAG,
     libs: "./js/libs.js?version=" + VERSION_TAG,
     default_translations: "./js/translation.en.js?version=" + VERSION_TAG,
