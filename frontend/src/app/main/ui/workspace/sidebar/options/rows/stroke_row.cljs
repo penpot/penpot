70f6be0750e8700be9f91ed2c5d4029331636224
Dexterity104 <hatanokanjiro@gmail.com>
2026-05-04 16:08:26 +0000

    :fire: Remove stray prn debug log in stroke-row* render

--- frontend/src/app/main/ui/workspace/sidebar/options/rows/stroke_row.cljs
+++ frontend/src/app/main/ui/workspace/sidebar/options/rows/stroke_row.cljs
@@ -205,8 +205,6 @@
      (when (some? on-reorder)
        [:> reorder-handler* {:ref dref}])
 
-     (prn "stroke-row*" applied-tokens)
-
      ;; Stroke Color
      ;; FIXME: memorize stroke color
      [:div {:class (stl/css :stroke-color-actions)}
