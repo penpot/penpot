bdfb574a5525d21854fb4ea47ff82088144bf121
Dexterity104 <hatanokanjiro@gmail.com>
2026-05-07 21:08:07 +0000

    :recycle: Migrate render-wasm api object-svg to modern component syntax

--- frontend/src/app/render_wasm/api.cljs
+++ frontend/src/app/render_wasm/api.cljs
@@ -237,7 +237,7 @@
        (.postMessage (.-port2 ch) nil)))))
 
 ;; Based on app.main.render/object-svg
-(mf/defc object-svg
+(mf/defc object-svg*
   {::mf/props :obj}
   [{:keys [shape] :as props}]
   (let [objects (mf/deref refs/workspace-page-objects)
@@ -261,7 +261,7 @@
 (defn get-static-markup
   [shape]
   (->
-   (mf/element object-svg #js {:shape shape})
+   (mf/element object-svg* #js {:shape shape})
    (rds/renderToStaticMarkup)))
 
 ;; forward declare helpers so render can call them
