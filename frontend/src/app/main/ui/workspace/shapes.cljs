8a8e9fbd067e8633caa2ca78da12a1fbccf20c1b
Dexterity104 <hatanokanjiro@gmail.com>
2026-05-10 19:34:09 +0000

    :zap: Avoid subscribing non-root shapes to active-frames context

--- frontend/src/app/main/ui/workspace/shapes.cljs
+++ frontend/src/app/main/ui/workspace/shapes.cljs
@@ -92,29 +92,15 @@
               :thumbnail? (not (contains? active-frames (dm/get-prop shape :id)))}]
             [:& shape-wrapper {:shape shape}])])]]]))
 
-(mf/defc shape-wrapper
-  {::mf/wrap [#(mf/memo' % common/check-shape-props)]
-   ::mf/wrap-props false}
-  [props]
-  (let [shape      (unchecked-get props "shape")
-        shape-type (dm/get-prop shape :type)
-        shape-id   (dm/get-prop shape :id)
-
-        all-active-frames (mf/use-ctx ctx/active-frames)
-        active-frames     (when (cfh/root-frame? shape) all-active-frames)
-
-        thumbnail?
-        (and (some? active-frames)
-             (not (contains? active-frames shape-id)))
-
-        props         #js {:shape shape :thumbnail? thumbnail?}
-
+(defn- render-shape-content
+  [shape thumbnail?]
+  (let [shape-type    (dm/get-prop shape :type)
         rawsvg?       (= :svg-raw shape-type)
         wrapper-elem  (if ^boolean rawsvg? mf/Fragment "g")
         wrapper-props (if ^boolean rawsvg?
                         #js {}
-                        #js {:className "workspace-shape-wrapper"})]
-
+                        #js {:className "workspace-shape-wrapper"})
+        props         #js {:shape shape :thumbnail? thumbnail?}]
     (when (and (some? shape)
                (not ^boolean (:hidden shape)))
       [:> wrapper-elem wrapper-props
@@ -130,6 +116,26 @@
          :frame   [:> nested-frame-wrapper props]
 
          nil)])))
+
+(mf/defc root-frame-shape-wrapper
+  {::mf/wrap [#(mf/memo' % common/check-shape-props)]
+   ::mf/wrap-props false}
+  [props]
+  (let [shape         (unchecked-get props "shape")
+        shape-id      (dm/get-prop shape :id)
+        active-frames (mf/use-ctx ctx/active-frames)
+        thumbnail?    (and (some? active-frames)
+                           (not (contains? active-frames shape-id)))]
+    (render-shape-content shape thumbnail?)))
+
+(mf/defc shape-wrapper
+  {::mf/wrap [#(mf/memo' % common/check-shape-props)]
+   ::mf/wrap-props false}
+  [props]
+  (let [shape (unchecked-get props "shape")]
+    (if ^boolean (cfh/root-frame? shape)
+      [:> root-frame-shape-wrapper props]
+      (render-shape-content shape false))))
 
 (def group-wrapper (group/group-wrapper-factory shape-wrapper))
 (def svg-raw-wrapper (svg-raw/svg-raw-wrapper-factory shape-wrapper))
