a2aeedbf5e82d0073acbf20ed9cdb1373e056bd1
María Valderrama <mavalroot@gmail.com>
2026-05-12 12:29:24 +0200

    :sparkles: Restrict team delete to owners, prep org-owner flow

--- backend/src/app/rpc/commands/teams.clj
+++ backend/src/app/rpc/commands/teams.clj
@@ -780,28 +780,28 @@
         team  (if (contains? cf/flags :nitrate)
                 (nitrate/add-org-info-to-team cfg team params)
                 team)
-        perms (get team :permissions)]
+        perms (get team :permissions)
+        org   (:organization team)
+        in-org? (and (contains? cf/flags :nitrate) org)
+        can-delete?
+        (if in-org?
+          (nitrate-perms/allowed? :delete-team
+                                  {:org-perms {:owner-id    (dm/get-in team [:organization :owner-id])
+                                               :permissions (dm/get-in team [:organization :permissions])}
+                                   :profile-id profile-id
+                                   :team-perms perms
+                                   ;; `onlyMe` is for a future org-level flow.
+                                   :allow-org-owner-delete? false})
+          (boolean (:is-owner perms)))]
+
+    (when-not can-delete?
+      (ex/raise :type :validation
+                :code :only-owner-can-delete-team))
 
     (when (:is-default team)
       (ex/raise :type :validation
                 :code :non-deletable-team
                 :hint "impossible to delete default team"))
-
-    ;; Check delete permissions based on organization settings.
-    ;; For non-org teams or when nitrate is disabled, only owners can delete.
-    (if (and (:organization team) (contains? cf/flags :nitrate))
-      (let [org-perms {:owner-id    (dm/get-in team [:organization :owner-id])
-                       :permissions (dm/get-in team [:organization :permissions])}]
-        (when-not (nitrate-perms/allowed? :delete-team
-                                          {:org-perms  org-perms
-                                           :profile-id profile-id
-                                           :team-perms perms})
-          (ex/raise :type :validation
-                    :code :not-allowed
-                    :hint "You are not allowed to delete teams in this organization")))
-      (when-not (:is-owner perms)
-        (ex/raise :type :validation
-                  :code :only-owner-can-delete-team)))
 
     (let [delay (ldel/get-deletion-delay team)
           team  (db/update! conn :team
