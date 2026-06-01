af81818b9730743ced84072169f6a59d64530ae7
Juan de la Cruz <delacruzgarciajuan@gmail.com>
2026-06-01 09:59:10 +0200

    :sparkles: Add new release 2.16 slides (#9940)
    * :sparkles: Add new slides for the 2.16 release
    
    * :tada: Add new slide gif images

--- frontend/src/app/main/ui/releases.cljs
+++ frontend/src/app/main/ui/releases.cljs
@@ -34,6 +34,7 @@
    [app.main.ui.releases.v2-13]
    [app.main.ui.releases.v2-14]
    [app.main.ui.releases.v2-15]
+   [app.main.ui.releases.v2-16]
    [app.main.ui.releases.v2-2]
    [app.main.ui.releases.v2-3]
    [app.main.ui.releases.v2-4]
@@ -106,4 +107,4 @@
 
 (defmethod rc/render-release-notes "0.0"
   [params]
-  (rc/render-release-notes (assoc params :version "2.15")))
+  (rc/render-release-notes (assoc params :version "2.16")))
