ca1b5541c04488771652f535af86e20d57210b1a
Andrés Moya <andres.moya@kaleidos.net>
2026-06-05 14:44:09 +0200

    :wrench: Add separated TokenStatus to store status apart of TokensLib

--- frontend/test/frontend_tests/runner.cljs
+++ frontend/test/frontend_tests/runner.cljs
@@ -73,6 +73,7 @@
    [frontend-tests.tokens.logic.token-actions-test]
    [frontend-tests.tokens.logic.token-data-test]
    [frontend-tests.tokens.logic.token-remapping-test]
+   [frontend-tests.tokens.logic.tokens-status-test]
    [frontend-tests.tokens.style-dictionary-test]
    [frontend-tests.tokens.token-errors-test]
    [frontend-tests.tokens.workspace-tokens-remap-test]
@@ -179,6 +180,7 @@
    'frontend-tests.tokens.logic.token-remapping-test
    'frontend-tests.tokens.style-dictionary-test
    'frontend-tests.tokens.token-errors-test
+   'frontend-tests.tokens.logic.tokens-status-test
    'frontend-tests.tokens.workspace-tokens-remap-test
    'frontend-tests.ui.check-updates-test
    'frontend-tests.ui.colorpicker-token-set-order-test
