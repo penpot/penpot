b2439694af99ac362b63141ea2ec8287410f3a95
Alonso Torres <alonso.torres@kaleidos.net>
2026-06-12 09:10:36 +0200

    :bug: Fix float precision in typography line-height/letter-spacing string conversion (#9973)

--- frontend/src/app/main/ui/workspace/sidebar/options/menus/typography.cljs
+++ frontend/src/app/main/ui/workspace/sidebar/options/menus/typography.cljs
@@ -382,7 +382,7 @@
         letter-spacing (or letter-spacing "0")
         handle-change
         (fn [value attr]
-          (on-change {attr (str value)}))]
+          (on-change {attr (ust/format-precision value 2)}))]
 
     [:div {:class (stl/css :spacing-options)}
      [:div {:class (stl/css :line-height)
