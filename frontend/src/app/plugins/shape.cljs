dc9a0d1e52bb143a6ae629606a8dadb181b7c403
alonso.torres <alonso.torres@kaleidos.net>
2026-06-16 12:54:57 +0200

    :bug: Add fixedWhenScrolling to API

--- frontend/src/app/plugins/shape.cljs
+++ frontend/src/app/plugins/shape.cljs
@@ -361,6 +361,22 @@
                   :else
                   (st/emit! (dwsh/update-shapes [id] #(assoc % :constraints-v value))))))}
 
+           :fixedWhenScrolling
+           {:this true
+            :get #(-> % u/proxy->shape :fixed-scroll boolean)
+            :set
+            (fn [self value]
+              (cond
+                (not (boolean? value))
+                (u/not-valid plugin-id :fixedWhenScrolling value)
+
+                (not (r/check-permission plugin-id "content:write"))
+                (u/not-valid plugin-id :fixedWhenScrolling "Plugin doesn't have 'content:write' permission")
+
+                :else
+                (let [id (obj/get self "$id")]
+                  (st/emit! (dwsh/update-shapes [id] #(assoc % :fixed-scroll value))))))}
+
            :borderRadius
            {:this true
             :get #(-> % u/proxy->shape :r1)
