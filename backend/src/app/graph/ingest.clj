bb6c04012b7ed07f41c732332816e025cd5e5b9d
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2026-07-14 12:07:37 +0200

    :sparkles: Add debug graph console for in-memory Cypher queries

--- backend/src/app/graph/ingest.clj
+++ backend/src/app/graph/ingest.clj
@@ -18,14 +18,14 @@
    [app.graph.project.transforms :as project.transforms]
    [app.graph.schema :as schema]
    [app.graph.stats :as stats]
-   [app.srepl.helpers :as h]))
+   [app.srepl.helpers :as h])
+  (:import
+   com.ladybugdb.Connection))
 
-(defn ingest-file!
-  [system file-id & {:keys [db-path reset-db? skip-stats?]
-                     :or   {reset-db? true}}]
+(defn- fetch-file!
+  [system file-id]
   (let [file-id (h/parse-uuid file-id)
-        file    (db/run! system #(bfc/get-file % file-id :realize? true))
-        db-path (or db-path (ladybug/db-path-for-file file-id))]
+        file    (db/run! system #(bfc/get-file % file-id :realize? true))]
     (when-not file
       (ex/raise :type :not-found
                 :code :file-not-found
@@ -35,30 +35,48 @@
                 :code :file-without-data
                 :hint "file has no data to project"
                 :file-id (str file-id)))
-    (ctf/check-file-data (:data file))
-    (when reset-db?
-      (ladybug/reset-db-path! db-path))
+    [file-id file]))
+
+(defn ingest-on-connection!
+  "Project `file-id` into an already open Ladybug `conn`."
+  [system ^Connection conn file-id & {:keys [db-path skip-stats? skip-validation?]
+                                      :or   {skip-stats? true}}]
+  (let [[file-id file] (fetch-file! system file-id)
+        db-path        (or db-path (ladybug/db-path-for-file file-id))
+        data           (:data file)]
+    (when-not skip-validation?
+      (ctf/check-file-data data))
     (l/inf :hint "graph ingest"
            :file-id (str file-id)
            :revn (:revn file)
            :db-path db-path
            :schema schema/schema-version)
-    (let [data              (:data file)
-          ddl               (schema/ddl-statements)
+    (let [ddl          (schema/ddl-statements)
           {:keys [nodes edges stats]}
           (project.document/projection-data data file)
-          staging-path      (bulk/staging-dir db-path file-id)]
-      (ladybug/with-connection! db-path
-        (fn [conn]
-          (ladybug/exec-on-connection! conn ddl)
-          (bulk/load-projection! conn {:nodes nodes :edges edges} staging-path)
-          (ladybug/exec-on-connection! conn ["CHECKPOINT;"])
-          {:file-id        file-id
-           :revn           (:revn file)
-           :name           (or (:name data) (:name file))
-           :db-path        db-path
-           :schema-version schema/schema-version
-           :projection     {:stats stats}
-           :transforms     (project.transforms/apply-transforms! system db-path data file)
-           :stats          (when-not skip-stats?
-                             (stats/summarize-connection conn))})))))
+          staging-path (bulk/staging-dir db-path file-id)]
+      (ladybug/exec-on-connection! conn ddl)
+      (bulk/load-projection! conn {:nodes nodes :edges edges} staging-path)
+      (ladybug/exec-on-connection! conn ["CHECKPOINT;"])
+      {:file-id        file-id
+       :revn           (:revn file)
+       :name           (or (:name data) (:name file))
+       :db-path        db-path
+       :schema-version schema/schema-version
+       :projection     {:stats stats}
+       :transforms     (project.transforms/apply-transforms! system db-path data file)
+       :stats          (when-not skip-stats?
+                         (stats/summarize-connection conn))})))
+
+(defn ingest-file!
+  [system file-id & {:keys [db-path reset-db? skip-stats? skip-validation?]
+                     :or   {reset-db? true}}]
+  (let [db-path (or db-path (ladybug/db-path-for-file (h/parse-uuid file-id)))]
+    (when reset-db?
+      (ladybug/reset-db-path! db-path))
+    (ladybug/with-connection! db-path
+      (fn [conn]
+        (ingest-on-connection! system conn file-id
+                               :db-path db-path
+                               :skip-stats? skip-stats?
+                               :skip-validation? skip-validation?)))))
