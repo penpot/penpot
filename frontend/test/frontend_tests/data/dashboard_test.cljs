a490a449fc8dc8855467aef43e56ff51297c3b5d
Pablo Alba <pablo.alba@kaleidos.net>
2026-07-30 10:46:39 +0200

    :bug: Fix nitrate sso doesn't notify workspace on activation

--- frontend/test/frontend_tests/data/dashboard_test.cljs
+++ frontend/test/frontend_tests/data/dashboard_test.cljs
@@ -8,7 +8,7 @@
   (:require
    [app.common.uuid :as uuid]
    [app.config :as cf]
-   [app.main.data.dashboard :as dd]
+   [app.main.data.common :as dcm]
    [app.main.repo :as rp]
    [app.main.router :as rt]
    [beicon.v2.core :as rx]
@@ -23,9 +23,43 @@
           current-url  (str "https://penpot.example.com/#/dashboard/recent?team-id=" team-id)
           redirect-url "https://idp.example.com/authorize"
           state        {:current-team-id team-id}
-          event        (dd/handle-change-team-org
+          event        (dcm/handle-change-team-org
                         {:team {:id team-id :organization organization}
                          :notification nil})]
+      (mock/with-mocks
+        {cf/flags (conj cf/flags :nitrate)
+         rp/cmd! (mock/stub
+                  (fn [cmd params]
+                    (if (= [:check-nitrate-sso
+                            {:team-id team-id :url current-url}]
+                           [cmd params])
+                      (rx/of {:authorized false :redirect-uri redirect-url})
+                      (rx/throw (ex-info "unexpected RPC" {:cmd cmd :params params})))))
+         rt/get-current-href (mock/stub (constantly current-url))}
+        (fn [done']
+          (->> (ptk/watch event state nil)
+               (rx/reduce conj [])
+               (rx/subs!
+                (fn [events]
+                  (t/is (= [::rt/nav-raw] (mapv ptk/type events))))
+                (fn [error]
+                  (t/is false (str "unexpected error: " error))
+                  (done'))
+                (fn []
+                  (done')))))
+        done))))
+
+(t/deftest organization-sso-activation-redirects-current-team
+  (t/async done
+    (let [team-id      (uuid/next)
+          org-id       (uuid/next)
+          current-url  (str "https://penpot.example.com/#/workspace?team-id=" team-id)
+          redirect-url "https://idp.example.com/authorize"
+          state        {:current-team-id team-id
+                        :teams {team-id {:id team-id
+                                         :organization {:id org-id}}}}
+          event        (dcm/handle-organization-change-sso
+                        {:organization-id org-id})]
       (mock/with-mocks
         {cf/flags (conj cf/flags :nitrate)
          rp/cmd! (mock/stub
