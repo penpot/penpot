c06246ce6ea50348e36c43f080e4ec5376036013
Luis de Dios <luis.dedios@kaleidos.net>
2026-08-05 18:50:38 +0200

    :bug: Fix remove unneeded common password check
    The dictionary check is only relevant for passwords that meet all other requirements, but all 10,000 common passwords would fail the character requirements, so this check is not needed

--- backend/src/app/auth/passwords.clj
+++ backend/src/app/auth/passwords.clj
@@ -7,28 +7,15 @@
 (ns app.auth.passwords
   "Password strength validation using Passay library."
   (:require
-   [app.common.exceptions :as ex]
-   [clojure.java.io :as io])
+   [app.common.exceptions :as ex])
   (:import
-   [org.passay CharacterCharacteristicsRule CharacterRule DictionaryRule EnglishCharacterData PasswordData]
-   [org.passay.dictionary ArrayWordList WordListDictionary]))
+   [org.passay CharacterCharacteristicsRule CharacterRule EnglishCharacterData PasswordData]))
 
 (defonce ^:private passay-code->translation-key
-  {"TOO_SHORT"                    "errors.weak-password.too-short"
-   "IN_DICTIONARY"                "errors.weak-password.in-dictionary"
-   "INSUFFICIENT_LOWERCASE"       "errors.weak-password.insufficient-lowercase"
-   "INSUFFICIENT_UPPERCASE"       "errors.weak-password.insufficient-uppercase"
-   "INSUFFICIENT_DIGIT"           "errors.weak-password.insufficient-digits"
-   "INSUFFICIENT_SPECIAL"         "errors.weak-password.insufficient-special"})
-
-(defonce ^:private dictionary
-  (let [lines (line-seq (io/reader (io/resource "app/common-passwords.txt")))
-        words (into-array String (sort lines))
-        word-list (ArrayWordList. words)]
-    (WordListDictionary. word-list)))
-
-(defonce ^:private dictionary-rule
-  (DictionaryRule. dictionary))
+  {"INSUFFICIENT_LOWERCASE"   "errors.weak-password.insufficient-lowercase"
+   "INSUFFICIENT_UPPERCASE"   "errors.weak-password.insufficient-uppercase"
+   "INSUFFICIENT_DIGIT"       "errors.weak-password.insufficient-digits"
+   "INSUFFICIENT_SPECIAL"     "errors.weak-password.insufficient-special"})
 
 (defonce ^:private character-characteristics-rule
   (doto (CharacterCharacteristicsRule.)
@@ -46,8 +33,7 @@
    - At least 1 lowercase letter
    - At least 1 uppercase letter
    - At least 1 digit
-   - At least 1 special character
-   - Not in common password dictionary"
+   - At least 1 special character"
   [password]
   (when (< (count password) 8)
     (ex/raise :type :validation
@@ -55,23 +41,13 @@
               :hint "password must be at least 8 characters"
               :details ["errors.weak-password.too-short"]))
 
-  (let [password-data (PasswordData. password)]
-    (let [char-result (.validate character-characteristics-rule password-data)]
-      (when-not (.isValid char-result)
-        (ex/raise :type :validation
-                  :code :weak-password
-                  :hint "password must contain at least 1 lowercase letter, 1 uppercase letter, 1 digit, and 1 special character"
-                  :details (->> (.getDetails char-result)
-                                (mapv #(.getErrorCode %))
-                                (mapv passay-code->translation-key)
-                                (filterv some?)))))
-
-    (let [dict-result (.validate dictionary-rule password-data)]
-      (when-not (.isValid dict-result)
-        (ex/raise :type :validation
-                  :code :weak-password
-                  :hint "password is too common"
-                  :details (->> (.getDetails dict-result)
-                                (mapv #(.getErrorCode %))
-                                (mapv passay-code->translation-key)
-                                (filterv some?)))))))
+  (let [password-data (PasswordData. password)
+        char-result   (.validate character-characteristics-rule password-data)]
+    (when-not (.isValid char-result)
+      (ex/raise :type :validation
+                :code :weak-password
+                :hint "password must contain at least 1 lowercase letter, 1 uppercase letter, 1 digit, and 1 special character"
+                :details (->> (.getDetails char-result)
+                              (mapv #(.getErrorCode %))
+                              (mapv passay-code->translation-key)
+                              (filterv some?))))))
