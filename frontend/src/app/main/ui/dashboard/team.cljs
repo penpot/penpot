e93c41978516e29f505254417b5c0ec09d3d8023
María Valderrama <mavalroot@gmail.com>
2026-08-24 15:25:31 +0200

    :bug: Fix wrong copy on disabled invite button tooltip (#11311)

--- frontend/src/app/main/ui/dashboard/team.cljs
+++ frontend/src/app/main/ui/dashboard/team.cljs
@@ -140,23 +140,16 @@
         [:a {:on-click on-nav-settings} (tr "labels.settings")]]]]
      [:div {:class (stl/css :dashboard-buttons)}
       (when (and (or invitations-section? members-section?) (not-empty invitations))
-        (let [organization          (:organization team)
-              owners-only-invites?  (and (contains? cfg/flags :admin-console)
-                                         organization
-                                         (= (get-in organization [:permissions :send-invitations]) "owners"))
-              title-text            (if owners-only-invites?
-                                      (tr "dashboard.invite-profile-disabled.owners-only" (:name organization))
-                                      (tr "dashboard.invite-profile-disabled"))
-              invite-button         (mf/html
-                                     [:> button* {:class (stl/css :invite-button)
-                                                  :variant "secondary"
-                                                  :on-click on-invite-member
-                                                  :disabled (not can-invite?)
-                                                  :data-testid "invite-member"}
-                                      (tr "dashboard.invite-profile")])]
+        (let [invite-button (mf/html
+                             [:> button* {:class (stl/css :invite-button)
+                                          :variant "secondary"
+                                          :on-click on-invite-member
+                                          :disabled (not can-invite?)
+                                          :data-testid "invite-member"}
+                              (tr "dashboard.invite-profile")])]
           (if can-invite?
             invite-button
-            [:> tooltip* {:content title-text
+            [:> tooltip* {:content (tr "dashboard.invite-profile-disabled")
                           :id "invite-member-disabled-tooltip"
                           :tab-index 0}
              invite-button])))]]))
