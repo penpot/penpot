Hologram-Technologies/hologram-brand-kit

:sparkles: Add export job model, store and scheduler to exporter

exporter/src/app/jobs/scheduler.cljs
@@ -1,0 +1,133 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; Copyright (c) KALEIDOS INC Sucursal en España SL
+
+(ns app.jobs.scheduler
+  "Admission control for export jobs.
+
+  Limits concurrent jobs and rejects work rather than allowing an unbounded backlog.
+  Queue order is FIFO, except jobs whose profile is already at its cap are skipped."
+  (:require
+   [app.common.exceptions :as ex]
+   [app.common.logging :as l]
+   [app.config :as cf]
+   [app.jobs :as jobs]
+   [app.jobs.utils :as job.utils]
+   [promesa.core :as p]))
+
+(l/set-level! :debug)
+
+(defonce ^:private state
+  (atom {:running {}   ;; job-id -> profile-id
+         :queue []}))  ;; vector of {:job :resolve :reject}
+
+(defn- max-concurrent [] (cf/get :export-max-concurrent-jobs 4))
+(defn- max-per-profile [] (cf/get :export-max-jobs-per-profile 2))
+(defn- max-queued [] (cf/get :export-queue-max 64))
+
+(defn- running-for
+  [{:keys [running]} profile-id]
+  (count (filter #(= profile-id %) (vals running))))
+
+(defn- eligible?
+  [state profile-id]
+  (and (< (count (:running state)) (max-concurrent))
+       (< (running-for state profile-id) (max-per-profile))))
+
+(declare ^:private pump!)
+
+(defn- finish!
+  [job-id]
+  (swap! state update :running dissoc (str job-id))
+  (jobs/release! job-id)
+  (job.utils/release! job-id)
+  (pump!))
+
+(defn- execute!
+  [{:keys [id profile-id] :as job}]
+  (swap! state update :running assoc (str id) profile-id)
+  (if (jobs/cancelled? id)
+    (do (finish! id)
+        (p/resolved job))
+    (let [run-fn (jobs/run-fn id)]
+      (->> (p/do (jobs/start! job))
+           (p/mcat (fn [job] (p/do (run-fn job))))
+           (p/fnly (fn [_ _] (finish! id)))))))
+
+(defn- drop-queued!
+  "Removes a queued job and settles its promise, freeing its queue slot on cancellation."
+  [job-id]
+  (let [entry (volatile! nil)]
+    (swap! state (fn [state]
+                   (let [queue (:queue state)
+                         idx   (->> (map-indexed vector queue)
+                                    (some (fn [[idx entry]]
+                                            (when (= (str job-id) (str (-> entry :job :id)))
+                                              idx))))]
+                     (if idx
+                       (do (vreset! entry (nth queue idx))
+                           (assoc state :queue (into (subvec queue 0 idx) (subvec queue (inc idx)))))
+                       state))))
+    (when-let [{:keys [resolve]} @entry]
+      (jobs/release! job-id)
+      (job.utils/release! job-id)
+      (resolve nil))))
+
+(defn- take-eligible
+  "Pops the first queued entry that can run now, or nil."
+  [state]
+  (let [queue (:queue state)
+        idx   (->> (map-indexed vector queue)
+                   (some (fn [[idx entry]]
+                           (when (eligible? state (-> entry :job :profile-id))
+                             idx))))]
+    (when idx
+      [(assoc state :queue (into (subvec queue 0 idx) (subvec queue (inc idx))))
+       (nth queue idx)])))
+
+(defn- pump!
+  []
+  (loop []
+    (let [entry (volatile! nil)]
+      (swap! state (fn [state]
+                     (if-let [[next-state next-entry] (take-eligible state)]
+                       (do (vreset! entry next-entry) next-state)
+                       (do (vreset! entry nil) state))))
+      (when-let [{:keys [job resolve reject]} @entry]
+        (-> (execute! job)
+            (p/then resolve)
+            (p/catch reject))
+        (recur)))))
+
+(defn submit!
+  "Registers `job` for execution. Returns a promise of the job's result, which
+  resolves when the export actually finishes; callers that only need the handle
+  can ignore it. Raises when the exporter is saturated."
+  [job]
+  (let [resolve* (volatile! nil)
+        reject*  (volatile! nil)
+        pending  (p/create (fn [resolve reject]
+                             (vreset! resolve* resolve)
+                             (vreset! reject* reject)))]
+    (if (eligible? @state (:profile-id job))
+      (-> (execute! job)
+          (p/then @resolve*)
+          (p/catch @reject*))
+
+      (let [queued (volatile! false)]
+        (swap! state (fn [state]
+                       (if (< (count (:queue state)) (max-queued))
+                         (do (vreset! queued true)
+                             (update state :queue conj {:job job
+                                                        :resolve @resolve*
+                                                        :reject @reject*}))
+                         (do (vreset! queued false) state))))
+        (when-not @queued
+          (ex/raise :type :validation
+                    :code :queue-full
+                    :hint "too many queued export jobs"))
+        (jobs/on-cancel (:id job) (fn [] (drop-queued! (:id job))))
+        (l/dbg :hint "export job queued" :job-id (str (:id job)))))
+    pending))

exporter/src/app/jobs/store.cljs
@@ -1,0 +1,87 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; Copyright (c) KALEIDOS INC Sucursal en España SL
+
+(ns app.jobs.store
+  "Redis persistence for export jobs.
+
+  Stores each job as a single blob with a TTL matching the exported file,
+  so records expire with their files.
+
+  Reads use Redis. Cancellation requires the process running the job."
+  (:require
+   [app.common.logging :as l]
+   [app.common.transit :as t]
+   [app.config :as cf]
+   [app.redis :as redis]
+   [promesa.core :as p]))
+
+(def ^:private cancel-topic "export.job-cancel")
+
+(defn- job-key
+  [job-id]
+  (redis/->key "export.job." job-id))
+
+(defn- ttl
+  []
+  (cf/get :export-job-ttl 3600))
+
+(defn persist!
+  "Writes the job record and refreshes its TTL.
+
+  If the write fails, log it and continue. The export still runs
+  and publishes websocket updates, but the job can't be fetched
+  afterward (fetch returns nil, REST returns 404)."
+  [{:keys [id state] :as job}]
+  (let [jkey (job-key id)]
+    (->> (p/do
+           (redis/hset! jkey {:data (t/encode-str job)})
+           (redis/expire! jkey (ttl))
+           job)
+         (p/merr (fn [cause]
+                   (if (= :redis-not-available (:code (ex-data cause)))
+                     (l/warn :hint "job record not persisted, no redis connection"
+                             :job-id (str id) :state state)
+                     (l/error :hint "unable to persist job record"
+                              :job-id (str id) :state state :cause cause))
+                   (p/resolved job))))))
+
+(defn fetch
+  "The job record, or nil when unknown or expired."
+  [job-id]
+  (->> (redis/hgetall (job-key job-id))
+       (p/fmap (fn [data]
+                 (when-let [blob (get data "data")]
+                   (try
+                     (t/decode-str blob)
+                     (catch :default cause
+                       (l/warn :hint "unable to decode job record" :job-id (str job-id) :cause cause)
+                       nil)))))))
+
+(defn fetch-all
+  []
+  (->> (redis/scan (redis/->key "export.job.*"))
+       (p/mcat (fn [keys]
+                 (->> (map (fn [k]
+                             (->> (redis/hgetall k)
+                                  (p/fmap (fn [data]
+                                            (when-let [blob (get data "data")]
+                                              (try
+                                                (t/decode-str blob)
+                                                (catch :default _ nil)))))))
+                           keys)
+                      (p/all))))
+       (p/fmap (fn [jobs] (vec (remove nil? jobs))))))
+
+(defn request-cancel!
+  "Asks every exporter to cancel `job-id`. Only the one running it will act."
+  [job-id]
+  (redis/pub! cancel-topic (str job-id)))
+
+(defn on-cancel-request
+  "Registers `handler` (fn of the job-id string) for cancel requests. Returns an
+  unsubscribe fn."
+  [handler]
+  (redis/sub! cancel-topic handler))

exporter/src/app/jobs/utils.cljs
@@ -1,0 +1,86 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; Copyright (c) KALEIDOS INC Sucursal en España SL
+
+(ns app.jobs.utils
+  "Temp file ownership for export jobs.
+
+  Temp files used to be cleaned only by the per-file timer in `app.util.shell`,
+  an hour after creation and lost entirely on restart. Here each job owns the
+  paths it creates, so they are dropped as soon as it settles and whatever a
+  crash left behind is cleaned at boot."
+  (:require
+   ["node:fs/promises" :as fsp]
+   ["node:path" :as path]
+   [app.common.logging :as l]
+   [app.config :as cf]
+   [app.util.shell :as sh]
+   [cuerdas.core :as str]
+   [promesa.core :as p]))
+
+(def ^:private managed-prefix "penpot.")
+
+(defonce ^:private tracked (atom {}))
+
+(defn track!
+  "Registers `path` as owned by `job-id`, so it is removed when the job settles."
+  [job-id path]
+  (when (and job-id path)
+    (swap! tracked update (str job-id) (fnil conj #{}) path))
+  path)
+
+(defn- remove-path!
+  [path]
+  (->> (p/do (fsp/rm path #js {:recursive true :force true}))
+       (p/merr (fn [cause]
+                 (l/warn :hint "unable to remove job temp file" :path path :cause cause)
+                 (p/resolved nil)))))
+
+(defn release!
+  "Removes every file the job owns. Called once the job reached a terminal
+  state and its result has already been uploaded, so nothing else reads them."
+  [job-id]
+  (let [k     (str job-id)
+        paths (get @tracked k)]
+    (swap! tracked dissoc k)
+    (if (seq paths)
+      (->> (map remove-path! paths)
+           (p/all)
+           (p/fmap (fn [_]
+                     (l/dbg :hint "released job temp files" :job-id k :count (count paths))
+                     nil)))
+      (p/resolved nil))))
+
+(defn- clean!
+  "Removes managed temp files older than the job TTL. They can only be leftovers
+  of a previous process: every live one belongs to a job of this process."
+  []
+  (let [max-age (* 1000 (cf/get :export-job-ttl 3600))
+        now     (js/Date.now)]
+    (->> (p/do (fsp/readdir sh/tmpdir))
+         (p/mcat (fn [entries]
+                   (->> (filter #(str/starts-with? % managed-prefix) entries)
+                        (map (fn [entry]
+                               (let [fpath (path/join sh/tmpdir entry)]
+                                 (->> (p/do (fsp/stat fpath))
+                                      (p/mcat (fn [^js stat]
+                                                (if (> (- now (inst-ms (.-mtime stat))) max-age)
+                                                  (->> (remove-path! fpath)
+                                                       (p/fmap (constantly 1)))
+                                                  (p/resolved 0))))
+                                      (p/merr (fn [_] (p/resolved 0)))))))
+                        (p/all))))
+         (p/fmap (fn [results]
+                   (let [removed (reduce + 0 results)]
+                     (when (pos? removed)
+                       (l/info :hint "removed orphaned export temp files" :count removed))
+                     removed)))
+         (p/merr (fn [cause]
+                   (l/warn :hint "temp file cleanup failed" :cause cause)
+                   (p/resolved 0))))))
+
+(defn init
+  []
+  (clean!))

penpot/penpot

exporter/src/app/jobs/scheduler.cljs
@@ -1,0 +1,133 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; Copyright (c) KALEIDOS INC Sucursal en España SL
+
+(ns app.jobs.scheduler
+  "Admission control for export jobs.
+
+  Limits concurrent jobs and rejects work rather than allowing an unbounded backlog.
+  Queue order is FIFO, except jobs whose profile is already at its cap are skipped."
+  (:require
+   [app.common.exceptions :as ex]
+   [app.common.logging :as l]
+   [app.config :as cf]
+   [app.jobs :as jobs]
+   [app.jobs.utils :as job.utils]
+   [promesa.core :as p]))
+
+(l/set-level! :debug)
+
+(defonce ^:private state
+  (atom {:running {}   ;; job-id -> profile-id
+         :queue []}))  ;; vector of {:job :resolve :reject}
+
+(defn- max-concurrent [] (cf/get :exporter-max-concurrent-jobs 4))
+(defn- max-per-profile [] (cf/get :exporter-max-jobs-per-profile 2))
+(defn- max-queued [] (cf/get :exporter-queue-max 64))
+
+(defn- running-for
+  [{:keys [running]} profile-id]
+  (count (filter #(= profile-id %) (vals running))))
+
+(defn- eligible?
+  [state profile-id]
+  (and (< (count (:running state)) (max-concurrent))
+       (< (running-for state profile-id) (max-per-profile))))
+
+(declare ^:private pump!)
+
+(defn- finish!
+  [job-id]
+  (swap! state update :running dissoc (str job-id))
+  (jobs/release! job-id)
+  (job.utils/release! job-id)
+  (pump!))
+
+(defn- execute!
+  [{:keys [id profile-id] :as job}]
+  (swap! state update :running assoc (str id) profile-id)
+  (if (jobs/cancelled? id)
+    (do (finish! id)
+        (p/resolved job))
+    (let [run-fn (jobs/run-fn id)]
+      (->> (p/do (jobs/start! job))
+           (p/mcat (fn [job] (p/do (run-fn job))))
+           (p/fnly (fn [_ _] (finish! id)))))))
+
+(defn- drop-queued!
+  "Removes a queued job and settles its promise, freeing its queue slot on cancellation."
+  [job-id]
+  (let [entry (volatile! nil)]
+    (swap! state (fn [state]
+                   (let [queue (:queue state)
+                         idx   (->> (map-indexed vector queue)
+                                    (some (fn [[idx entry]]
+                                            (when (= (str job-id) (str (-> entry :job :id)))
+                                              idx))))]
+                     (if idx
+                       (do (vreset! entry (nth queue idx))
+                           (assoc state :queue (into (subvec queue 0 idx) (subvec queue (inc idx)))))
+                       state))))
+    (when-let [{:keys [resolve]} @entry]
+      (jobs/release! job-id)
+      (job.utils/release! job-id)
+      (resolve nil))))
+
+(defn- take-eligible
+  "Pops the first queued entry that can run now, or nil."
+  [state]
+  (let [queue (:queue state)
+        idx   (->> (map-indexed vector queue)
+                   (some (fn [[idx entry]]
+                           (when (eligible? state (-> entry :job :profile-id))
+                             idx))))]
+    (when idx
+      [(assoc state :queue (into (subvec queue 0 idx) (subvec queue (inc idx))))
+       (nth queue idx)])))
+
+(defn- pump!
+  []
+  (loop []
+    (let [entry (volatile! nil)]
+      (swap! state (fn [state]
+                     (if-let [[next-state next-entry] (take-eligible state)]
+                       (do (vreset! entry next-entry) next-state)
+                       (do (vreset! entry nil) state))))
+      (when-let [{:keys [job resolve reject]} @entry]
+        (-> (execute! job)
+            (p/then resolve)
+            (p/catch reject))
+        (recur)))))
+
+(defn submit!
+  "Registers `job` for execution. Returns a promise of the job's result, which
+  resolves when the export actually finishes; callers that only need the handle
+  can ignore it. Raises when the exporter is saturated."
+  [job]
+  (let [resolve* (volatile! nil)
+        reject*  (volatile! nil)
+        pending  (p/create (fn [resolve reject]
+                             (vreset! resolve* resolve)
+                             (vreset! reject* reject)))]
+    (if (eligible? @state (:profile-id job))
+      (-> (execute! job)
+          (p/then @resolve*)
+          (p/catch @reject*))
+
+      (let [queued (volatile! false)]
+        (swap! state (fn [state]
+                       (if (< (count (:queue state)) (max-queued))
+                         (do (vreset! queued true)
+                             (update state :queue conj {:job job
+                                                        :resolve @resolve*
+                                                        :reject @reject*}))
+                         (do (vreset! queued false) state))))
+        (when-not @queued
+          (ex/raise :type :validation
+                    :code :queue-full
+                    :hint "too many queued export jobs"))
+        (jobs/on-cancel (:id job) (fn [] (drop-queued! (:id job))))
+        (l/dbg :hint "export job queued" :job-id (str (:id job)))))
+    pending))

exporter/src/app/jobs/store.cljs
@@ -1,0 +1,87 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; Copyright (c) KALEIDOS INC Sucursal en España SL
+
+(ns app.jobs.store
+  "Redis persistence for export jobs.
+
+  Stores each job as a single blob with a TTL matching the exported file,
+  so records expire with their files.
+
+  Reads use Redis. Cancellation requires the process running the job."
+  (:require
+   [app.common.logging :as l]
+   [app.common.transit :as t]
+   [app.config :as cf]
+   [app.redis :as redis]
+   [promesa.core :as p]))
+
+(def ^:private cancel-topic (redis/->key "job-cancel"))
+
+(defn- job-key
+  [job-id]
+  (redis/->key "job." job-id))
+
+(defn- ttl
+  []
+  (cf/get :exporter-job-ttl 3600))
+
+(defn persist!
+  "Writes the job record and refreshes its TTL.
+
+  If the write fails, log it and continue. The export still runs
+  and publishes websocket updates, but the job can't be fetched
+  afterward (fetch returns nil, REST returns 404)."
+  [{:keys [id state] :as job}]
+  (let [jkey (job-key id)]
+    (->> (p/do
+           (redis/hset! jkey {:data (t/encode-str job)})
+           (redis/expire! jkey (ttl))
+           job)
+         (p/merr (fn [cause]
+                   (if (= :redis-not-available (:code (ex-data cause)))
+                     (l/warn :hint "job record not persisted, no redis connection"
+                             :job-id (str id) :state state)
+                     (l/error :hint "unable to persist job record"
+                              :job-id (str id) :state state :cause cause))
+                   (p/resolved job))))))
+
+(defn fetch
+  "The job record, or nil when unknown or expired."
+  [job-id]
+  (->> (redis/hgetall (job-key job-id))
+       (p/fmap (fn [data]
+                 (when-let [blob (get data "data")]
+                   (try
+                     (t/decode-str blob)
+                     (catch :default cause
+                       (l/warn :hint "unable to decode job record" :job-id (str job-id) :cause cause)
+                       nil)))))))
+
+(defn fetch-all
+  []
+  (->> (redis/scan (redis/->key "job.*"))
+       (p/mcat (fn [keys]
+                 (->> (map (fn [k]
+                             (->> (redis/hgetall k)
+                                  (p/fmap (fn [data]
+                                            (when-let [blob (get data "data")]
+                                              (try
+                                                (t/decode-str blob)
+                                                (catch :default _ nil)))))))
+                           keys)
+                      (p/all))))
+       (p/fmap (fn [jobs] (vec (remove nil? jobs))))))
+
+(defn request-cancel!
+  "Asks every exporter to cancel `job-id`. Only the one running it will act."
+  [job-id]
+  (redis/pub! cancel-topic (str job-id)))
+
+(defn on-cancel-request
+  "Registers `handler` (fn of the job-id string) for cancel requests. Returns an
+  unsubscribe fn."
+  [handler]
+  (redis/sub! cancel-topic handler))

exporter/src/app/jobs/utils.cljs
@@ -1,0 +1,86 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; Copyright (c) KALEIDOS INC Sucursal en España SL
+
+(ns app.jobs.utils
+  "Temp file ownership for export jobs.
+
+  Temp files used to be cleaned only by the per-file timer in `app.util.shell`,
+  an hour after creation and lost entirely on restart. Here each job owns the
+  paths it creates, so they are dropped as soon as it settles and whatever a
+  crash left behind is cleaned at boot."
+  (:require
+   ["node:fs/promises" :as fsp]
+   ["node:path" :as path]
+   [app.common.logging :as l]
+   [app.config :as cf]
+   [app.util.shell :as sh]
+   [cuerdas.core :as str]
+   [promesa.core :as p]))
+
+(def ^:private managed-prefix "penpot.")
+
+(defonce ^:private tracked (atom {}))
+
+(defn track!
+  "Registers `path` as owned by `job-id`, so it is removed when the job settles."
+  [job-id path]
+  (when (and job-id path)
+    (swap! tracked update (str job-id) (fnil conj #{}) path))
+  path)
+
+(defn- remove-path!
+  [path]
+  (->> (p/do (fsp/rm path #js {:recursive true :force true}))
+       (p/merr (fn [cause]
+                 (l/warn :hint "unable to remove job temp file" :path path :cause cause)
+                 (p/resolved nil)))))
+
+(defn release!
+  "Removes every file the job owns. Called once the job reached a terminal
+  state and its result has already been uploaded, so nothing else reads them."
+  [job-id]
+  (let [k     (str job-id)
+        paths (get @tracked k)]
+    (swap! tracked dissoc k)
+    (if (seq paths)
+      (->> (map remove-path! paths)
+           (p/all)
+           (p/fmap (fn [_]
+                     (l/dbg :hint "released job temp files" :job-id k :count (count paths))
+                     nil)))
+      (p/resolved nil))))
+
+(defn- clean!
+  "Removes managed temp files older than the job TTL. They can only be leftovers
+  of a previous process: every live one belongs to a job of this process."
+  []
+  (let [max-age (* 1000 (cf/get :exporter-job-ttl 3600))
+        now     (js/Date.now)]
+    (->> (p/do (fsp/readdir sh/tmpdir))
+         (p/mcat (fn [entries]
+                   (->> (filter #(str/starts-with? % managed-prefix) entries)
+                        (map (fn [entry]
+                               (let [fpath (path/join sh/tmpdir entry)]
+                                 (->> (p/do (fsp/stat fpath))
+                                      (p/mcat (fn [^js stat]
+                                                (if (> (- now (inst-ms (.-mtime stat))) max-age)
+                                                  (->> (remove-path! fpath)
+                                                       (p/fmap (constantly 1)))
+                                                  (p/resolved 0))))
+                                      (p/merr (fn [_] (p/resolved 0)))))))
+                        (p/all))))
+         (p/fmap (fn [results]
+                   (let [removed (reduce + 0 results)]
+                     (when (pos? removed)
+                       (l/info :hint "removed orphaned export temp files" :count removed))
+                     removed)))
+         (p/merr (fn [cause]
+                   (l/warn :hint "temp file cleanup failed" :cause cause)
+                   (p/resolved 0))))))
+
+(defn init
+  []
+  (clean!))