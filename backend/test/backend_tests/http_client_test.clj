3a8f3fd7864807897bcd326545e092c815cf0141
Pablo Alba <pablo.alba@kaleidos.net>
2026-09-07 15:56:21 +0200

    :sparkles: PR changes 2

--- backend/test/backend_tests/http_client_test.clj
+++ backend/test/backend_tests/http_client_test.clj
@@ -8,30 +8,23 @@
   (:require
    [app.http.client :as http]
    [clojure.test :as t]
-   [java-http-clj.core :as jhttp]))
-
-(defn- capture-send!
-  "Run `f` with java-http-clj send stubbed; return the request map passed to send."
-  [f]
-  (let [captured (atom nil)]
-    (with-redefs [jhttp/send (fn [req _opts]
-                               (reset! captured req)
-                               {:status 200 :body ""})]
-      (f)
-      @captured)))
+   [java-http-clj.core :as jhttp]
+   [mockery.core :refer [with-mocks]]))
 
 (t/deftest send-injects-default-timeout-when-absent
-  (let [client  (jhttp/build-client {})
-        request (capture-send!
-                 (fn []
-                   (http/send! client {:method :get :uri "https://example.com/"})))]
-    (t/is (= http/default-request-timeout (:timeout request)))))
+  (with-mocks [mock {:target 'java-http-clj.core/send
+                     :return {:status 200 :body ""}}]
+    (let [client (jhttp/build-client {})]
+      (http/send! client {:method :get :uri "https://example.com/"})
+      (let [[req _opts] (:call-args @mock)]
+        (t/is (= http/default-request-timeout (:timeout req)))))))
 
 (t/deftest send-preserves-caller-supplied-timeout
-  (let [client  (jhttp/build-client {})
-        request (capture-send!
-                 (fn []
-                   (http/send! client {:method :get
-                                       :uri "https://example.com/"
-                                       :timeout 5000})))]
-    (t/is (= 5000 (:timeout request)))))
+  (with-mocks [mock {:target 'java-http-clj.core/send
+                     :return {:status 200 :body ""}}]
+    (let [client (jhttp/build-client {})]
+      (http/send! client {:method :get
+                          :uri "https://example.com/"
+                          :timeout 5000})
+      (let [[req _opts] (:call-args @mock)]
+        (t/is (= 5000 (:timeout req)))))))
